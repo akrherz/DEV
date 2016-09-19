4b1d2651b4e06d9bd79008745e707684e6c391ee
akrherz <akrherz@iastate.edu>
2016-09-19 07:34:40 -0500

    cleanups and update

--- gmail/plot.py
+++ gmail/plot.py
@@ -5,18 +5,19 @@ xticks = []
 xticklabels = []
 
 for linenum, line in enumerate(open('data.csv')):
-  (year, month, count) = line.split(",")
-  counts.append( float(count) )
-  if month == "1":
-    xticks.append(linenum)
-    xticklabels.append( year)
-(fig, ax)  = plt.subplots(1,1)
+    (year, month, count) = line.split(",")
+    counts.append(float(count))
+    if month == "1" and int(year) % 2 == 0:
+        xticks.append(linenum)
+        xticklabels.append(year)
+(fig, ax) = plt.subplots(1, 1)
 
-ax.bar(range(len(counts)), counts,fc='g', ec='g')
+ax.bar(range(len(counts)), counts, fc='g', ec='g')
 ax.set_xticks(xticks)
 ax.set_xticklabels(xticklabels)
+ax.set_xlim(0, len(counts) + 1)
 ax.grid(True)
-ax.set_title("May 2001 - June 2014:: Emails sent by daryl per month")
+ax.set_title("May 2001 - Aug 2016 :: Emails sent by daryl per month")
 ax.set_ylabel("email messages saved to sent-mail")
 
 fig.savefig('test.png')
