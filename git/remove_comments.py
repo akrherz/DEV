dd2042a773289099b12f109aa97f218239b657e6
akrherz <akrherz@iastate.edu>
2017-01-05 11:45:01 -0600

    util scripts for purging CVS keywords

---
+++ git/remove_comments.py
@@ -0,0 +1,24 @@
+import os
+
+for root, dirs, files in os.walk("."):
+    for fn in files:
+        localfn = "%s/%s" % (root, fn)
+        lastline = ""
+        dofile = False
+        for line in open(localfn):
+            if line.strip() == '*' and lastline.strip() == '/**':
+                # if line.strip() == '-->' and lastline.strip() == '<!--':
+                print localfn, line, lastline
+                dofile = True
+            lastline = line.strip()
+        if not dofile:
+            continue
+        lines = open(localfn).readlines()
+        o = open(localfn, 'w')
+        for line in lines:
+            if line.strip() == '*' and lastline.strip() == '/**':
+                pass
+            else:
+                o.write(line)
+            lastline = line.strip()
+        o.close()
---
+++ git/remove_cvstags.py
@@ -0,0 +1,15 @@
+# git grep '$RCSfile' > todo
+import re
+
+TAGS = re.compile("\$(RCSfile|Date|Revision|Id)")
+
+for line in open('todo'):
+    if line.startswith("Binary"):
+        continue
+    fn = line.split(":")[0]
+    lines = open(fn).readlines()
+    o = open(fn, 'w')
+    for line in lines:
+        if not TAGS.search(line):
+            o.write(line)
+    o.close()
