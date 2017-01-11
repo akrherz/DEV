ddb9a0b4c2c6d5977d8ee2e3e3543e114444506f
akrherz <akrherz@iastate.edu>
2017-01-11 11:52:00 -0600

    python zonal stats example

---
+++ zonalstats/sandbox.py
@@ -0,0 +1,19 @@
+"""My first attempt at zonal stats!"""
+from rasterstats import zonal_stats
+import netCDF4
+from affine import Affine
+import geopandas as gpd
+import pandas as pd
+
+geodf = gpd.GeoDataFrame.from_file(("/mesonet/data/gis/static/shape/"
+                                    "4326/us/huc6_01.shp"))
+
+nc = netCDF4.Dataset("/mesonet/data/iemre/2016_mw_daily.nc")
+grid = nc.variables['p01d_12z'][100, :, :]
+
+# http://www.perrygeo.com/python-affine-transforms.html
+aff = Affine(0.25, 0., -104., 0., -0.25, 50.)
+
+zs = zonal_stats(geodf, grid, affine=aff)
+new_geodf = geodf.join(pd.DataFrame(zs))
+print new_geodf[new_geodf['HUC6'] == '070802']
