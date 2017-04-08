1ebeaded8f1cd9fe22b2b24a4fff779ed2e614d5
akrherz <akrherz@iastate.edu>
2017-04-07 20:15:42 -0500

    some SBW stats work

---
+++ nws/sbw_freq_map.py
@@ -0,0 +1,47 @@
+import numpy as np
+import psycopg2
+import datetime
+import pytz
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from pyiem.plot import MapPlot
+from pyiem import reference
+
+pgconn = psycopg2.connect(database='postgis', host='localhost', port=5555,
+                          user='nobody')
+cursor = pgconn.cursor()
+
+dx = 0.01
+lons = np.arange(reference.IA_WEST, reference.IA_EAST, dx)
+lats = np.arange(reference.IA_SOUTH, reference.IA_NORTH, dx)
+vals = np.zeros((len(lats), len(lons)))
+utcnow = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
+for i, lon in enumerate(tqdm(lons)):
+    for j, lat in enumerate(lats):
+        cursor.execute("""
+        select eventid from sbw where
+        ST_Covers(geom, ST_GeomFromEWKT('SRID=4326;POINT(%s %s)'))
+        and status = 'NEW' and phenomena = 'TO' and significance = 'W'
+        and issue < '2017-01-01' and issue > '2002-01-01'
+        """, (lon, lat))
+        val = cursor.rowcount
+        vals[j, i] = val / 15.0  # Number of years 2002-2016
+
+print("Maximum is: %.1f" % (np.max(vals),))
+
+m = MapPlot(sector='iowa',
+            title='Avg Number of Storm Based Tornado Warnings per Year',
+            subtitle=("(2003 through 2016) based on unofficial "
+                      "archives maintained by the IEM, %sx%s analysis grid"
+                      ) % (dx, dx))
+cmap = plt.get_cmap('jet')
+cmap.set_under('white')
+cmap.set_over('black')
+lons, lats = np.meshgrid(lons, lats)
+rng = np.arange(0, 2.1, 0.2)
+rng[0] = 0.01
+m.pcolormesh(lons, lats, vals,
+             rng, cmap=cmap, units='count')
+m.drawcounties()
+m.postprocess(filename='count.png')
+m.close()
