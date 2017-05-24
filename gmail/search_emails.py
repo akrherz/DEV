efea42598b24a609e8cf3a7631d32201ded0e2ae
akrherz <akrherz@iastate.edu>
2017-05-24 14:28:55 -0500

    generalize

--- gmail/search_emails.py
+++ gmail/search_emails.py
@@ -1,16 +1,26 @@
-import imaplib
-import re
+"""Search emails for a given subject for NCEI"""
+from __future__ import print_function
+import sys
 import email
-import datetime
 import getpass
 
-obj = imaplib.IMAP4_SSL('imap.gmail.com', 993)
-obj.login('akrherz@gmail.com', getpass.getpass())
-obj.select("[Gmail]/All Mail")
-typ, data = obj.search(None, '(SUBJECT "AIRS Order" since "21-May-2017")')
-for num in data[0].split():
-    found = False
-    typ, data = obj.fetch(num, '(RFC822)')
-    msg = email.message_from_string(data[0][1])
-    val = msg['Subject'].replace('AIRS Order', '').replace('Complete', '')
-    print 'mirror', val 
+import imaplib
+
+
+def main(argv):
+    """Do Something"""
+    obj = imaplib.IMAP4_SSL('imap.gmail.com', 993)
+    obj.login('akrherz@gmail.com', getpass.getpass())
+    obj.select("[Gmail]/All Mail")
+    date = argv[1]  # Format dd-Mon-YYYY
+    _typ, data = obj.search(None,
+                            '(SUBJECT "AIRS Order" since "%s")' % (date, ))
+    for num in data[0].split():
+        _typ, data = obj.fetch(num, '(RFC822)')
+        msg = email.message_from_string(data[0][1])
+        val = msg['Subject'].replace('AIRS Order', '').replace('Complete', '')
+        print('mirror %s' % (val, ))
+
+
+if __name__ == '__main__':
+    main(sys.argv)
