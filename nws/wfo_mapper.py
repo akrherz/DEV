8456416cecfc0adf537399cf33f3e4b150470e2b
akrherz <akrherz@iastate.edu>
2017-05-26 22:42:06 -0500

    average number of SVSs

--- nws/wfo_mapper.py
+++ nws/wfo_mapper.py
@@ -1,6 +1,7 @@
 """Generic plotter"""
 import datetime
 
+import numpy as np
 from pyiem.plot import MapPlot
 from pyiem.network import Table
 import matplotlib.pyplot as plt
@@ -13,16 +14,12 @@ def get_database_data():
     pgconn = psycopg2.connect(database='postgis', host='localhost',
                               port=5555, user='nobody')
     df = read_sql("""
-    WITH wfos as (
-        SELECT id, tzname from stations where network = 'WFO'),
-    data as (
-        SELECT wfo, extract(hour from issue at time zone w.tzname) as hr
-        from sbw s, wfos w where s.wfo = w.id and
-        status = 'NEW' and phenomena = 'TO'
-        and significance = 'W')
-    select wfo,
-    sum(case when hr >= 16 and hr < 21 then 1 else 0 end) /
-    count(*)::float * 100. as val from data GROUP by wfo
+    with data as (
+        select wfo, eventid, extract(year from expire) as yr, count(*)
+        from sbw where expire > '2012-01-01' and phenomena = 'SV'
+        and significance = 'W' GROUP by wfo, eventid, yr)
+
+    select wfo, avg(count) - 1 as val from data GROUP by wfo ORDER by val DESC
     """, pgconn, index_col='wfo')
     return df['val'].to_dict()
 
@@ -31,13 +28,13 @@ def main():
     """Go MAin"""
     vals = get_database_data()
 
-    bins = range(0, 101, 10)
+    bins = np.arange(0, 4.3, 0.25)
     cmap = plt.get_cmap('plasma')
     p = MapPlot(sector='nws', continental_color='white',
-                title="2002-17 Percentage of Tornado Warnings between 4 PM and 9 PM",
-                subtitle='based on warning issuance using the local time zone of the WFO')
-    p.fill_cwas(vals, bins=bins, lblformat='%.0f', cmap=cmap, ilabel=True,
-                units='Percentage')
+                title="2012-17 Average Number of SVS Updates per SVR T'Storm Warning",
+                subtitle='based on unofficial IEM Data')
+    p.fill_cwas(vals, bins=bins, lblformat='%.1f', cmap=cmap, ilabel=True,
+                units='Count')
     p.postprocess(filename='test.png')
 
 
