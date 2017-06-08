c5c188a69f49877b87491cd3facd8eeb66477ef9
akrherz <akrherz@iastate.edu>
2017-06-08 05:31:39 -0500

    spc sql for county tor watch outlook at

--- spc/run.sql
+++ spc/run.sql
@@ -1,3 +1,27 @@
+-- Get the outlook valid for Tornado Watches
+with watches as (
+	select w.ugc, u.geom, w.issue, w.expire from warnings_2017 w JOIN ugcs u
+	on (w.gid = u.gid) WHERE  w.phenomena = 'TO'
+	and w.significance = 'A'),
+
+agg as (
+	select w.ugc, o.threshold, o.valid as outlook_valid,
+	o.issue as outlook_issue, o.expire as outlook_expire,
+	w.issue as watch_issue
+	from watches w LEFT JOIN spc_outlooks o ON (ST_Intersects(w.geom, o.geom)
+		and w.issue > o.valid and w.issue < o.expire)
+	WHERE o.day = 1 and o.outlook_type = 'C' and o.category = 'CATEGORICAL'),
+
+agg2 as (
+    SELECT a.*, t.priority,
+    rank() OVER (PARTITION by ugc, watch_issue, outlook_expire
+		ORDER by outlook_valid DESC, t.priority DESC)
+    from agg a JOIN spc_outlook_thresholds t
+    on (a.threshold = t.threshold))
+
+SELECT threshold, count(*) from agg2 WHERE rank = 1 GROUP by threshold;
+
+
 with mods as (
     select expire, geom, threshold from spc_outlooks where
     outlook_type = 'C' and day = 1 and threshold = 'MDT'
