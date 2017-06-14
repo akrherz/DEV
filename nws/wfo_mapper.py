9190033239603a72949e3421c5c1117e1a1d501a
akrherz <akrherz@iastate.edu>
2017-06-14 09:00:50 -0500

    TORNADO tag possible frequency
    https://twitter.com/akrherz/status/874989850914697218

--- nws/wfo_mapper.py
+++ nws/wfo_mapper.py
@@ -14,12 +14,14 @@ def get_database_data():
     pgconn = psycopg2.connect(database='postgis', host='localhost',
                               port=5555, user='nobody')
     df = read_sql("""
-    with data as (
-        select wfo, eventid, extract(year from expire) as yr, count(*)
-        from sbw where expire > '2012-01-01' and phenomena = 'SV'
-        and significance = 'W' GROUP by wfo, eventid, yr)
-
-    select wfo, avg(count) - 1 as val from data GROUP by wfo ORDER by val DESC
+    with bnds as (
+        select wfo, min(issue) from sbw where phenomena = 'SV'
+        and hailtag > 0 GROUP by wfo)
+    select s.wfo, sum(case when tornadotag = 'POSSIBLE' then 1 else 0 end)
+    / count(*)::float * 100. as val from sbw s, bnds b
+    WHERE s.wfo = b.wfo and s.expire >= b.min and
+    s.status != 'NEW'
+    and s.phenomena = 'SV' GROUP by s.wfo ORDER by val
     """, pgconn, index_col='wfo')
     return df['val'].to_dict()
 
@@ -28,14 +30,16 @@ def main():
     """Go MAin"""
     vals = get_database_data()
 
-    bins = np.arange(0, 4.3, 0.25)
+    bins = np.arange(0, 41.0, 4.0)
     cmap = plt.get_cmap('plasma')
-    p = MapPlot(sector='nws', continental_color='white',
-                title="2012-17 Average Number of SVS Updates per SVR T'Storm Warning",
-                subtitle='based on unofficial IEM Data')
-    p.fill_cwas(vals, bins=bins, lblformat='%.1f', cmap=cmap, ilabel=True,
-                units='Count')
-    p.postprocess(filename='test.png')
+    mp = MapPlot(sector='nws', continental_color='white',
+                 title=("2010-17 % of SVR Warn SVS Update "
+                        "with TORNADO POSSIBLE Tag"),
+                 subtitle=('based on unofficial IEM Data and for period of '
+                           'tag issuance per office, SVSs not weighted by warning'))
+    mp.fill_cwas(vals, bins=bins, lblformat='%.1f', cmap=cmap, ilabel=True,
+                 units='Count')
+    mp.postprocess(filename='test.png')
 
 
 if __name__ == '__main__':
