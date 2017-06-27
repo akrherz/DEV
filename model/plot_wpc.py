978c903c3c21eb984e096395f740512fd7ffa362
akrherz <akrherz@iastate.edu>
2017-06-26 22:35:50 -0500

    pylint WPC plot

--- model/plot_wpc.py
+++ model/plot_wpc.py
@@ -1,23 +1,33 @@
+"""Plot WPC"""
+from __future__ import print_function
+
 import pygrib
 from pyiem.plot import MapPlot
 from pyiem import reference
-import numpy as np
 from pyiem.datatypes import distance
+import numpy as np
 
-g = pygrib.open('p168m_2016090600f168.grb')
 
-grb = g[1]
-lats, lons = grb.latlons()
+def main():
+    """Go Main"""
+    g = pygrib.open('/tmp/p120m_2017062700f120.grb')
 
-m = MapPlot(sector='custom', project='aea',
-            south=reference.MW_SOUTH, north=reference.MW_NORTH,
-            east=reference.MW_EAST, west=reference.MW_WEST,
-            axisbg='tan',
-            title=("Weather Prediction Center (WPC) "
-                   "7 Day Forecasted Precipitation"),
-            subtitle='7 PM 5 September thru 7 PM 12 September 2016')
+    grb = g[1]
+    lats, lons = grb.latlons()
 
-m.contourf(lons, lats, distance(grb['values'], 'MM').value('IN'),
-           np.arange(0, 4.6, 0.25), units='inches')
+    mp = MapPlot(sector='custom', project='aea',
+                 south=reference.MW_SOUTH, north=reference.MW_NORTH,
+                 east=reference.MW_EAST, west=reference.MW_WEST,
+                 axisbg='tan',
+                 title=("Weather Prediction Center (WPC) "
+                        "5 Day Forecasted Precipitation"),
+                 subtitle='7 PM 26 June thru 7 PM 1 July 2017')
 
-m.postprocess(filename='test.png')
+    mp.contourf(lons, lats, distance(grb['values'], 'MM').value('IN'),
+                np.arange(0, 4.6, 0.5), units='inches')
+
+    mp.postprocess(filename='test.png')
+
+
+if __name__ == '__main__':
+    main()
