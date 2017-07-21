bc4970d0eb74c64ffb0d7363a8d851e2bcdd1ac9
akrherz <akrherz@iastate.edu>
2017-07-21 10:38:26 -0500

    pylint

--- matplotlib/ames_mrms.py
+++ matplotlib/ames_mrms.py
@@ -1,66 +1,44 @@
+"""MRMS Plotting util for zoomed in areas"""
+
 import pygrib
-import pyiem.mrms as mrms2
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
-from pyiem import reference
-from matplotlib.patches import Polygon
-import numpy as np
-import netCDF4
-from PIL import Image
-from pyiem.plot import MapPlot, nwsprecip
+from pyiem.plot import MapPlot, nwsprecip, Z_OVERLAY2
 from pyiem.datatypes import distance
-from pyiem import iemre
-import datetime
+import shapefile
+from shapely.geometry import shape
+import cartopy.crs as ccrs
 
-import matplotlib.patheffects as PathEffects
-import matplotlib.colors as mpcolors
 
-m = MapPlot(sector='custom', north=42.69, east=-93.1, south=41.77, west=-94.1,
-            axisbg='white',
-  title='NOAA MRMS Q3: RADAR Estimated + Gauge Corrected Precipitation',
-  subtitle='Valid: 8 PM 26 June - 8 PM 29 June 2016')
+def main():
+    """Go!"""
+    title = 'NOAA MRMS Q3: RADAR+Gauge Corr Estimated Precipitation'
+    mp = MapPlot(sector='custom',
+                 north=42.3, east=-93.0, south=41.45, west=-94.1,
+                 axisbg='white',
+                 title=title,
+                 subtitle='Valid: 8 AM 20 July - 8 AM 21 July 2017')
 
-"""
-m.map.readshapefile('high', 'roads', ax=m.ax, drawbounds=False)
-for nshape, seg in enumerate(m.map.roads):
-     if (m.map.roads_info[nshape]['US1'] in (30,) or
-        m.map.roads_info[nshape]['INT1'] in (35, 235, 80)):
-         data = np.array( seg )
-         m.ax.plot(data[:,0], data[:,1], lw=3, linestyle='-',color='k',
-                   zorder=3)
-         m.ax.plot(data[:,0], data[:,1], lw=1, linestyle='-',color='w',
-                   zorder=4)
-m.map.readshapefile('cities', 'cities', ax=m.ax)
-for nshape, seg in enumerate(m.map.cities):
-     #if m.map.cities_info[nshape]['NAME10'] != 'Ames':
-     #    continue
-     poly=Polygon(seg, fc='None', ec='k', lw=1.5, zorder=3)
-     m.ax.add_patch(poly)
+    shp = shapefile.Reader('cities.shp')
+    for record in shp.shapeRecords():
+        geo = shape(record.shape)
+        mp.ax.add_geometries([geo], ccrs.PlateCarree(), zorder=Z_OVERLAY2,
+                             facecolor='None', edgecolor='k', lw=2)
 
-"""
-grbs = pygrib.open('MRMS_GaugeCorr_QPE_72H_00.00_20170630-010000.grib2')
-g = grbs.message(1)
-pcpn = distance(g['values'], 'MM').value('IN')
-lats, lons = g.latlons()
-lons -= 360.
-clevs = [0.01, 0.1, 0.3, 0.5, 0.75, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 8, 10]
-cmap = nwsprecip()
+    grbs = pygrib.open('MRMS_GaugeCorr_QPE_24H_00.00_20170721-130000.grib2')
+    grb = grbs.message(1)
+    pcpn = distance(grb['values'], 'MM').value('IN')
+    lats, lons = grb.latlons()
+    lons -= 360.
+    clevs = [0.01, 0.1, 0.3, 0.5, 0.75, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 8, 10]
+    cmap = nwsprecip()
+    cmap.set_over('k')
 
-m.pcolormesh(lons, lats, pcpn, clevs, cmap=cmap, latlon=True, units='inch')
-"""
-xx, yy = m.map(-93.723, 41.731)
-txt = m.ax.text(xx, yy, "DMX", zorder=5, ha='center',
-          va='center', fontsize=10)
-txt.set_path_effects([PathEffects.withStroke(linewidth=2, foreground="w")])
+    mp.pcolormesh(lons, lats, pcpn, clevs, cmap=cmap, latlon=True,
+                  units='inch')
 
-xx, yy = m.map(-93.62, 41.99)
-txt = m.ax.text(xx, yy, "%.2f" % (3.79,), zorder=5, ha='center',
-                va='center', fontsize=20)
-txt.set_path_effects([PathEffects.withStroke(linewidth=2, foreground="w")])
-"""
+    mp.drawcities(labelbuffer=5, minarea=0.2)
+    mp.drawcounties()
+    mp.postprocess(filename='test.png')
 
-m.drawcities(labelbuffer=5, minarea=0.2)
-m.drawcounties()
-m.postprocess(filename='test.png')
+
+if __name__ == '__main__':
+    main()
