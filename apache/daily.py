d3853fac26db11db198bde7153214647b5f52b88
akrherz <akrherz@iastate.edu>
2017-08-25 08:18:43 -0500

    make twitter plot of BRO nexrad hits
    https://twitter.com/akrherz/status/901064882623315968

---
+++ apache/daily.py
@@ -0,0 +1,44 @@
+"""barplot of daily requests"""
+import datetime
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+
+def main():
+    """Plot"""
+    hits = {}
+    for line in open('KBRO.log'):
+        tokens = line.split()
+        ts = datetime.datetime.strptime(tokens[3].split()[0],
+                                        "[%d/%b/%Y:%H:%M:%S")
+        yyyymmdd = ts.strftime("%Y%m%d")
+        if yyyymmdd not in hits:
+            hits[yyyymmdd] = 0
+        hits[yyyymmdd] += 1
+
+    keys = hits.keys()
+    keys.sort()
+    xs = []
+    ys = []
+    for yyyymmdd in keys:
+        if yyyymmdd == '20170825':
+            continue
+        date = datetime.datetime.strptime(yyyymmdd, '%Y%m%d')
+        xs.append(date)
+        ys.append(hits[yyyymmdd])
+
+    (fig, ax) = plt.subplots(1, 1)
+    ax.bar(xs, ys)
+    ax.grid(True)
+    ax.set_ylabel("Website Requests")
+    ax.set_xlim(datetime.datetime(2017, 7, 23, 12),
+                datetime.datetime(2017, 8, 25))
+    ax.set_title(("IEM NEXRAD Level II Download Website (24 Jul - 24 Aug 2017)\n"
+                  "Daily Requests for KBRO Brownsville Level II Data"))
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%-d\n%b"))
+    fig.savefig('test.png')
+
+
+if __name__ == '__main__':
+    main()
