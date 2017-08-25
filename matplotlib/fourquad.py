6dbab705dc34d515f7cd63145099d65a1eccdfb2
akrherz <akrherz@iastate.edu>
2017-08-25 11:47:04 -0500

    update javed's plot

--- matplotlib/fourquad.py
+++ matplotlib/fourquad.py
@@ -1,34 +1,45 @@
+"""Plot for Javed's paper"""
+from __future__ import print_function
+
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
-font0 = FontProperties()
-font0.set_weight('bold')
 
-df = pd.read_excel("/tmp/ames.xlsx")
-print df
 
-(fig, ax) = plt.subplots(1, 1)
+def main():
+    """GO!"""
+    font0 = FontProperties()
+    font0.set_weight('bold')
 
-for _, row in df.iterrows():
-    c = 'r' if row['Year'] in [2011, 2012, 2013] else 'k'
-    c = 'g' if row['Year'] in [1980, 1992, 1993] else c
-    ax.text(row['t'], row['p'], ("%i" % (row['Year'],))[-2:], color=c,
-            ha='center')
+    # email from Javed dated Sep 22, 2016
+    df = pd.read_excel("/tmp/ames.xlsx")
 
-ax.set_xlim(df['t'].min() - 0.3, df['t'].max() + 0.3)
-ax.set_ylim(df['p'].min() - 10, df['p'].max() + 10)
-ax.set_xlabel("Average Temperature ($^\circ$C)")
-ax.set_ylabel("Cumulative Precipitation (cm)")
+    (fig, ax) = plt.subplots(1, 1)
 
-ax.text(0.15, 0.95, "Cool & Wet", ha='center', transform=ax.transAxes,
-        fontproperties=font0)
-ax.text(0.85, 0.95, "Warm & Wet", ha='center', transform=ax.transAxes,
-        fontproperties=font0)
-ax.text(0.85, 0.05, "Warm & Dry", ha='center', transform=ax.transAxes,
-        fontproperties=font0)
-ax.text(0.15, 0.05, "Cool & Dry", ha='center', transform=ax.transAxes,
-        fontproperties=font0)
-ax.axhline(df['p'].mean())
-ax.axvline(df['t'].mean())
+    for _, row in df.iterrows():
+        c = 'r' if row['Year'] in [2011, 2012, 2013] else 'k'
+        c = 'g' if row['Year'] in [1980, 1992, 1993] else c
+        ax.text(row['t'], row['p'], ("%i" % (row['Year'],))[-2:], color=c,
+                ha='center')
 
-fig.savefig('test.png')
+    ax.set_xlim(df['t'].min() - 0.3, df['t'].max() + 0.3)
+    ax.set_ylim(df['p'].min() - 10, df['p'].max() + 10)
+    ax.set_xlabel(r"Average Temperature ($^\circ$C)")
+    ax.set_ylabel("Cumulative Precipitation (cm)")
+
+    ax.text(0.15, 0.95, "Cool & Wet", ha='center', transform=ax.transAxes,
+            fontproperties=font0)
+    ax.text(0.85, 0.95, "Warm & Wet", ha='center', transform=ax.transAxes,
+            fontproperties=font0)
+    ax.text(0.85, 0.05, "Warm & Dry", ha='center', transform=ax.transAxes,
+            fontproperties=font0)
+    ax.text(0.15, 0.05, "Cool & Dry", ha='center', transform=ax.transAxes,
+            fontproperties=font0)
+    ax.axhline(df['p'].mean())
+    ax.axvline(df['t'].mean())
+
+    fig.savefig('test.pdf', dpi=300)
+
+
+if __name__ == '__main__':
+    main()
