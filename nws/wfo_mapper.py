526a8636ee688edee4284dd7f5403fd1c5f2b92d
akrherz <akrherz@iastate.edu>
2017-10-03 21:12:22 -0500

    twitter plot on first FL.W polygon
    https://twitter.com/akrherz/status/915393368800337920

--- nws/wfo_mapper.py
+++ nws/wfo_mapper.py
@@ -1,4 +1,5 @@
 """Generic plotter"""
+from __future__ import print_function
 import datetime
 
 import numpy as np
@@ -15,29 +16,28 @@ def get_database_data():
                               port=5555, user='nobody')
     df = read_sql("""
     with bnds as (
-        select wfo, min(issue) from sbw where phenomena = 'SV'
-        and hailtag > 0 GROUP by wfo)
-    select s.wfo, sum(case when tornadotag = 'POSSIBLE' then 1 else 0 end)
-    / count(*)::float * 100. as val from sbw s, bnds b
-    WHERE s.wfo = b.wfo and s.expire >= b.min and
-    s.status != 'NEW'
-    and s.phenomena = 'SV' GROUP by s.wfo ORDER by val
+        select wfo, min(extract(year from issue)) from sbw
+        WHERE phenomena = 'FL' and significance = 'W'
+        and issue > '2001-01-01' GROUP by wfo)
+    select s.wfo, b.min::int as year from sbw s, bnds b
+    WHERE s.wfo = b.wfo ORDER by year ASC
     """, pgconn, index_col='wfo')
-    return df['val'].to_dict()
+    print(df['year'].describe())
+    return df['year'].to_dict()
 
 
 def main():
     """Go MAin"""
     vals = get_database_data()
 
-    bins = np.arange(0, 41.0, 4.0)
+    bins = np.arange(2011, 2019, 1, dtype='i')
     cmap = plt.get_cmap('plasma')
-    mp = MapPlot(sector='nws', continental_color='white',
-                 title=("2010-17 % of SVR Warn SVS Update "
-                        "with TORNADO POSSIBLE Tag"),
-                 subtitle=('based on unofficial IEM Data and for period of '
-                           'tag issuance per office, SVSs not weighted by warning'))
-    mp.fill_cwas(vals, bins=bins, lblformat='%.1f', cmap=cmap, ilabel=True,
+    cmap.set_over('black')
+    cmap.set_under('black')
+    mp = MapPlot(sector='nws', continentalcolor='white',
+                 title=("Year of First Flood Warning (FL.W) Polygon"),
+                 subtitle=('based on unofficial IEM Processing and Archiving'))
+    mp.fill_cwas(vals, bins=bins, lblformat='%.0f', cmap=cmap, ilabel=True,
                  units='Count')
     mp.postprocess(filename='test.png')
 
