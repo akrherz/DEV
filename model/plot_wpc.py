4299c57f1052e32d32da4f8ea760c98d446fd1ac
akrherz <akrherz@iastate.edu>
2017-10-04 23:13:12 -0500

    generalize wpc plot some more

--- model/plot_wpc.py
+++ model/plot_wpc.py
@@ -1,5 +1,6 @@
 """Plot WPC"""
 from __future__ import print_function
+import glob
 
 import pygrib
 from pyiem.plot import MapPlot
@@ -10,20 +11,25 @@ import numpy as np
 
 def main():
     """Go Main"""
-    g = pygrib.open('/tmp/p120m_2017062700f120.grb')
-
-    grb = g[1]
-    lats, lons = grb.latlons()
+    total = None
+    for fn in glob.glob("/tmp/p24m_201710050*"):
+        grbs = pygrib.open(fn)
+        grb = grbs[1]
+        if total is None:
+            total = grb['values']
+            lats, lons = grb.latlons()
+        else:
+            total += grb['values']
 
     mp = MapPlot(sector='custom', project='aea',
                  south=reference.MW_SOUTH, north=reference.MW_NORTH,
                  east=reference.MW_EAST, west=reference.MW_WEST,
                  axisbg='tan',
                  title=("Weather Prediction Center (WPC) "
-                        "5 Day Forecasted Precipitation"),
-                 subtitle='7 PM 26 June thru 7 PM 1 July 2017')
+                        "3 Day Forecasted Precipitation"),
+                 subtitle='7 PM 4 October thru 7 PM 7 October 2017')
 
-    mp.contourf(lons, lats, distance(grb['values'], 'MM').value('IN'),
+    mp.contourf(lons, lats, distance(total, 'MM').value('IN'),
                 np.arange(0, 4.6, 0.5), units='inches')
 
     mp.postprocess(filename='test.png')
