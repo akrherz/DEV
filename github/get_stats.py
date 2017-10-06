aff026509ae9bbe47293d6150d668a4279919187
akrherz <akrherz@iastate.edu>
2017-10-06 12:37:13 -0500

    pylint

--- github/get_stats.py
+++ github/get_stats.py
@@ -1,16 +1,25 @@
-import requests
+"""Cheap script to get stats"""
+from __future__ import print_function
 import sys
 
-PACKAGE = sys.argv[1]
-RELEASE = sys.argv[2]
+import requests
 
-req = requests.get(("https://api.github.com/repos/igniterealtime/%s/"
-                    "releases/tags/v%s" % (PACKAGE, RELEASE)))
-j = req.json()
-total = 0
-print("%s Download Stats for Release: %s" % (PACKAGE, RELEASE))
-for a in j['assets']:
-    print("%27s %6i" % (a['name'], a['download_count']))
-    total += a['download_count']
-print("%27s ======" % ("", ))
-print("%27s %6i" % ("", total))
+
+def main(argv):
+    """Go Main Go"""
+    package = argv[1]
+    release = argv[2]
+    req = requests.get(("https://api.github.com/repos/igniterealtime/%s/"
+                        "releases/tags/v%s" % (package, release)))
+    j = req.json()
+    total = 0
+    print("%s Download Stats for Release: %s" % (package, release))
+    for asset in j['assets']:
+        print("%33s %6i" % (asset['name'], asset['download_count']))
+        total += asset['download_count']
+    print("%33s ======" % ("", ))
+    print("%33s %6i" % ("", total))
+
+
+if __name__ == '__main__':
+    main(sys.argv)
