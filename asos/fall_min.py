5f917304b920f677aa9b084c085f3502ae7b33d3
akrherz <akrherz@iastate.edu>
2017-10-16 21:17:59 -0500

    17 Oct feature

--- asos/fall_min.py
+++ asos/fall_min.py
@@ -1,31 +1,37 @@
-from pyiem.plot import MapPlot
+"""Minimum Fall Temperature"""
+from __future__ import print_function
 import datetime
 import psycopg2
-IEM = psycopg2.connect(database='iem', host='iemdb', user='nobody')
-icursor = IEM.cursor()
 
-vals = []
-lats = []
-lons = []
-valmask = []
-icursor.execute("""SELECT id, network, ST_x(geom) as lon,
-    ST_y(geom) as lat, min(min_tmpf)
-    from summary_2015 s JOIN stations t on (t.iemid = s.iemid)
-    WHERE network IN ('IA_ASOS','AWOS') and min_tmpf > -50 and
-    day > '2012-08-01' and id not in ('XXX')
-    GROUP by id, network, lon, lat ORDER by min ASC""")
-for row in icursor:
-    vals.append(row[4])
-    lats.append(row[3])
-    lons.append(row[2])
-    valmask.append(row[1] in ['IA_ASOS', 'AWOS'])
-    print row[4], row[0]
+from pandas.io.sql import read_sql
+from pyiem.plot import MapPlot
 
-m = MapPlot(title="2014-2015 Winter Minimum Temperature $^\circ$F",
-            axisbg='white',
-            subtitle=('Automated Weather Stations, Valid Fall 2014 - %s'
-                      ) % (datetime.datetime.now().strftime("%d %b %Y"),))
-# m.contourf(lons, lats, vals, np.arange(-30,1,4))
-m.plot_values(lons, lats, vals, '%.0f', valmask)
-m.drawcounties()
-m.postprocess(filename='150223.png')
+
+def main():
+    """Go"""
+    pgconn = psycopg2.connect(database='iem', host='localhost', port=5555,
+                              user='nobody')
+
+    df = read_sql("""
+        SELECT id, network, ST_x(geom) as lon,
+        ST_y(geom) as lat, min(min_tmpf)
+        from summary_2017 s JOIN stations t on (t.iemid = s.iemid)
+        WHERE network IN ('IA_ASOS','AWOS') and min_tmpf > -50 and
+        day > '2017-08-01' and id not in ('XXX')
+        GROUP by id, network, lon, lat ORDER by min ASC
+    """, pgconn, index_col=None)
+
+    mp = MapPlot(title=r"2017 Fall Season Minimum Temperature $^\circ$F",
+                 axisbg='white',
+                 subtitle=('Automated Weather Stations ASOS/AWOS, '
+                           'Valid Fall 2017 thru %s'
+                           ) % (datetime.datetime.now().strftime("%d %b %Y"),))
+    # m.contourf(lons, lats, vals, np.arange(-30,1,4))
+    mp.plot_values(df['lon'].values, df['lat'].values, df['min'].values,
+                   '%.0f', labels=df['id'].values, labelbuffer=3)
+    mp.drawcounties()
+    mp.postprocess(filename='test.png')
+
+
+if __name__ == '__main__':
+    main()
