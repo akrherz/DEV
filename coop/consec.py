b9919f89269afdc3d3740aa64304f3baadb327c9
akrherz <akrherz@iastate.edu>
2017-11-08 09:15:22 -0600

    answer question about consec days

---
+++ coop/consec.py
@@ -0,0 +1,32 @@
+"""consec days"""
+from __future__ import print_function
+
+from pyiem.util import get_dbconn
+
+
+def main():
+    """Go Main Go"""
+    pgconn = get_dbconn('coop', user='nobody')
+    cursor = pgconn.cursor()
+    cursor.execute("""
+    WITH climo as (
+        select sday, avg(high) from alldata_ia where station = 'IA1319'
+        GROUP by sday)
+    select day, high, c.avg from alldata_ia a JOIN climo c on (a.sday = c.sday)
+    WHERE a.station = 'IA1319' ORDER by day ASC
+    """)
+
+    running = 0
+    maxrunning = 0
+    for row in cursor:
+        if row[1] < row[2]:
+            running += 1
+            if running > maxrunning:
+                print("maxrunning: %s date: %s" % (maxrunning, row[0]))
+                maxrunning = running
+        else:
+            running = 0
+
+
+if __name__ == '__main__':
+    main()
