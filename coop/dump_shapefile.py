887f19aa9bb7bc5dd7195b78202a8f46ccc70e8e
akrherz <akrherz@iastate.edu>
2017-11-20 10:54:09 -0600

    he had second thoughts...

--- coop/dump_shapefile.py
+++ coop/dump_shapefile.py
@@ -1,6 +1,7 @@
 """Fill a data request from Fawaz Alharbi"""
 from __future__ import print_function
 from StringIO import StringIO
+import shutil
 
 import geopandas as gpd
 import pandas as pd
@@ -20,28 +21,29 @@ def main():
         and station != 'IA0000' and substr(station, 3, 1) != 'C'
         GROUP by station, year
     ), agg as (
-        SELECT station, avg(avg_temp) as avg_temp,
-        sum(case when tot_snow = 0 then 1 else 0 end) as snow_missing,
-        avg(tot_precip) as tot_precip, avg(tot_snow) as tot_snow
-        from data GROUP by station
+        SELECT * from data WHERE tot_snow > 0
     )
     select a.*, t.geom from agg a JOIN stations t on (a.station = t.id)
-    WHERE t.network = 'IACLIMATE' and snow_missing < 3
-    """, pgconn, geom_col='geom', index_col='station')
-    df.drop('snow_missing', axis=1, inplace=True)
-    df['avg_cycles'] = 0
+    WHERE t.network = 'IACLIMATE'
+    """, pgconn, geom_col='geom', index_col=None)
+    df['cycles'] = 0
 
-    for station in tqdm.tqdm(df.index.values):
+    for station in tqdm.tqdm(df['station'].unique()):
         uri = ("http://iem.local/plotting/auto/plot/121/network:IACLIMATE::"
                "station:%s::thres1:30-32::dpi:100.csv") % (station, )
         req = requests.get(uri)
         ldf = pd.read_csv(StringIO(req.content), index_col='year')
         ldf['total'] = ldf['30-32f'] + ldf['30-32s']
-        avg = ldf.loc[slice(1998, 2015)]['total'].mean()
-        df.at[station, 'avg_cycles'] = avg
+        for year in range(1998, 2016):
+            val = ldf.loc[year]['total']
+            df.loc[((df['year'] == year) &
+                    (df['station'] == station)), 'cycles'] = val
 
-    df.reset_index(inplace=True)
-    df.to_file('iowaclimate.shp')
+    for year in range(1998, 2016):
+        df2 = df[df['year'] == year]
+        df2.to_file('iowaclimate_%s.shp' % (year, ))
+        shutil.copyfile('/mesonet/data/gis/meta/4326.prj',
+                        'iowaclimate_%s.prj' % (year, ))
 
 
 if __name__ == '__main__':
