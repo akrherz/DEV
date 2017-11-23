9ce116142f94cf0580bd326ca5a9b1c4a0200a37
akrherz <akrherz@iastate.edu>
2017-11-22 21:17:23 -0600

    23 nov feature, update to use cartopy

--- modis/plot_modis.py
+++ modis/plot_modis.py
@@ -1,24 +1,31 @@
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from mpl_toolkits.basemap import Basemap
 from pyiem.plot import MapPlot
 from shapely.wkb import loads
 import psycopg2
 import numpy as np
-DBCONN = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
-cursor = DBCONN.cursor()
+import cartopy.crs as ccrs
 """
+xDBCONN = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
+cursor = DBCONN.cursor()
 (fig, ax) = plt.subplots(1,1)
+"""
 
-m = MapPlot(title="27 January 2014 :: Terra MODIS True Color",
-            subtitle="Orange Roads are those with 'Travel Not Recommended' Status (26-27 Jan), Red box (Blizzard Warning)")
+mp = MapPlot(title="21 November 2017 :: Aqua MODIS True Color",
+             subtitle=('The whites and blues over '
+                       'the eastern portion are clouds'),
+             sector='custom', west=-98.40, east=-89.132, south=38.775,
+             north=45.15, projection=ccrs.PlateCarree())
 
-x,y= m.map(-99.40,38.775)
-x2,y2=m.map(-88.132,45.15)
+img = plt.imread('AERONET_Ames.jpg')
+mp.ax.imshow(img, extent=(-99.40, -88.132, 38.775, 45.25), origin='upper',
+             transform=ccrs.PlateCarree(), zorder=1)
 
-img=mpimg.imread('/tmp/AERONET_Ames.jpg')
-m.ax.imshow(img, extent=(x,x2,y, y2) )
+mp.postprocess(filename='test.png')
 
+"""
 cursor.execute('''select ST_asEWKB(ST_Transform(simple_geom,4326)) from roads_base
  WHERE segid in (select distinct segid from roads_2014_log where cond_code = 51 and valid > '2014-01-26')''')
 for row in cursor:
@@ -40,7 +47,6 @@ a = np.asarray(geom.exterior)
 x,y = m.map(a[:,0], a[:,1])
 m.ax.plot(x,y, color='red', lw=2.5, zorder=2)
 
-"""
 (fig, ax) = plt.subplots(2, 1)
 
 m = Basemap(projection='cea', llcrnrlat=40, urcrnrlat=44,
@@ -66,19 +72,4 @@ m2.drawstates(linewidth=2.5)
 plt.savefig('test.png')
 
 """
-(fig, ax) = plt.subplots(1,1)
 
-m = Basemap(projection='cea',llcrnrlat=40,urcrnrlat=44,
-            llcrnrlon=-97,urcrnrlon=-90,resolution='i',
-            ax=ax, fix_aspect=False)
-
-x,y= m(-99.4023, 38.8753)
-x2,y2=m(-88.1321, 45.3504)
-
-img=mpimg.imread('ames.jpg')
-ax.imshow(img, extent=(x,x2,y, y2) )
-ax.set_title("2 January 2016 :: Terra MODIS True Color")
-
-m.drawstates(linewidth=2.5)
-plt.savefig('test.png')
-"""
