10f05aac8b8807f093e74cfc71e285c270820b05
akrherz <akrherz@iastate.edu>
2017-12-29 22:58:32 -0600

    make updated twitter plot
    https://twitter.com/akrherz/status/946968486802321410

--- nws/warning_time_profile.py
+++ nws/warning_time_profile.py
@@ -1,39 +1,56 @@
-import psycopg2
+"""How quickly are warnings killed off"""
+from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
-
-POSTGIS = psycopg2.connect(database='postgis', host='localhost',
-                           port=5555, user='nobody')
-cursor = POSTGIS.cursor()
-
-cursor.execute("""
- SELECT issue, expire, init_expire from warnings where phenomena = 'BZ'
- and significance = 'W' and issue > '2005-10-01' and init_expire is not null
-""")
-
-total = cursor.rowcount
-
-counts = np.zeros(300)
-
-for row in cursor:
-    origdur = (row[2] - row[0]).days * 86400. + (row[2] - row[0]).seconds
-
-    finaldur = (row[1] - row[0]).days * 86400. + (row[1] - row[0]).seconds
-    if origdur > 0:
-        ratio = int((finaldur / origdur) * 100.0)
-        counts[:ratio] += 1
+from pyiem.util import get_dbconn
+from pyiem.nws.vtec import VTEC_PHENOMENA
 
 
-(fig, ax) = plt.subplots(1, 1)
+def get_counts(phenomena, significance):
+    """Get counts"""
+    pgconn = get_dbconn('postgis')
+    cursor = pgconn.cursor()
+    cursor.execute("""
+     SELECT issue, expire, init_expire from warnings where phenomena = %s
+     and significance = %s and issue > '2005-10-01'
+     and init_expire is not null
+    """, (phenomena, significance))
 
-ax.bar(np.linspace(0, 3, 300), counts / counts[0] * 100.0, fc='b', ec='b',
-       width=0.01)
-ax.grid(True)
-ax.set_yticks([0, 5, 10, 25, 40, 50, 60, 75, 90, 95, 100])
-ax.set_xlim(0, 3.1)
-ax.set_title(("Duration of Blizzard Warnings Relative to Issuance\n"
-              "1 Oct 2005 - 19 May 2016"))
-ax.set_xlabel("Relative to Issuance Duration")
-ax.set_ylabel("Percent by Forecast Zone Still Valid [%]")
+    counts = np.zeros(300)
 
-fig.savefig('test.png')
+    for row in cursor:
+        origdur = (row[2] - row[0]).days * 86400. + (row[2] - row[0]).seconds
+
+        finaldur = (row[1] - row[0]).days * 86400. + (row[1] - row[0]).seconds
+        if origdur > 0:
+            ratio = int((finaldur / origdur) * 100.0)
+            counts[:ratio] += 1
+    return counts
+
+
+def main():
+    """Go Main Go"""
+
+    (fig, ax) = plt.subplots(1, 1)
+
+    styles = [':', '--', '-']
+    for i, phenomena in enumerate(['TO', 'SV', 'BZ', 'WS', 'FF', 'MA']):
+        print("Processing %s" % (phenomena, ))
+        counts = get_counts(phenomena, 'W')
+        ax.plot(np.linspace(0, 3, 300), counts / counts[0] * 100.0,
+                lw=3, label=VTEC_PHENOMENA[phenomena],
+                linestyle=styles[i % 3])
+    ax.grid(True)
+    ax.legend()
+    ax.set_yticks([0, 5, 10, 25, 40, 50, 60, 75, 90, 95, 100])
+    ax.set_xlim(0, 3.1)
+    ax.set_title(("Time Duration of NWS Warnings Relative to Issuance\n"
+                  "1 Oct 2005 - 29 Dec 2017"))
+    ax.set_xlabel("Relative to Issuance Duration")
+    ax.set_ylabel("Percent by Issuance Counties/Zones Still Valid [%]")
+
+    fig.savefig('test.png')
+
+
+if __name__ == '__main__':
+    main()
