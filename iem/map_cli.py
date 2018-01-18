05833515f7f7ad551b2f8e99551aba27c3d95ca9
akrherz <akrherz@iastate.edu>
2018-01-17 21:50:08 -0600

    modernize

--- iem/map_cli.py
+++ iem/map_cli.py
@@ -1,35 +1,57 @@
-import psycopg2
+"""Map cli_data"""
+from __future__ import print_function
 import datetime
+
+from pyiem.util import get_dbconn
 from pyiem.plot import MapPlot
-IEM = psycopg2.connect(database='iem', host='iemdb', user='nobody')
-cursor = IEM.cursor()
-
-cursor.execute("""select station, st_x(geom), st_y(geom),
-    max(valid) from
-    cli_data c JOIN stations s on (s.id = c.station)
-    WHERE s.network = 'NWSCLI' and c.valid > '2016-01-01'
-    and low < 32 GROUP by station, st_x, st_y""")
-
-lats = []
-lons = []
-colors = []
-vals = []
-
-base = datetime.date(2016, 4, 15)
-
-for row in cursor:
-    if row[3] is None or row[0] in ['KGFK', 'KRAP']:
-        continue
-    lats.append(row[2])
-    lons.append(row[1])
-    vals.append(row[3].strftime("%-m/%-d"))
-    colors.append('#ff0000' if row[3] < base else '#0000ff')
 
 
-m = MapPlot(sector='midwest', axisbg='white',
-            title="2016 Date of Last Sub 32$^\circ$F Temperature",
-            subtitle='2 May 2016 based on NWS issued CLI Reports, pre April 15 dates in red')
-m.plot_values(lons, lats, vals, fmt='%s', textsize=12, color=colors,
-              labelbuffer=5)
-m.postprocess(filename='test.png')
+def main():
+    """Map some CLI data"""
+    pgconn = get_dbconn('iem')
+    cursor = pgconn.cursor()
 
+    cursor.execute("""
+    WITH data as (
+        SELECT station, valid,
+        snow + lag(snow) OVER (PARTITION by station ORDER by valid ASC) as s
+        from cli_data)
+
+    select station, st_x(geom), st_y(geom),
+    max(case when c.s >= 3 then valid else '1990-01-01'::date end),
+    max(case when c.s > 0 then valid else '1990-01-01'::date end) from
+    data c JOIN stations s on (s.id = c.station)
+    WHERE s.network = 'NWSCLI'
+    GROUP by station, st_x, st_y
+    """)
+
+    lats = []
+    lons = []
+    colors = []
+    vals = []
+
+    base = datetime.date.today()
+
+    for row in cursor:
+        if row[4].year < 2017:
+            # print(row)
+            continue
+        lats.append(row[2])
+        lons.append(row[1])
+        days = (base - row[3]).days
+        if days > 10000:
+            print(row)
+            continue
+        vals.append(days)
+        colors.append('#ff0000' if days > 1000 else '#0000ff')
+
+    mp = MapPlot(sector='midwest', axisbg='white',
+                 title="Days since last Two Day Snowfall of 3+ Inches",
+                 subtitle='17 Jan 2017 based on NWS issued CLI Reports')
+    mp.plot_values(lons, lats, vals, fmt='%s', textsize=12, color=colors,
+                   labelbuffer=1)
+    mp.postprocess(filename='test.png')
+
+
+if __name__ == '__main__':
+    main()
