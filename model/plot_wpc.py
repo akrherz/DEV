bab73fd719baf740962faff0f38346f1a78f3b45
akrherz <akrherz@iastate.edu>
2018-03-21 21:01:16 -0500

    22 mar 2018 feature

--- model/plot_wpc.py
+++ model/plot_wpc.py
@@ -3,16 +3,19 @@ from __future__ import print_function
 import glob
 
 import pygrib
+import numpy as np
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
 from pyiem.plot import MapPlot
 from pyiem import reference
 from pyiem.datatypes import distance
-import numpy as np
 
 
 def main():
     """Go Main"""
     total = None
-    for fn in glob.glob("/tmp/p24m_201710050*"):
+    for fn in glob.glob("/tmp/psnow72_ge4_2018032200f072.grb"):
         grbs = pygrib.open(fn)
         grb = grbs[1]
         if total is None:
@@ -26,11 +29,16 @@ def main():
                  east=reference.MW_EAST, west=reference.MW_WEST,
                  axisbg='tan',
                  title=("Weather Prediction Center (WPC) "
-                        "3 Day Forecasted Precipitation"),
-                 subtitle='7 PM 4 October thru 7 PM 7 October 2017')
-
-    mp.contourf(lons, lats, distance(total, 'MM').value('IN'),
-                np.arange(0, 4.6, 0.5), units='inches')
+                        "Probability of 4+ Inches of Snowfall"),
+                 subtitle='Falling between 7 PM 21 March and 7 PM 24 March 2018')
+    cmap = plt.get_cmap('viridis')
+    cmap.set_bad('tan')
+    cmap.set_under('white')
+    cmap.set_over('red')
+    levs = np.arange(0, 101, 20)
+    levs[0] = 1
+    mp.contourf(lons, lats, total * 100.,
+                levs, cmap=cmap, units='percent')
 
     mp.postprocess(filename='test.png')
 
