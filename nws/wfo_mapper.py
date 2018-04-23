1d6df6257fffbf482006f8dfa1fc64b41a9be027
akrherz <akrherz@iastate.edu>
2018-04-23 13:36:57 -0500

    twitter plot of latest date for first TOR/SVR
    https://twitter.com/akrherz/status/988486766414921728

--- nws/wfo_mapper.py
+++ nws/wfo_mapper.py
@@ -1,6 +1,7 @@
 """Generic plotter"""
 from __future__ import print_function
 import datetime
+from calendar import month_abbr
 
 import numpy as np
 from pandas.io.sql import read_sql
@@ -16,29 +17,66 @@ def get_database_data():
     """Get data from database"""
     pgconn = get_dbconn('postgis')
     df = read_sql("""
-    with data as (
-        select distinct wfo, valid, geom from lsrs_2018 WHERE
-        valid >= '2018-04-01' and type = 'S'
-        )
-    select wfo, count(*) from data GROUP by wfo ORDER by count DESC
+    with mins as (
+        select wfo, extract(year from issue) as year, min(issue)
+        from warnings where issue > '2001-01-01' and
+        phenomena in ('SV', 'TO') and significance = 'W' GROUP by wfo, year),
+    wfos as (
+        select distinct wfo from mins),
+    series as (
+        select wfo, generate_series(2001, 2018, 1) as year, 
+        '1980-12-31'::date from wfos),
+    agg as (
+        select * from mins UNION select * from series),
+    agg2 as (
+        select wfo, year, max(min) from agg GROUP by wfo, year), agg3 as (
+    select wfo, year,
+        case when year = 2018 and extract(year from max) = 1980
+        then 'TODAY'::date else max end as ts from agg2),
+    agg4 as(
+        select wfo, ts, year,
+        rank() OVER (PARTITION by wfo ORDER by to_char(ts, 'mmdd') DESC, year DESC)
+        from agg3)
+
+    select wfo, year, ts, extract(doy from ts) as doy
+    from agg4 WHERE rank = 1 ORDER by wfo
+
     """, pgconn, index_col='wfo')
-    print(df['count'].describe())
-    return df['count'].to_dict()
+    return df
 
 
 def main():
     """Go MAin"""
-    vals = get_database_data()
+    df = get_database_data()
+    vals = {}
+    labels = {}
+    for wfo, row in df.iterrows():
+        if wfo == 'JSJ':
+            wfo = 'SJU'
+        if wfo == 'HUN':
+            vals[wfo] = 68
+            labels[wfo] = "3/9\n2006"
+            continue
+        if row['ts'].year == 2018 and row['ts'].month == 4 and row['ts'].day == 23:
+            vals[wfo] = 400
+            labels[wfo] = "2018"
+            continue
+        if row["ts"].year == 1980:
+            vals[wfo] = -1
+            labels[wfo] = 'None\n%.0f' % (row['year'], )
+            continue
+        vals[wfo] = row['doy']
+        labels[wfo] = row['ts'].strftime("%-m/%-d\n%Y")
 
-    bins = np.arange(0, 900, 100, dtype='i')
-    bins[0] = 1
+    bins = [1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 365]
     cmap = plt.get_cmap('plasma')
     cmap.set_over('black')
     cmap.set_under('white')
-    mp = MapPlot(sector='nws', continentalcolor='white',
-                 title=("April 2018 Number of Local Storm Reports for Snowfall Issued"),
-                 subtitle=('based on unofficial IEM Processing thru 18 April 2018 18 UTC (number of reports not LSR Text Products)'))
-    mp.fill_cwas(vals, bins=bins, lblformat='%.0f', cmap=cmap, ilabel=True,
+    mp = MapPlot(sector='nws', continentalcolor='white', figsize=(12., 9.),
+                 title=("2001-2018 Latest Date of First WFO Severe T'Storm or Tornado Warning"),
+                 subtitle=('based on IEM Archives, Huntsville data to 2003, thru 22 April 2018, 2018 black cells indicate latest date on-going'))
+    mp.fill_cwas(vals, bins=bins, lblformat='%s', labels=labels,
+                 cmap=cmap, ilabel=True, clevlabels=month_abbr[1:],
                  units='Count')
     mp.postprocess(filename='test.png')
 
