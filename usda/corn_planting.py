ae9a1146e0361073b3515009ccdda8df3518e00a
akrherz <akrherz@iastate.edu>
2018-05-02 22:51:32 -0500

    3 May 2018 feature on corn planting

--- usda/corn_planting.py
+++ usda/corn_planting.py
@@ -1,60 +1,74 @@
-import numpy
-import numpy.ma
-import datetime
+"""A corn planting daily feature"""
+from __future__ import print_function
+
+import numpy as np
+import pandas as pd
+from pandas.io.sql import read_sql
 import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import matplotlib.font_manager
-colors = ['red', 'black','green','teal','purple', 'blue']
-fig, ax = plt.subplots(2,1)
-
-progress = numpy.ma.zeros((2013-1979,52), 'f')
-juliandate = numpy.ma.zeros((2013-1979,52), 'f')
-
-lyear = 2013
-x = []
-y = []
-ldiff = []
-for line in open('crop_progress.csv').readlines()[1:]:
-    tokens = line.split(",")
-    ts = datetime.datetime.strptime(tokens[3], '%Y-%m-%d')
-    if ts.year != lyear:
-        if lyear in [1993,2013,2012, 2010,1984,1979,1991]:
-            ax[0].plot( x, y, c=colors.pop(), 
-                     label='%s' % (lyear,), lw=3, zorder=2)
-        else:
-            ax[0].plot( x, y, c='tan')
-        lyear = ts.year
-        y = numpy.array(y)
-        diff = numpy.max( y[1:] - y[0:-1] )
-        ldiff.append( diff )
-        x = []
-        y = []
-    x.append( int(ts.strftime("%j")) )
-    y.append( float(tokens[-1]) )
-        
-ax[0].plot( x, y, c='tan')
-y = numpy.array(y)
-diff = numpy.max( y[1:] - y[0:-1] )
-ldiff.append( diff )
-ldiff = numpy.array(ldiff)
-prop = matplotlib.font_manager.FontProperties(size=12)
+from pyiem.util import get_dbconn
 
 
-    
-ax[0].legend(ncol=2, loc=4, prop=prop)
-ax[0].set_xticks( (91,105,121,135,152,166, 182,213,244,274,305,335,365) )
-ax[0].set_xticklabels( ('Apr 1','Apr 15', 'May 1', 'May 15', 'Jun 1', 'Jun 15', 'Jul 1','Aug','Sep','Oct','Nov','Dec') )
-ax[0].set_xlim(90,170)
-ax[0].grid(True)
-ax[0].set_title("USDA Weekly Crop Progress Report (1979-2014)\nIowa Corn Planting Progress (6 years highlighted)")
-ax[0].set_ylabel("Percent Planted [%]")
+def main():
+    """Go Main Go"""
+    colors = ['red', 'black', 'green', 'teal', 'purple', 'blue']
+    fig, ax = plt.subplots(2, 1, figsize=(8, 6.4))
 
-print len(ldiff)
-ax[1].bar(numpy.arange(1979,2014)-0.4, ldiff[::-1])
-ax[1].set_xlim(1978.5, 2013.5)
-ax[1].set_ylabel("Max Weekly Change [%]")
-ax[1].grid(True)
+    pgconn = get_dbconn('coop')
+    df = read_sql("""
+        select year, week_ending, num_value, state_alpha from nass_quickstats
+        where commodity_desc = 'CORN' and statisticcat_desc = 'PROGRESS'
+        and unit_desc = 'PCT PLANTED' and
+        util_practice_desc = 'ALL UTILIZATION PRACTICES'
+        and num_value is not null and state_alpha = 'IA'
+        ORDER by state_alpha, week_ending
+    """, pgconn, index_col=None)
+    df['week_ending'] = pd.to_datetime(df['week_ending'])
+    df['doy'] = pd.to_numeric(df['week_ending'].dt.strftime("%j"))
+    ldiff = []
+    highyears = [1993, 2018, 2012, 2010, 2017, 1991]
+    sx = []
+    sy = []
+    for year, gdf in df.groupby('year'):
+        y = gdf['num_value'].values
+        deltas = (y[1:] - y[:-1]).tolist()
+        maxval = np.max(deltas)
+        ldiff.append(maxval)
+        sx.append(gdf.iloc[deltas.index(maxval) + 1]['doy'])
+        sy.append(y[deltas.index(maxval) + 1])
+        ax[0].plot(gdf['doy'], y,
+                   lw=3 if year in highyears else 1,
+                   color='tan' if year not in highyears else colors.pop(),
+                   label=str(year) if year in highyears else None,
+                   zorder=3 if year in highyears else 1)
+    ax[0].scatter(sx, sy, c='brown', s=30, zorder=4)
+    ax[0].set_xlabel(("dots represent end of week with "
+                      "largest yearly planting percentage"))
+    prop = matplotlib.font_manager.FontProperties(size=12)
 
-fig.savefig('test.svg')
-import iemplot
-iemplot.makefeature('test')
+    ax[0].legend(ncol=2, loc=4, prop=prop)
+    ax[0].set_xticks([91, 105, 121, 135, 152, 166, 182, 213, 244,
+                      274, 305, 335, 365])
+    ax[0].set_xticklabels(('Apr 1', 'Apr 15', 'May 1', 'May 15', 'Jun 1',
+                           'Jun 15', 'Jul 1', 'Aug', 'Sep', 'Oct',
+                           'Nov', 'Dec'))
+    ax[0].set_xlim(90, 170)
+    ax[0].set_yticks(range(0, 101, 25))
+    ax[0].grid(True)
+    ax[0].set_title(("USDA Weekly Crop Progress Report (1979-2018)\n"
+                     "Iowa Corn Planting Progress (6 years highlighted)"))
+    ax[0].set_ylabel("Percent Planted [%]")
+
+    ax[1].bar(np.arange(1979, 2019),  ldiff)
+    ax[1].set_xlim(1978.5, 2018.5)
+    ax[1].set_ylabel("Max Weekly Change [%]")
+    ax[1].grid(True)
+    ax[1].set_xlabel("valid thru week ending 29 April 2018")
+
+    fig.savefig('test.png')
+
+
+if __name__ == '__main__':
+    main()
