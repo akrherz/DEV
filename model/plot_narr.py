02b1ba6239648a0a8cdb35fd6a0ba14ac9be15a5
akrherz <akrherz@iastate.edu>
2018-05-07 10:21:51 -0500

    offline narr plotter

---
+++ model/plot_narr.py
@@ -0,0 +1,27 @@
+
+import numpy as np
+from pyiem.plot import MapPlot
+from pyiem.datatypes import distance
+from pyiem.util import ncopen
+
+
+def main():
+    """Go Main Go"""
+    nc = ncopen("/mesonet/data/iemre/1979_narr.nc")
+
+    data = np.sum(nc.variables['apcp'][:, :, :], axis=0)
+
+    m = MapPlot(sector='conus', axisbg='tan',
+                title=(""),
+                subtitle='',
+                titlefontsize=16)
+
+    t = distance(data, 'MM').value('IN')
+    m.pcolormesh(nc.variables['lon'][:], nc.variables['lat'][:], t,
+                 np.arange(0, 60, 2), units='F')
+
+    m.postprocess(filename='test.png')
+
+
+if __name__ == '__main__':
+    main()
