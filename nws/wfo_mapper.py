61e5d39b52ac5654e52e995c0d688a297f58f39d
akrherz <akrherz@iastate.edu>
2018-05-07 08:57:31 -0500

    plot SVS to SVR ratio

--- nws/wfo_mapper.py
+++ nws/wfo_mapper.py
@@ -15,15 +15,20 @@ from pyiem.util import get_dbconn
 
 def get_database_data():
     """Get data from database"""
-    pgconn = get_dbconn('afos')
+    pgconn = get_dbconn('postgis')
     df = read_sql("""
-    with data as (
-        select source, count(*) from products WHERE
-        entered > '2015-01-01' and substr(pil, 1, 3) = 'SPS'
-        GROUP by source)
-
-    SELECT substr(source, 2, 3) as wfo, count from data
-    ORDER by count DESC
+    with warns as (
+        SELECT wfo, avg(st_area(geography(geom))) from sbw WHERE
+        issue > '2015-01-01' and phenomena = 'SV' and status = 'NEW'
+        GROUP by wfo ORDER by wfo
+        ),
+    sps as (
+        SELECT substr(product_id, 15, 3) as wfo, avg(st_area(geography(geom)))
+        from text_products where issue > '2015-01-01' GROUP by wfo
+    )
+    SELECT w.wfo, coalesce(s.avg, 0) / w.avg as ratio
+    from warns w LEFT JOIN sps s
+    on (w.wfo = s.wfo) ORDER by ratio DESC
     """, pgconn, index_col='wfo')
     print(df)
     return df
@@ -37,20 +42,20 @@ def main():
     for wfo, row in df.iterrows():
         if wfo == 'JSJ':
             wfo = 'SJU'
-        vals[wfo] = row['count']
-        labels[wfo] = "{:,}".format(row['count'])
-
-    bins = range(0, 6500, 500)
-    bins[0] = 1
+        vals[wfo] = row['ratio']
+        labels[wfo] = "%.1f" % (row['ratio'], )
+        
+    bins = [0.25, 0.5, 0.75, 1., 1.25, 1.5, 1.75, 2., 5, 10]
+    bins[0] = 0.01
     cmap = plt.get_cmap('plasma_r')
     cmap.set_over('black')
     cmap.set_under('white')
     mp = MapPlot(sector='nws', continentalcolor='white', figsize=(12., 9.),
-                 title=("1 Jan 2015 - 24 Apr 2018 Number of Special Weather Statements (SPS) Issued"),
-                 subtitle=('based on unofficial IEM Archives'))
+                 title=("1 Jan 2015 - 24 Apr 2018 Ratio of Average Polygon Size SPS / SVR"),
+                 subtitle=('Special Weather Statement (SPS) Polygon to Severe TStorm Polygon, based on unofficial IEM Archives'))
     mp.fill_cwas(vals, bins=bins, lblformat='%s', labels=labels,
                  cmap=cmap, ilabel=True, # clevlabels=month_abbr[1:],
-                 units='Count')
+                 units='Ratio')
     mp.postprocess(filename='test.png')
 
 
