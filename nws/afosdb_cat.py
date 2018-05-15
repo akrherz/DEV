5b3f11f0023b242027d704c59451397506d3928d
akrherz <akrherz@iastate.edu>
2018-05-15 08:14:30 -0500

    dump flood emergencies

---
+++ nws/afosdb_cat.py
@@ -0,0 +1,24 @@
+"""Send products from AFOS database to pyWWA"""
+
+from tqdm import tqdm
+from pyiem.util import noaaport_text, get_dbconn
+AFOS = get_dbconn('afos')
+acursor = AFOS.cursor()
+
+
+o = open('flood_emergency.txt', 'w')
+for year in tqdm(range(1996, 2018)):
+    for suffix in ['0106', '0712']:
+        table = "products_%s_%s" % (year, suffix)
+        acursor.execute("""
+            SELECT data from """ + table + """
+            WHERE substr(pil, 1, 3) in ('FFW', 'FFS', 'FLW')
+            and data ~* 'EMERGENCY' ORDER by entered ASC
+        """)
+        for row in acursor:
+            raw = " ".join(
+                row[0].upper().replace("\r", "").replace("\n", " ").split())
+            if raw.find("FLASH FLOOD EMERGENCY") == -1:
+                continue
+            o.write(noaaport_text(row[0]))
+o.close()
---
+++ nws/list_emergency.py
@@ -0,0 +1,31 @@
+"""Parse what was dumped into more fun"""
+
+import pandas as pd
+from pyiem.nws.products.vtec import parser
+
+
+def main():
+    """Go Main Go"""
+    data = open('flood_emergency.txt', 'rb').read().decode('ascii')
+    rows = []
+    for report in data.split("\003"):
+        if report == "":
+            continue
+        v = parser(report)
+        data = {'link': ('http://mesonet.agron.iastate.edu/'
+                         'p.php?pid=%s') % (v.get_product_id(), ),
+                'utc_valid': v.valid.strftime("%Y-%m-%d %H:%M"),
+                'source': v.source,
+                'year': v.valid.year}
+        if len(v.segments[0].vtec) == 1:
+            vt = v.segments[0].vtec[0]
+            data['eventid'] = vt.etn
+            data['phenomena'] = vt.phenomena
+            data['significance'] = vt.significance
+        rows.append(data)
+    df = pd.DataFrame(rows)
+    df.to_csv('flood_emergencies.csv')
+
+
+if __name__ == '__main__':
+    main()
