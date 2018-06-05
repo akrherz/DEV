c11de401d2a1bd4af5c28685682288feb38b5708
akrherz <akrherz@iastate.edu>
2018-06-05 12:52:16 -0500

    6 Jun 2018 feature on large month to month changes

--- coop/map_percentile.py
+++ coop/map_percentile.py
@@ -1,36 +1,52 @@
 """Map values"""
 
 from pandas.io.sql import read_sql
-import psycopg2
+from pyiem.util import get_dbconn
 from pyiem.plot import MapPlot
 from pyiem.network import Table as NetworkTable
 
 
 def main():
     """Go Main!"""
-    nt = NetworkTable("WICLIMATE")
-    pgconn = psycopg2.connect(database='coop', host='localhost', port=5555,
-                              user='nobody')
+    nt = NetworkTable("IACLIMATE")
+    pgconn = get_dbconn('coop')
 
     df = read_sql("""
-        SELECT station, max(year) as val from alldata_wi where
-        high >= 100 and substr(station, 3, 1) != 'C'
-        and station != 'IA0000' GROUP by station
+    with monthly as (
+        select station, year, month, avg((high+low)/2.) from alldata_ia
+        WHERE day < '2018-06-01' and high is not null
+        GROUP by station, year, month),
+    agg as (
+        select station, year, month, avg,
+        lag(avg) OVER (PARTITION by station ORDER by year ASC, month ASC)
+        from monthly),
+    agg2 as (
+        select station, year, month, avg, lag, avg - lag as val,
+        rank() OVER (PARTITION by station ORDER by avg - lag DESC)
+        from agg WHERE lag is not null)
+    select * from agg2 where rank = 1 ORDER by station
     """, pgconn, index_col='station')
     df['lat'] = 0.
     df['lon'] = 0.
     for station, _ in df.iterrows():
-        if station in nt.sts:
+        if station in nt.sts and station != 'IA0000' and station[2] != 'C':
             df.at[station, 'lat'] = nt.sts[station]['lat']
             df.at[station, 'lon'] = nt.sts[station]['lon']
 
-    mp = MapPlot(title="Year of Last 100+ Daily High Temperature",
-                 subtitle=('Valid up till 9 June 2017'), sector='state',
-                 state='WI',
+    mp = MapPlot(title="Largest Positive Change in Month to Month Average Temperature",
+                 subtitle=('values in red set record for April to May 2018'), sector='state',
+                 state='IA',
                  drawstates=True, continentalcolor='white')
-    mp.plot_values(df['lon'].values,
-                   df['lat'].values,
-                   df['val'].values, textsize=12, labelbuffer=5, color='k')
+    df2 = df[df['year'] == 2018]
+    mp.plot_values(df2['lon'].values,
+                   df2['lat'].values,
+                   df2['val'].values, fmt='%.1f', textsize=12, labelbuffer=5,
+                   color='r')
+    df2 = df[df['year'] != 2018]
+    mp.plot_values(df2['lon'].values,
+                   df2['lat'].values,
+                   df2['val'].values, fmt='%.1f', textsize=12, labelbuffer=5,
+                   color='b')
     mp.drawcounties()
     mp.postprocess(filename='test.png')
     mp.close()
