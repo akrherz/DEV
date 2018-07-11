09ecbf9009f40b3b1137fe948f6af244ab2247e0
akrherz <akrherz@iastate.edu>
2018-07-11 07:21:41 -0500

    11 Jul 2018 feature on corn silking

--- usda/corn_map.py
+++ usda/corn_map.py
@@ -1,7 +1,7 @@
 """Make a map"""
 from __future__ import print_function
 
-import matplotlib.pyplot as plt
+from pyiem.plot.use_agg import plt
 from pyiem.plot.geoplot import MapPlot
 from pyiem.util import get_dbconn
 import pandas as pd
@@ -14,8 +14,9 @@ def get_data():
     df = read_sql("""
         select year, week_ending, num_value, state_alpha from nass_quickstats
         where commodity_desc = 'CORN' and statisticcat_desc = 'PROGRESS'
-        and unit_desc = 'PCT HARVESTED' and
-        util_practice_desc = 'GRAIN' and num_value is not null
+        and unit_desc = 'PCT SILKING' and
+        util_practice_desc = 'ALL UTILIZATION PRACTICES'
+        and num_value is not null
         ORDER by state_alpha, week_ending
     """, pgconn, index_col=None)
     df['week_ending'] = pd.to_datetime(df['week_ending'])
@@ -24,7 +25,7 @@ def get_data():
         sdf = gdf.copy()
         sdf.set_index('week_ending', inplace=True)
         newdf = sdf.resample('D').interpolate(method='linear')
-        y10 = newdf[newdf['year'] > 2006]
+        y10 = newdf[newdf['year'] > 2007]
         doyavgs = y10.groupby(y10.index.strftime("%m%d")).mean()
         lastdate = pd.Timestamp(newdf.index.values[-1]).to_pydatetime()
         data[state] = {'date': lastdate,
@@ -40,19 +41,22 @@ def main():
     """Go Main Go"""
     data = get_data()
     mp = MapPlot(sector='midwest',
-                 title='5 Nov 2017 USDA NASS Corn Grain Harvest Completion',
-                 subtitle=('Top value is 2017 completion, bottom value is '
-                           'departure from 2007-2016 avg'))
+                 title='8 July 2018 USDA NASS Corn Progress Percent Silking',
+                 subtitle=('Top value is 2018 percentage, bottom value is '
+                           'departure from 2008-2017 avg'))
     data2 = {}
     labels = {}
     for state in data:
         val = data[state]['d2017'] - data[state]['avg']
-        data2[state] = data[state]['d2017']
-        labels[state] = "%.0f%%\n%.1f%%" % (data[state]['d2017'],
-                                            val)
+        data2[state] = val
+        labels[state.encode('utf-8')] = "%i%%\n%s%.1f%%" % (data[state]['d2017'],
+                                          "+" if val > 0 else "", val)
 
-    mp.fill_states(data2, ilabel=True, labels=labels,
-                   cmap=plt.get_cmap('terrain'), units='%')
+    print(labels)
+    levels = range(-40, 41, 10)
+    mp.fill_states(data2, ilabel=True, labels=labels, bins=levels,
+                   cmap=plt.get_cmap('RdBu_r'), units='Absolute %',
+                   labelfontsize=16)
     mp.postprocess(filename='test.png')
     mp.close()
 
