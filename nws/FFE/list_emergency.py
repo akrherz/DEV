1a3c7a6139afc53af95f159d22ad832843663cfc
akrherz <akrherz@iastate.edu>
2018-08-20 12:29:13 -0500

    isolate Flash Flood Emergency code

---
+++ nws/FFE/list_emergency.py
@@ -0,0 +1,40 @@
+"""Parse what was dumped into more fun"""
+
+import pandas as pd
+from pyiem.nws.products.vtec import parser
+
+
+def main():
+    """Go Main Go"""
+    data = open('flood_emergency_filtered.txt', 'rb').read().decode('ascii')
+    rows = []
+    etn = 9000
+    for report in data.split("\003"):
+        if report == "":
+            continue
+        v = parser(report)
+        data = {'link': ('http://mesonet.agron.iastate.edu/'
+                         'p.php?pid=%s') % (v.get_product_id(), ),
+                'utc_valid': v.valid.strftime("%Y-%m-%d %H:%M"),
+                'source': v.source,
+                'phenomena': 'TO', 'significance': 'W', 'eventid': etn,
+                'year': v.valid.year}
+        if v.segments[0].vtec:
+            vt = v.segments[0].vtec[0]
+            if vt.action == 'CAN':
+                print("False Positive?!")
+                pos = report.find("FLASH FLOOD EMERGENCY")
+                print(report[pos-50:pos+50])
+                continue
+            data['eventid'] = vt.etn
+            # data['phenomena'] = vt.phenomena
+            # data['significance'] = vt.significance
+        else:
+            etn += 1
+        rows.append(data)
+    df = pd.DataFrame(rows)
+    df.to_csv('flood_emergencies.csv')
+
+
+if __name__ == '__main__':
+    main()
---
+++ nws/FFE/search_text.py
@@ -0,0 +1,25 @@
+"""Send products from AFOS database to pyWWA"""
+
+from tqdm import tqdm
+from pyiem.util import noaaport_text, get_dbconn
+AFOS = get_dbconn('afos')
+acursor = AFOS.cursor()
+
+
+o = open('flood_emergency.txt', 'a')
+for year in tqdm(range(2018, 2019)):
+    for suffix in ['0106', '0712']:
+        table = "products_%s_%s" % (year, suffix)
+        acursor.execute("""
+            SELECT data, source, entered from """ + table + """
+            WHERE substr(pil, 1, 3) in ('FFW', 'FFS')
+            and data ~* 'EMERGENCY' ORDER by entered ASC
+        """)
+        for row in acursor:
+            raw = " ".join(
+                row[0].upper().replace("\r", "").replace("\n", " ").split())
+            if raw.find("FLASH FLOOD EMERGENCY") == -1:
+                continue
+            o.write(noaaport_text(row[0]))
+            print(" Hit %s %s" % (row[1], row[2]))
+o.close()
