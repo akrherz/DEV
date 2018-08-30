845a384b274129e3907dc61eddd42a49f73a0354
akrherz <akrherz@iastate.edu>
2018-08-30 15:02:45 -0500

    dump more stuff for mdgunn

--- senior_thesis/mdgunn/extract_warnings.py
+++ senior_thesis/mdgunn/extract_warnings.py
@@ -3,11 +3,28 @@
 from pandas.io.sql import read_sql
 from pyiem.util import get_dbconn
 
+def get_misses(pgconn):
+    """Our orig dataset."""
+    return read_sql("""
+        with tornadowarnings as (
+            SELECT wfo, eventid, geom, issue, expire from sbw
+            WHERE phenomena = 'TO'
+            and significance = 'W' and status = 'NEW' and
+            (expire - issue) >= '30 minutes'::interval   
+        ), agg as (SELECT valid, w.wfo, w.eventid, w.issue, w.expire from
+            tornadowarnings w LEFT JOIN lsrs l on (l.type = 'T' and
+            ST_Contains(w.geom, l.geom)
+            and l.valid >= w.issue and l.valid < w.expire and
+            w.wfo = l.wfo and (w.expire - w.issue) >= '30 minutes'::interval)
+            ORDER by w.wfo, w.issue)
+        select wfo, extract(year from issue) as year, count(*) from agg
+        WHERE valid is null GROUP by wfo, year ORDER by wfo, year
+    """, pgconn, index_col=None)
 
-def main():
-    """Go Main Go"""
-    pgconn = get_dbconn('postgis')
-    df = read_sql("""
+
+def get_hits(pgconn):
+    """Our orig dataset."""
+    return read_sql("""
         with tornadowarnings as (
             SELECT wfo, eventid, geom, issue, expire from sbw
             WHERE phenomena = 'TO'
@@ -27,7 +44,13 @@ def main():
         from agg where (first - issue) >= '30 minutes'::interval
         ORDER by issue
     """, pgconn, index_col=None)
-    df.to_csv('events.csv')
+
+
+def main():
+    """Go Main Go"""
+    pgconn = get_dbconn('postgis')
+    df = get_misses(pgconn)
+    df.to_csv('misses.csv')
 
 
 if __name__ == '__main__':
