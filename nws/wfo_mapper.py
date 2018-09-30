dfb644917ad151d83a9d861778a31576c12dac6d
akrherz <akrherz@iastate.edu>
2018-09-30 15:40:20 -0500

    flood warning time
    https://twitter.com/akrherz/status/1046499842564993028

--- nws/wfo_mapper.py
+++ nws/wfo_mapper.py
@@ -15,52 +15,42 @@ def get_database_data():
     """Get data from database"""
     pgconn = get_dbconn('postgis')
     df = read_sql("""
-    WITH data as (
-        select wfo, extract(year from issue) as year, count(*) from sbw
-        WHERE extract(month from issue) = 7 and status = 'NEW' and
-        phenomena in ('TO', 'SV') and significance = 'W'
-        and issue > '2008-01-01' GROUP by wfo, year
-    ), agg as (
-        select wfo, sum(count) / 10. as avg
-        from data where year < 2018 GROUP by wfo
-    ), d2018 as (
-        select wfo, count from data where year = 2018
-    )
-    select a.wfo, a.avg, d.count from
-    agg a LEFT JOIN d2018 d on (a.wfo = d.wfo)
-    """, pgconn, index_col=None)
+    with data as (
+        select distinct wfo,
+        generate_series(issue, expire, '1 minute'::interval) as ts
+        from warnings_2018 where phenomena = 'FL' and significance = 'W')
+    select wfo, count(*) / 391621. * 100. as percent from data
+    GROUP by wfo ORDER by percent DESC
+    """, pgconn, index_col='wfo')
     return df
 
 
 def main():
     """Go Main"""
     df = get_database_data()
-    df.fillna(0, inplace=True)
-    df['diff'] = df['count'] - df['avg']
-    df['percent'] = df['count'] / df['avg'] * 100.
-    df.set_index('wfo', inplace=True)
     print(df)
     vals = {}
     labels = {}
     for wfo, row in df.iterrows():
         if wfo == 'JSJ':
             wfo = 'SJU'
-        vals[wfo] = row['diff']
-        labels[wfo] = '%.1f' % (row['diff'], )
+        vals[wfo] = row['percent']
+        labels[wfo] = '%.0f%%' % (row['percent'], )
         #if row['count'] == 0:
         #    labels[wfo] = '-'
-        
+
+    bins = np.arange(0, 101, 10)    
     #bins = [1, 25, 50, 75, 100, 125, 150, 200, 300]
-    bins = [-50, -25, -10, -5, 0, 5, 10, 25, 50]
+    #bins = [-50, -25, -10, -5, 0, 5, 10, 25, 50]
     # bins[0] = 1
     #clevlabels = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW', 'N']
     cmap = plt.get_cmap('PuOr')
     mp = MapPlot(sector='nws', continentalcolor='white', figsize=(12., 9.),
-                 title=("July 2018 Severe T'Storm + Tornado Warning Count Departure from Average"),
-                 subtitle=('2018 count minus 2008-2017 average warning count, based on IEM archives, CDT Period'))
-    mp.fill_cwas(vals, bins=bins, lblformat='%.0f', labels=labels,
+                 title=("2018 Percentage of Time with 1+ Flood Warning Active"),
+                 subtitle=('1 January - 30 September 2018, based on IEM archives'))
+    mp.fill_cwas(vals, bins=bins, lblformat='%s', labels=labels,
                  cmap=cmap, ilabel=True,  # clevlabels=clevlabels,
-                 units='count difference')
+                 units='percent')
     
     mp.postprocess(filename='test.png')
 
