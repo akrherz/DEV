ab83c15b19d1e2be55ffcfa3781ab859475b5605
akrherz <akrherz@iastate.edu>
2018-11-26 23:11:50 -0600

    revamp blizzard criteria plot, 27 nov 2018 feature

--- asos/blizzard_criteria.py
+++ asos/blizzard_criteria.py
@@ -1,141 +1,200 @@
-# Lets run some diagnostics on blizzard criterion
+"""Lets run some diagnostics on blizzard criterion."""
 
+from tqdm import tqdm
+import numpy as np
+from matplotlib.ticker import FuncFormatter
 import matplotlib.pyplot as plt
-import pylab
-import psycopg2
+from pandas.io.sql import read_sql
 from pyiem.network import Table as NetworkTable
-import numpy
 from pyiem.util import get_dbconn
 
-IEM = get_dbconn('iem')
-icursor = IEM.cursor()
-st = NetworkTable( ('IA_ASOS', 'AWOS') )
 
-bestvsby = {}
-bestsknt = {}
-bs_vsby = []
-bs_sknt = []
-bv_vsby = []
-bv_sknt = []
+def main():
+    """Go Main Go."""
+    pgconn = get_dbconn('iem')
+    postgis = get_dbconn('postgis')
+    nt = NetworkTable(('IA_ASOS', 'AWOS'))
 
-abestvsby = {}
-abestsknt = {}
-abs_vsby = []
-abs_sknt = []
-abv_vsby = []
-abv_sknt = []
-hits = {}
-ahits = {}
+    bestvsby = {}
+    bestsknt = {}
+    bs_vsby = []
+    bs_sknt = []
+    bv_vsby = []
+    bv_sknt = []
 
-for id in st.sts.keys():
-    bestvsby[id] = {'sknt': 0, 'vsby': 100}
-    bestsknt[id] = {'sknt': 0, 'vsby': 100}
-    abestvsby[id] = {'sknt': 0, 'vsby': 100}
-    abestsknt[id] = {'sknt': 0, 'vsby': 100}
-    
-    icursor.execute("""
-    SELECT valid, vsby, sknt from current_log WHERE
-    station = %s and valid > '2011-02-01 12:00' and
-    sknt >= 0 and vsby >= 0 ORDER by valid ASC
-    """, (id,))
-    valid = []
-    sknt = []
-    vsby = []
-    for row in icursor:
-        valid.append( row[0] )
-        sknt.append( row[2] )
-        vsby.append( row[1] )
-    # Now, we process...
-    obs  = len(valid)
-    if obs < 10:
-        continue
-    for i in range(obs):
-        j = i
-        while j < obs and (valid[j] - valid[i]).seconds < (3*3600):
-            j += 1
-        avg_sknt = numpy.average( numpy.array(sknt[i:j]) )
-        min_sknt = min( sknt[i:j] )
-        avg_vsby = numpy.average( numpy.array(vsby[i:j]) )
-        max_vsby = max( vsby[i:j] )
-        if avg_sknt >= 30 and avg_vsby <= 0.25:
-            ahits[id] = True
-        if min_sknt >= 30 and max_vsby <= 0.25:
-            hits[id] = True
-            #print 'Hit %s %s %s %s %s' % (id, valid[i], valid[j-1],
-            #                min_sknt, max_vsby)
-        if min_sknt > bestsknt[id]['sknt']:
-            bestsknt[id]['sknt'] = min_sknt
-            bestsknt[id]['vsby'] = max_vsby
-        if max_vsby < bestvsby[id]['vsby']:
-            bestvsby[id]['sknt'] = min_sknt
-            bestvsby[id]['vsby'] = max_vsby
-            
-        if avg_sknt > abestsknt[id]['sknt']:
-            abestsknt[id]['sknt'] = avg_sknt
-            abestsknt[id]['vsby'] = avg_vsby
-        if avg_vsby < abestvsby[id]['vsby']:
-            abestvsby[id]['sknt'] = avg_sknt
-            abestvsby[id]['vsby'] = avg_vsby
+    abestvsby = {}
+    abestsknt = {}
+    abs_vsby = []
+    abs_sknt = []
+    abv_vsby = []
+    abv_sknt = []
+
+    mbestvsby = {}
+    mbestsknt = {}
+    mbs_vsby = []
+    mbs_sknt = []
+    mbv_vsby = []
+    mbv_sknt = []
+
+    hits = {}
+    ahits = {}
+    mhits = {}
+    for sid in tqdm(nt.sts):
+        df = read_sql("""
+        select w.ugc from warnings_2018 w JOIN ugcs u on (w.gid = u.gid)
+        where substr(w.ugc, 1, 2) = 'IA' and phenomena = 'BZ' and
+        significance = 'W' and issue > '2018-11-20' and
+        st_contains(u.geom, ST_GeomFromEWKT('SRID=4326;POINT(%s %s)'))
+        """, postgis, params=(nt.sts[sid]['lon'], nt.sts[sid]['lat']))
+        if df.empty:
+            continue
+        bestvsby[sid] = {'sknt': 0, 'vsby': 100}
+        bestsknt[sid] = {'sknt': 0, 'vsby': 100}
+        abestvsby[sid] = {'sknt': 0, 'vsby': 100}
+        abestsknt[sid] = {'sknt': 0, 'vsby': 100}
+        mbestvsby[sid] = {'sknt': 0, 'vsby': 100}
+        mbestsknt[sid] = {'sknt': 0, 'vsby': 100}
+
+        df = read_sql("""
+        SELECT valid, vsby, greatest(sknt, gust) as wind
+        from current_log c JOIN stations t
+        ON (c.iemid = t.iemid) WHERE
+        t.id = %s and valid > '2018-11-24 12:00' and
+        sknt >= 0 and vsby >= 0 ORDER by valid ASC
+        """, pgconn, params=(sid,), index_col=None)
+        obs = len(df.index)
+        if obs < 10:
+            print("failed to find enough obs for %s" % (sid, ))
+            continue
+        for i, row in df.iterrows():
+            j = i
+            while (j < obs and
+                    (df.valid[j] - row['valid']).total_seconds() < (3 * 3600)):
+                j += 1
+            avg_sknt = np.average(df.wind.values[i:j])
+            min_sknt = np.min(df.wind.values[i:j])
+            med_sknt = np.median(df.wind.values[i:j])
+            avg_vsby = np.average(df.vsby.values[i:j])
+            max_vsby = np.max(df.vsby.values[i:j])
+            med_vsby = np.median(df.vsby.values[i:j])
+            if avg_sknt >= 30 and avg_vsby <= 0.25:
+                ahits[sid] = True
+            if min_sknt >= 30 and max_vsby <= 0.25:
+                hits[sid] = True
+            if med_sknt >= 30 and med_vsby <= 0.25:
+                mhits[sid] = True
+
+            if min_sknt > bestsknt[sid]['sknt']:
+                bestsknt[sid]['sknt'] = min_sknt
+                bestsknt[sid]['vsby'] = max_vsby
+            if max_vsby < bestvsby[sid]['vsby']:
+                bestvsby[sid]['sknt'] = min_sknt
+                bestvsby[sid]['vsby'] = max_vsby
+
+            if avg_sknt > abestsknt[sid]['sknt']:
+                abestsknt[sid]['sknt'] = avg_sknt
+                abestsknt[sid]['vsby'] = avg_vsby
+            if avg_vsby < abestvsby[sid]['vsby']:
+                abestvsby[sid]['sknt'] = avg_sknt
+                abestvsby[sid]['vsby'] = avg_vsby
+
+            if avg_sknt > mbestsknt[sid]['sknt']:
+                mbestsknt[sid]['sknt'] = med_sknt
+                mbestsknt[sid]['vsby'] = med_vsby
+            if avg_vsby < mbestvsby[sid]['vsby']:
+                mbestvsby[sid]['sknt'] = med_sknt
+                mbestvsby[sid]['vsby'] = med_vsby
+
+        bv_sknt.append(bestvsby[sid]['sknt'])
+        bv_vsby.append(bestvsby[sid]['vsby'])
+        bs_sknt.append(bestsknt[sid]['sknt'])
+        bs_vsby.append(bestsknt[sid]['vsby'])
+        abv_sknt.append(abestvsby[sid]['sknt'])
+        abv_vsby.append(abestvsby[sid]['vsby'])
+        abs_sknt.append(abestsknt[sid]['sknt'])
+        abs_vsby.append(abestsknt[sid]['vsby'])
+
+        mbv_sknt.append(mbestvsby[sid]['sknt'])
+        mbv_vsby.append(mbestvsby[sid]['vsby'])
+        mbs_sknt.append(mbestsknt[sid]['sknt'])
+        mbs_vsby.append(mbestsknt[sid]['vsby'])
+
+    def log_10_product(x, _pos):
+        """The two args are the value and tick position.
+        Label ticks with the product of the exponentiation"""
+        return '%1i' % (x)
+
+    fig = plt.figure(figsize=(9, 6))
+    fig.text(
+        .5, .95, "Iowa ASOS/AWOS 25 November 2018 Blizzard Criteria",
+        ha='center', fontsize=16)
+    ax1 = fig.add_axes([0.075, 0.1, 0.25, 0.8])
+    ax2 = fig.add_axes([0.4, 0.1, 0.25, 0.8])
+    ax3 = fig.add_axes([0.725, 0.1, 0.25, 0.8])
+
+    ax1.set_ylim(0, 45)
+    ax1.set_ylabel("Minimum 3HR Wind Speed/Gust [mph]")
+    ax1.set_xlim(1e-1, 1.2e1)
+    ax1.set_xlabel("Maximum 3HR Visibility [miles]")
+
+    ax2.set_ylabel("Average 3HR Wind Speed/Gust [mph]")
+    ax2.set_ylim(0, 45)
+    ax2.set_xlim(1e-1, 1.2e1)
+    ax2.set_xlabel("Average 3HR Visibility [miles]")
+
+    ax3.set_ylabel("Median 3HR Wind Speed/Gust [mph]")
+    ax3.set_ylim(0, 45)
+    ax3.set_xlim(1e-1, 1.2e1)
+    ax3.set_xlabel("Median 3HR Visibility [miles]")
+
+    ax1.plot([1e-1, 1.2e1], [30 * 1.15, 30 * 1.15], 'g--')
+    ax1.plot([0.25, 0.25], [0, 45], 'g--')
+    ax2.plot([1e-1, 1.2e1], [30 * 1.15, 30 * 1.15], 'g--')
+    ax2.plot([0.25, 0.25], [0, 45], 'g--')
+    ax3.plot([1e-1, 1.2e1], [30 * 1.15, 30 * 1.15], 'g--')
+    ax3.plot([0.25, 0.25], [0, 45], 'g--')
+
+    ax1.scatter(
+        bs_vsby, np.array(bs_sknt) * 1.15, marker='+', color='r',
+        label='Best Wind')
+    ax1.scatter(
+        bv_vsby, np.array(bv_sknt) * 1.15, marker='o', facecolor='w',
+        edgecolor='b', label="Best Vis")
+    ax1.set_xscale('log')
+    ax1.legend()
+
+    ax2.scatter(
+        abs_vsby, np.array(abs_sknt) * 1.15, marker='+', color='r',
+        label='Best Wind')
+    ax2.scatter(
+        abv_vsby, np.array(abv_sknt) * 1.15, marker='o', facecolor='w',
+        edgecolor='b', label='Best Vis')
+    ax2.set_xscale('log')
+    ax2.legend()
+
+    ax3.scatter(
+        mbs_vsby, np.array(mbs_sknt) * 1.15, marker='+', color='r',
+        label='Best Wind')
+    ax3.scatter(
+        mbv_vsby, np.array(mbv_sknt) * 1.15, marker='o', facecolor='w',
+        edgecolor='b', label='Best Vis')
+    ax3.set_xscale('log')
+    ax3.legend()
+
+    formatter = FuncFormatter(log_10_product)
+    ax1.xaxis.set_major_formatter(formatter)
+    ax2.xaxis.set_major_formatter(formatter)
+    ax3.xaxis.set_major_formatter(formatter)
+
+    ax1.set_title(" \nMax/Min Method: %s/%s Sites Hit" % (
+                                len(hits), len(bs_vsby)))
+    ax2.set_title(" \nAverage Method: %s/%s Sites Hit" % (
+                                len(ahits), len(abs_vsby)))
+    ax3.set_title(" \nMedian Method: %s/%s Sites Hit" % (
+                                len(mhits), len(mbs_vsby)))
+
+    fig.savefig('181127.png')
 
 
-    bv_sknt.append( bestvsby[id]['sknt'] )
-    bv_vsby.append( bestvsby[id]['vsby'] )
-    bs_sknt.append( bestsknt[id]['sknt'] )
-    bs_vsby.append( bestsknt[id]['vsby'] )
-    abv_sknt.append( abestvsby[id]['sknt'] )
-    abv_vsby.append( abestvsby[id]['vsby'] )
-    abs_sknt.append( abestsknt[id]['sknt'] )
-    abs_vsby.append( abestsknt[id]['vsby'] )
-
-def log_10_product(x, pos):
-    """The two args are the value and tick position.
-    Label ticks with the product of the exponentiation"""
-    return '%1i' % (x)
-
-
-print 'A hits', len(ahits.keys())
-print 'Hits', len( hits.keys() )
-print hits.keys()
-
-
-fig = plt.figure()
-fig.text(.5,.97, "Iowa ASOS/AWOS 11-12 Dec 2010 Blizzard Criteria", ha='center')
-ax1 = fig.add_subplot(121)
-ax2 = fig.add_subplot(122)
-
-ax1.set_ylim(0,45)
-ax1.set_ylabel("Minimum 3HR Wind Speed [mph]")
-ax2.set_ylabel("Average 3HR Wind Speed [mph]")
-ax2.set_ylim(0,45)
-ax1.set_xlim(1e-1,1.2e1)
-ax2.set_xlim(1e-1,1.2e1)
-ax1.set_xlabel("Maximum 3HR Visibility [miles]")
-ax2.set_xlabel("Average 3HR Visibility [miles]")
-
-ax1.plot([1e-1,1.2e1], [30*1.15,30*1.15], 'g--')
-ax1.plot([0.25,0.25], [0,45], 'g--')
-ax2.plot([1e-1,1.2e1], [30*1.15,30*1.15], 'g--')
-ax2.plot([0.25,0.25], [0,45], 'g--')
-
-ax1.scatter(bs_vsby, numpy.array(bs_sknt) * 1.15, marker='+', color='r', label='Best Wind')
-ax1.scatter(bv_vsby, numpy.array(bv_sknt) * 1.15, marker='o', facecolor='w', edgecolor='b', label="Best Vis")
-ax1.set_xscale('log')
-ax1.legend()
-
-ax2.scatter(abs_vsby, numpy.array(abs_sknt) * 1.15, marker='+', color='r', label='Best Wind')
-ax2.scatter(abv_vsby, numpy.array(abv_sknt) * 1.15, marker='o', facecolor='w', edgecolor='b',label='Best Vis')
-ax2.set_xscale('log')
-ax2.legend()
-
-formatter = pylab.FuncFormatter(log_10_product)
-ax1.xaxis.set_major_formatter(formatter)
-ax2.xaxis.set_major_formatter(formatter)
-
-ax1.set_title(" \nMax/Min Method: %s/%s Sites Hit" % (
-                            len(hits), len(bs_vsby)))
-ax2.set_title(" \nAverage Method: %s/%s Sites Hit" % (
-                            len(ahits), len(abs_vsby)))
-
-fig.savefig('test.png')
-#import iemplot
-#iemplot.makefeature('test')
+if __name__ == '__main__':
+    main()
