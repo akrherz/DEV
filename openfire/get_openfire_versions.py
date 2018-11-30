ea0dcdb24f4b5e697f43a2f297ed903147ea53d5
akrherz <akrherz@iastate.edu>
2018-11-29 21:49:17 -0600

    sundry

--- openfire/get_openfire_versions.py
+++ openfire/get_openfire_versions.py
@@ -1,13 +1,17 @@
+"""Query our version.jsp to see what it returns."""
 import requests
-import time
 
-payload = ("<available>"
-           "</available>")
-# base = "http://openfire:8080/ignite/"
-base = "http://www.igniterealtime.org/"
-for i in range(1800):
+
+def main():
+    """Go Main Go."""
+    payload = "<available></available>"
+    # base = "http://openfire:8080/ignite/"
+    base = "http://www.igniterealtime.org/"
     req = requests.post(base + "projects/openfire/versions.jsp",
                         dict(type='available', query=payload))
     if len(req.content) < 1800:
-        print req.content
-    time.sleep(0.1)
+        print(req.content)
+
+
+if __name__ == '__main__':
+    main()
