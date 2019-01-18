9e104307fec194947d345c72e055e5fb6b5a6e0a
akrherz <akrherz@iastate.edu>
2019-01-17 22:39:15 -0600

    plot snowfall NDFD

--- model/plot_ndfd.py
+++ model/plot_ndfd.py
@@ -3,54 +3,44 @@ from __future__ import print_function
 import datetime
 import subprocess
 
-import pygrib
-from pyiem.plot import MapPlot
 import numpy as np
-import matplotlib.pyplot as plt
+import pygrib
 import cartopy.io.shapereader as shpreader
 import cartopy.crs as ccrs
+from pyiem.plot.use_agg import plt
+from pyiem.plot import MapPlot, nwssnow
 
-WANT = datetime.datetime(2017, 8, 21, 18)  # UTC
 
 
 def main():
     """Go Main"""
-    msgnumber = None
-    subprocess.call(("wget -O ds.sky.bin ftp://tgftp.nws.noaa.gov/"
-                     "SL.us008001/ST.opnl/DF.gr2/DC.ndfd/AR.conus/"
-                     "VP.001-003/ds.sky.bin"
-                     ), shell=True)
-    grbs = pygrib.open('ds.sky.bin')
-    for grb in grbs:
-        if grb.validDate == WANT:
-            msgnumber = grb.messagenumber
+    grbs = pygrib.open('ds.snow.bin')
+    # skip 1-off first field
+    total = None
+    lats = lons = None
+    for grb in grbs[1:]:
+        if lats is None:
+            lats, lons = grb.latlons()
+            total = grb['values']
+            continue
+        total += grb['values']
+    analtime = grb.analDate - datetime.timedelta(hours=6)
 
-    print("Message number is %s" % (msgnumber, ))
-    grb = grbs[msgnumber]
-    analtime = grb.analDate - datetime.timedelta(hours=5)
-    lats, lons = grb.latlons()
+    mp = MapPlot(sector='custom', west=-100, east=-68, north=50, south=32,
+                 axisbg='tan',
+                 title=("NWS Forecasted Accumulated Snowfall "
+                        "thru 8 PM 21 Jan 2019"),
+                 subtitle='NDFD Forecast Issued %s' % (analtime.strftime("%-I %p %-d %B %Y"), ))
+    cmap = nwssnow()
+    cmap.set_bad('tan')
+    mp.pcolormesh(
+        lons, lats, total * 39.3701,
+        [0.01, 1, 2, 3, 4, 6, 8, 12, 18, 24, 30, 36],
+        cmap=cmap,
+        units='inch')
 
-    for sector in ['conus', 'midwest']:
-        mp = MapPlot(sector=sector,
-                     axisbg='tan',
-                     title=("NWS Forecasted Sky Cover [%] "
-                            "for 1 PM CDT 21 August 2017"),
-                     subtitle=('NDFD Forecast issued %s, Red '
-                               'swath is path of eclipse totality'
-                               ) % (analtime.strftime("%-I %p %-d %B %Y")))
-
-        mp.pcolormesh(lons, lats, grb['values'],
-                      np.arange(0, 101, 25.), cmap=plt.get_cmap('binary_r'),
-                      units='Sky Coverage %')
-
-        shp = shpreader.Reader('upath17.shp')
-        mp.ax.add_geometries(shp.geometries(), ccrs.PlateCarree(),
-                             facecolor='None', edgecolor='r', zorder=100)
-
-        mp.postprocess(filename='test.png')
-        mp.close()
-        subprocess.call(("scp test.png mesonet@mesonet:/mesonet/share/pickup/"
-                         "eclipse_%s.png") % (sector, ), shell=True)
+    mp.postprocess(filename='test.png')
+    mp.close()
 
 
 if __name__ == '__main__':
