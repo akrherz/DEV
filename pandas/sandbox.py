577f9dfc94d8bd12e084c9a4d5487dfa1a4c24c9
akrherz <akrherz@iastate.edu>
2019-03-14 13:32:48 -0500

    cleanup soon

--- pandas/sandbox.py
+++ pandas/sandbox.py
@@ -1,26 +1,20 @@
-import psycopg2
-from pandas.io.sql import read_sql
-import pandas as pd
-import calendar
-import datetime
+"""We Play."""
+
 import pytz
-import numpy as np
-from geopandas import GeoDataFrame
-from rasterio import features
-from pyiem import reference
-import requests
+from pandas.io.sql import read_sql
+from pyiem.util import get_dbconn
 
-basets = datetime.datetime(2016, 11, 17).replace(tzinfo=pytz.utc)
-x = [basets + datetime.timedelta(seconds=i/20.) for i in range(10000)]
 
-df = pd.DataFrame({'valid': x})
+def main():
+    """Go Main Go."""
+    pgconn = get_dbconn('asos')
+    df = read_sql("""
+    SELECT valid at time zone 'UTC' as valid2 from alldata LIMIT 10
+    """, pgconn, index_col=None)
+    df['valid2'] = df['valid2'].dt.tz_localize(pytz.UTC)
+    df['valid'] = df['valid2'].dt.tz_convert(pytz.timezone("America/Chicago"))
+    print(df)
 
-#df['delta'] = (df['valid'] - basets) / np.timedelta64(1, 's')
-#df['sample'] = ((df['delta'] * 100) - (df['delta'].astype('i') * 100)) / 5
 
-for i, row in df.iterrows():
-    delta = (row['valid'] - basets).total_seconds()
-    tm = int(delta)
-    sample = int((delta * 100) - (tm * 100)) / 5
-
-#print df
+if __name__ == '__main__':
+    main()
