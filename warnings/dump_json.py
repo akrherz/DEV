be19f5b14c24d97a77a1a9eefe2d99940ad87b60
akrherz <akrherz@iastate.edu>
2019-03-29 13:32:49 -0500

    ugly one-off

---
+++ warnings/dump_json.py
@@ -0,0 +1,15 @@
+from pandas.io.sql import read_sql
+from pyiem.util import get_dbconn
+
+pgconn = get_dbconn('postgis')
+df = read_sql("""
+    SELECT row_number() OVER () as id,
+    wfo as issuing_wfo,
+    to_char(issue, 'YYYY-mm-dd') as startdate,
+    to_char(expire, 'YYYY-mm-dd') as endate,
+    ugc,
+    report as forecast_blob from warnings where phenomena = 'FW'
+    and significance = 'W'
+""", pgconn)
+
+df.to_json('fire_weather.json', orient='records')
