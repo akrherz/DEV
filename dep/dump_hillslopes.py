796bbd627c0fdc4e2fdb12fa69803320c4f12b67
akrherz <akrherz@iastate.edu>
2019-04-08 14:19:34 -0500

    more dumping

---
+++ dep/dump_hillslopes.py
@@ -0,0 +1,18 @@
+"""Shrug, just do as I am told."""
+
+from geopandas import read_postgis
+from pyiem.util import get_dbconn
+
+
+def main():
+    """Go Main Go."""
+    pgconn = get_dbconn('idep')
+    df = read_postgis("""
+        SELECT st_transform(geom, 4326) as geom, huc_12, fpath
+        from flowpaths WHERE scenario = 0
+    """, pgconn)
+    df.to_file("flowpaths.shp")
+
+
+if __name__ == '__main__':
+    main()
--- dep/dump_points.py
+++ dep/dump_points.py
@@ -3,6 +3,7 @@
 from pandas.io.sql import read_sql
 from pyiem.util import get_dbconn
 
+
 def main():
     """Go Main Go."""
     pgconn = get_dbconn('idep')
@@ -11,7 +12,7 @@ def main():
         st_y(st_centroid(st_transform(st_geometryn(geom, 1), 4326))), huc_12
         from flowpaths WHERE scenario = 0
     """, pgconn)
-    for i, row in df.iterrows():
+    for _i, row in df.iterrows():
         if row['st_y'] > 39.78 and row['st_y'] < 43.7 and row['st_x'] < -94:
             print("%.4f,%.4f,%s" % (row['st_x'], row['st_y'], row['huc_12']))
 
