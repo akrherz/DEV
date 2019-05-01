9520a1de970ce33cbcc9d7731339c36d2b04112d
akrherz <akrherz@iastate.edu>
2019-05-01 14:23:27 -0500

    add updated col correction
    see akrherz/iem#183

---
+++ nws/correct_updated.py
@@ -0,0 +1,50 @@
+"""See akrherz/iem#183"""
+import sys
+
+from pyiem.util import get_dbconn, noaaport_text
+from pyiem.nws.products.vtec import parser
+
+
+def main(argv):
+    """Go Main Go."""
+    pgconn = get_dbconn('postgis')
+    cursor = pgconn.cursor('streamer')
+    cursor2 = pgconn.cursor()
+
+    table = "warnings_%s" % (argv[1],)
+
+    cursor.execute("""
+        SELECT oid, report, svs, updated from """+table+"""
+        WHERE svs is not null ORDER by issue
+    """)
+    considered = 0
+    updated = 0
+    for row in cursor:
+        considered += 1
+        oid = row[0]
+        # report = row[1]
+        svss = row[2]
+        baseupdated = row[3]
+        for svs in svss.split("__")[::-1]:
+            if svs.strip() == '':
+                continue
+            try:
+                prod = parser(noaaport_text(svs))
+            except Exception as exp:
+                print(exp)
+                sys.exit()
+            if prod.valid != baseupdated:
+                print("old: %s new: %s" % (baseupdated, prod.valid))
+                updated += 1
+                cursor2.execute("""
+                    UPDATE """ + table + """ SET updated = %s WHERE oid = %s
+                """, (prod.valid, oid))
+            break
+    print("done updated %s rows of %s candidates" % (updated, considered))
+    cursor2.close()
+    pgconn.commit()
+    pgconn.close()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
