0a7a68193f911b62545336bd21586552acdd0ccf
akrherz <akrherz@iastate.edu>
2019-05-20 09:09:01 -0500

    modernize

--- basemap/make_pyiem_bounds.py
+++ basemap/make_pyiem_bounds.py
@@ -1,43 +1,53 @@
-from pyiem.plot import MapPlot, mask_outside_polygon
+"""Generate the _ccw files used in pyIEM."""
 from shapely.wkb import loads
 from descartes.patch import PolygonPatch
-from shapely.geometry import Polygon
-import psycopg2
+from shapely.geometry import Polygon, MultiPolygon
 import numpy as np
-DBCONN = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
-cursor = DBCONN.cursor()
+from pyiem.plot import MapPlot, mask_outside_polygon
+from pyiem.util import get_dbconn
 
-cursor.execute("""SELECT ST_asEWKB(
+
+def main():
+    """Go Main Go."""
+    DBCONN = get_dbconn('postgis')
+    cursor = DBCONN.cursor()
+
+    cursor.execute("""
+        SELECT
             ST_Transform(
                 ST_Simplify(
                     ST_Union(ST_Transform(the_geom,2163)),
                     500.),
-                4326)) from states 
-        WHERE state_abbr = 'IA'
----    where state_abbr in ('MI','WI', 'IL', 'IN', 'OH', 'KY', 'MO', 'KS',
----    'NE', 'SD', 'ND', 'MN', 'IA')
- """)
+                4326) from states
+        WHERE state_abbr in ('IA', 'IL', 'IN')
+    ---    where state_abbr in ('MI','WI', 'IL', 'IN', 'OH', 'KY', 'MO', 'KS',
+    ---    'NE', 'SD', 'ND', 'MN', 'IA')
+    """)
 
-m = MapPlot('iowa')
-lons = np.linspace(-110,-70,50)
-lats = np.linspace(28,52,50)
-vals = np.linspace(0,50,50)
-m.contourf(lons,lats,vals, vals)
+    m = MapPlot('iowa')
+    lons = np.linspace(-110, -70, 50)
+    lats = np.linspace(28, 52, 50)
+    vals = np.linspace(0, 50, 50)
+    m.contourf(lons, lats, vals, vals)
 
-#"""
-for row in cursor:
-    multipoly = loads( str(row[0]) )
-    for geo in multipoly.geoms:
-        if geo.area < 1:
-            continue
-        (lons,lats) = geo.exterior.xy
-        print 'Masking with geo...', geo.area, len(lons)
+    #"""
+    for row in cursor:
+        multipoly = MultiPolygon([loads(row[0], hex=True)])
+        for geo in multipoly.geoms:
+            if geo.area < 1:
+                continue
+            (lons, lats) = geo.exterior.xy
+            print('Masking with geo... %s %s' % (geo.area, len(lons)))
 
-        ar = zip(lons, lats)
-        ar.reverse()
-        np.save('iowa_ccw.npy', ar)
-        #mask_outside_polygon(ar, ax=m.ax)
-        #poly = PolygonPatch(Polygon(zip(x,y)), fc='r', zorder=10000)
-        #m.ax.add_patch(poly)
-#""" 
-m.postprocess(filename='test.png')
+            ar = list(zip(lons, lats))
+            ar.reverse()
+            np.save('iailin_ccw.npy', ar)
+            # mask_outside_polygon(ar, ax=m.ax)
+            poly = PolygonPatch(Polygon(ar), fc='r', zorder=10000)
+            m.ax.add_patch(poly)
+    #""" 
+    m.postprocess(filename='test.png')
+
+
+if __name__ == '__main__':
+    main()
