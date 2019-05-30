8c6d2c925e805fea2d0b73a2b9f5f0ab0a87c841
akrherz <akrherz@iastate.edu>
2019-05-30 10:36:50 -0500

    adjust example ffmpeg

--- frames/images/gen.sh
+++ frames/images/gen.sh
@@ -1,3 +1,3 @@
-ffmpeg -i %05d.png -vcodec libx264 -r 10 -pix_fmt yuv420p out.mp4
+ffmpeg -r 4 -i %05d.gif.png -vcodec libx264 -pix_fmt yuv420p out.mp4
 # needed this for the larger 4panel plots
 # ffmpeg -i %05d.png -s:v 1280x720 -vcodec libx264 -r 10 -pix_fmt yuv420p out.mp4
