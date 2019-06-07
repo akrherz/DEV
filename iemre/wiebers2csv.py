701a777322760efb761ebdce5f8346093d67986b
akrherz <akrherz@iastate.edu>
2019-06-07 13:05:25 -0500

    abh

---
+++ iemre/wiebers2csv.py
@@ -0,0 +1,23 @@
+"""A util script to dump IEMRE json service to csv files"""
+
+import requests
+import pandas as pd
+import geopandas as gpd
+
+def main():
+    """Go Main Go"""
+    df = gpd.read_file('/tmp/sample_centroid.shp')
+    res = []
+    for _i, row in df.iterrows():
+        uri = ("http://mesonet.agron.iastate.edu/iemre/multiday/"
+               "2018-01-01/2018-12-31/%s/%s/json"
+               ) % (row['geometry'].y, row['geometry'].x)
+        req = requests.get(uri)
+        for row2 in req.json()['data']:
+            res.append(row2)
+        df2 = pd.DataFrame(res)
+        df2.to_csv("%s.csv" % (row['SITE'],), index=False)
+
+
+if __name__ == '__main__':
+    main()
