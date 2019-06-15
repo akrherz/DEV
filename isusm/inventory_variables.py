4feca206a0215d97fa71b272e24b0c1f9bc7007b
akrherz <akrherz@iastate.edu>
2019-06-14 22:11:31 -0500

    add inventory script

---
+++ isusm/inventory_variables.py
@@ -0,0 +1,34 @@
+"""Figure out how much of a variable name mess we have."""
+import glob
+import sys
+import os
+
+
+def runtype(name):
+    """Run for a given filetime."""
+    os.chdir("/mnt/home/loggernet")
+    cols = {}
+    for fn in glob.glob("*%s.dat" % (name, )):
+        station = fn.split("_")[0]
+        for linenum, line in enumerate(open(fn, 'rb').readlines()):
+            if linenum == 1:
+                for token in line.decode(
+                        'ascii', 'ignore').strip().replace('"', "").split(","):
+                    d = cols.setdefault(token, [])
+                    d.append(station)
+            if linenum > 1:
+                break
+            linenum += 1
+    keys = list(cols.keys())
+    keys.sort()
+    for key in keys:
+        print("%s\n  %s" % (key, " ".join(cols[key])))
+
+
+def main(argv):
+    """Go Main Go."""
+    runtype(argv[1])
+
+
+if __name__ == '__main__':
+    main(sys.argv)
