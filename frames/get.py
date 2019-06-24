941bfc3a37d8cf8cb330d5b6931560ca8b78bce7
akrherz <akrherz@iastate.edu>
2019-06-23 22:40:27 -0500

    update for sat download

--- frames/get.py
+++ frames/get.py
@@ -1,7 +1,5 @@
 """Lapse"""
-from __future__ import print_function
 import datetime
-import os
 
 import requests
 
@@ -9,23 +7,42 @@ import requests
 def main():
     """Go Main"""
     # Generate series of images between 0z and 12z on the 3rd of August
-    now = datetime.datetime(2017, 6, 12, 20, 0)
-    ets = datetime.datetime(2017, 6, 13, 2, 0)
-    interval = datetime.timedelta(minutes=1)
+    now = datetime.datetime(2019, 4, 21, 0, 0)
+    ets = datetime.datetime(2019, 6, 24, 0, 0)
+    interval = datetime.timedelta(minutes=1440)
 
-    baseuri = "http://mesonet.agron.iastate.edu/GIS/radmap.php?"
-    baseuri += ("width=720&height=486&layers[]=ridge&ridge_radar=DVN&"
-                "ridge_product=N0Q&bbox=-91.6,41.2,-88.6,41.3&"
-                "layers[]=uscounties")
 
     stepi = 0
     while now < ets:
-        url = "%s&ts=%s" % (baseuri, now.strftime("%Y%m%d%H%M"))
-        req = requests.get(url)
-        output = open('images/%05i.png' % (stepi, ), 'wb')
-        output.write(req.content)
-        output.close()
-        stepi += 1
+        uri = now.strftime((
+            "http://mtarchive.geol.iastate.edu/%Y/%m/%d/cod/sat/goes16/"
+            "global/fulldiskeast/abi08/000index.txt"
+        ))
+        req = requests.get(uri)
+        if req.status_code != 200:
+            print(uri)
+            continue
+        fns = [None]*24
+        for line in req.content.decode('ascii').split("\n"):
+            tokens = line.split("_")
+            if len(tokens) != 3:
+                continue
+            hr = int(tokens[2][8:10])
+            if fns[hr] is None:
+                fns[hr] = line
+        for fn in fns:
+            if fn is None:
+                continue
+            uri = now.strftime((
+                "http://mtarchive.geol.iastate.edu/%Y/%m/%d/cod/sat/goes16/"
+                "global/fulldiskeast/abi08/" + fn
+            ))
+            req = requests.get(uri)
+            fp = open('images/%05i.jpg' % (stepi, ), 'wb')
+            fp.write(req.content)
+            fp.close()
+            stepi += 1
+
         now += interval
 
 
