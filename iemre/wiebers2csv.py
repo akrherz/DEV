6dbb88187c515eaebe9396b33eb4f626ef70e0f7
akrherz <akrherz@iastate.edu>
2019-06-25 08:42:55 -0500

    iterate as per request

--- iemre/wiebers2csv.py
+++ iemre/wiebers2csv.py
@@ -4,19 +4,21 @@ import requests
 import pandas as pd
 import geopandas as gpd
 
+
 def main():
     """Go Main Go"""
-    df = gpd.read_file('/tmp/sample_centroid.shp')
-    res = []
+    df = gpd.read_file('/tmp/AFREC_2018_centroid_id.shp')
     for _i, row in df.iterrows():
         uri = ("http://mesonet.agron.iastate.edu/iemre/multiday/"
                "2018-01-01/2018-12-31/%s/%s/json"
                ) % (row['geometry'].y, row['geometry'].x)
         req = requests.get(uri)
+        res = []
         for row2 in req.json()['data']:
             res.append(row2)
         df2 = pd.DataFrame(res)
-        df2.to_csv("%s.csv" % (row['SITE'],), index=False)
+        df2['ID'] = row['ID']
+        df2.to_csv("%s.csv" % (row['ID'],), index=False)
 
 
 if __name__ == '__main__':
