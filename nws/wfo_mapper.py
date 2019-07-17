0e847369e725aea8e788543ecbebfe2c822bdde5
akrherz <akrherz@iastate.edu>
2019-07-17 13:26:18 -0500

    twitter plot of spc MDT risks per WFO
    https://twitter.com/akrherz/status/1151558718976397313

--- nws/wfo_mapper.py
+++ nws/wfo_mapper.py
@@ -4,8 +4,10 @@ import datetime
 from calendar import month_abbr
 
 import numpy as np
+import pandas as pd
 from pandas.io.sql import read_sql
 from pyiem.plot.use_agg import plt
+from tqdm import tqdm
 from pyiem.plot import MapPlot
 from pyiem.network import Table as NetworkTable
 from pyiem.util import get_dbconn, drct2text
@@ -13,52 +15,51 @@ from pyiem.util import get_dbconn, drct2text
 
 def get_database_data():
     """Get data from database"""
+    return pd.read_csv('data.csv', index_col='wfo')
+    data = {'wfo': [], 'days': []}
+    nt = NetworkTable("WFO")
     pgconn = get_dbconn('postgis')
-    df = read_sql("""
-    with data as (
-        select wfo, count(*) / 10. as cnt
-        from sbw WHERE phenomena in ('TO') and significance = 'W'
-        and status = 'NEW' and issue > '2009-01-01' and issue < '2019-01-01'
-        and to_char(issue, 'mmdd') < '0523'
-        GROUP by wfo),
-    d2019 as (
-        select wfo, count(*)
-        from sbw_2019 WHERE phenomena in ('TO') and significance = 'W'
-        and status = 'NEW'
-        GROUP by wfo)
+    cursor = pgconn.cursor()
+    for sid in tqdm(nt.sts):
+        cursor.execute("""
+            select distinct date(expire - '1 day'::interval)
+            from spc_outlooks o, ugcs c where day = 1 and threshold = 'MDT'
+            and ST_Intersects(c.geom, o.geom) and c.wfo = %s
+            and c.end_ts is null
+        """, (sid, ))
+        data['wfo'].append(sid)
+        data['days'].append(cursor.rowcount)
 
-    SELECT d.wfo, coalesce(t.count, 0) / d.cnt * 100. as percent from
-    data d LEFT JOIN d2019 t on (d.wfo = t.wfo)
-    """, pgconn, index_col='wfo')
+    df = pd.DataFrame(data)
+    df.to_csv('data.csv')
     return df
 
 
 def main():
     """Go Main"""
     df = get_database_data()
-    print(df)
     vals = {}
     labels = {}
     for wfo, row in df.iterrows():
         if wfo == 'JSJ':
             wfo = 'SJU'
-        vals[wfo] = row['percent']
-        labels[wfo] = '%.0f%%' % (row['percent'], )
+        vals[wfo] = row['days']
+        labels[wfo] = '%.0f' % (row['days'], )
         #if row['count'] == 0:
         #    labels[wfo] = '-'
 
     # bins = np.arange(0, 101, 10)    
-    bins = [1, 25, 50, 75, 100, 125, 150, 200, 300]
+    bins = [1, 2, 5, 10, 20, 30, 50, 75, 100, 125, 150, 175]
     # bins = [-50, -25, -10, -5, 0, 5, 10, 25, 50]
     # bins[0] = 1
     # clevlabels = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW', 'N']
-    cmap = plt.get_cmap('PuOr')
-    mp = MapPlot(sector='nws', continentalcolor='white', figsize=(12., 9.),
-                 title=("2019 YTD Issued Tornado Warnings Departure from Average"),
-                 subtitle=('1 Jan till 12 AM 23 May 2019 vs 2009-2018, based on IEM archives'))
+    cmap = plt.get_cmap('magma')
+    mp = MapPlot(sector='conus', continentalcolor='white', figsize=(12., 9.),
+                 title=("2002-2019 SPC Day 1 Moderate Convective Risk Days by WFO"),
+                 subtitle=('till 17 Jul 2019, based on unofficial IEM Archives of any part of risk area overlapping CWA'))
     mp.fill_cwas(vals, bins=bins, lblformat='%s', labels=labels,
                  cmap=cmap, ilabel=True,  # clevlabels=clevlabels,
-                 units='percent')
+                 units='days')
     
     mp.postprocess(filename='test.png')
 
