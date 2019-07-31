8b16f1587a1c63e97f51d38210fcdc34dd0f041c
akrherz <akrherz@iastate.edu>
2019-07-31 14:38:44 -0500

    another one-off dumper

---
+++ asos/dump_wind.py
@@ -0,0 +1,38 @@
+"""Dump ASOS wind data per year."""
+import sys
+
+from pyiem.util import get_dbconn
+from tqdm import tqdm
+
+
+def main(argv):
+    """Go Main Go."""
+    year = int(argv[1])
+    pgconn = get_dbconn('asos')
+    cursor = pgconn.cursor("streamer")
+    cursor.execute("""
+        SELECT
+        to_char(valid at time zone 'UTC', 'YYYYmmddHH24MI') as utc_valid,
+        id, ST_x(geom) as lon, ST_y(geom) as lat,
+        sknt as wind_speed_kts, drct as wind_direction,
+        gust as wind_gust_kts, peak_wind_gust as peak_wind_gust_kts,
+        peak_wind_drct as peak_wind_drct,
+        to_char(peak_wind_time at time zone 'UTC', 'YYYYmmddHH24MI')
+          as utc_peak_wind_time
+        from t""" + str(year) + """ d, stations t
+        WHERE t.id = d.station and (t.network ~* 'ASOS' or t.network = 'AWOS')
+        and report_type = 2 and country = 'US' and
+        network not in ('AK_ASOS', 'HI_ASOS', 'PR_ASOS')
+    """)
+    with open("%s.csv" % (year, ), 'w') as fp:
+        fp.write((
+            "utc_valid,id,lon,lat,wind_speed_kts,wind_direction,"
+            "wind_gust_kts,peak_wind_gust_kts,peak_wind_drct,"
+            "utc_peak_wind_time\n"
+        ))
+        for row in tqdm(cursor):
+            fp.write(",".join([str(s) for s in row]) + "\n")
+
+
+if __name__ == '__main__':
+    main(sys.argv)
