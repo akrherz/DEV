25d93665bb57657501e48ba226bb85ccb1b35eb5
akrherz <akrherz@iastate.edu>
2019-08-06 09:39:56 -0500

    add Cow API example

---
+++ cow/api2shapefile.py
@@ -0,0 +1,28 @@
+"""Example using the Cow API to generate a shapefile."""
+from io import BytesIO
+import json
+
+import requests
+import geopandas as gpd
+
+
+def main():
+    """Our main method."""
+    url = (
+        "https://mesonet.agron.iastate.edu/api/1/cow.json?wfo=DMX&"
+        "begints=2019-01-01T00:00Z&endts=2019-08-06T00:00Z&"
+        "phenomena=TO&lsrtype=TO&"
+    )
+    reg = requests.get(url)
+    jsobj = reg.json()
+    for name in ['events', 'stormreports']:
+        # This feels hacky, but I did not find another means yet to do it
+        buf = BytesIO()
+        buf.write(json.dumps(jsobj[name]).encode("ASCII"))
+        buf.seek(0)
+        events = gpd.read_file(buf)
+        events.to_file(name + ".shp")
+
+
+if __name__ == '__main__':
+    main()
