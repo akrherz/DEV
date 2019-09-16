6bd7d345c5cb160bb05ee422328ce1350eff5710
akrherz <akrherz@iastate.edu>
2019-09-15 21:38:14 -0500

    squawkeye football game plot
    https://twitter.com/akrherz/status/1173423912396439552

--- nldn/jack_trice_strikes.py
+++ nldn/jack_trice_strikes.py
@@ -10,11 +10,11 @@ from pyiem.plot.use_agg import plt
 
 def get_data():
     """Fetch."""
-    pgconn = get_dbconn('postgis')
+    pgconn = get_dbconn('nldn')
     df = read_sql("""
         select distinct date_trunc('minute', valid) as dt from
-        nldn2018_09 where valid > '2018-09-01 17:00'
-        and st_distance(
+        nldn2019_09 where valid > '2019-09-14 15:00' and
+        valid < '2019-09-14 21:00' and st_distance(
             ST_GeomFromEWKT('SRID=4326;POINT(-93.6379 42.0140)')::geography,
             geom::geography) < 12874.8 ORDER by dt ASC
     """, pgconn, index_col=None)
@@ -23,14 +23,14 @@ def get_data():
 
 def main():
     """Go Main Go."""
-    # get_data()
+    get_data()
     df = pd.read_csv('data.csv')
     df['dt'] = pd.to_datetime(df['dt']) - datetime.timedelta(hours=5)
     df['s'] = df['dt'].dt.strftime("%Y%m%d%H%M")
     x = []
     y = []
-    sts = datetime.datetime(2018, 9, 1, 19)
-    ets = datetime.datetime(2018, 9, 2, 2, 1)
+    sts = datetime.datetime(2019, 9, 14, 15)
+    ets = datetime.datetime(2019, 9, 14, 21)
     interval = datetime.timedelta(minutes=1)
     now = sts
     current = None
@@ -50,16 +50,25 @@ def main():
         now += interval
 
     (fig, ax) = plt.subplots(1, 1)
-    ax.plot(x, y)
+    ax.plot(x, y, zorder=6)
     ax.set_xticks(xticks)
     ax.set_xticklabels(xticklabels)
-    ax.set_yticks(list(range(0,91, 15)))
-    ax.set_ylabel("Minutes since Last Lightning Strike")
-    ax.set_xlabel("Evening of 1 September 2018 (CDT)")
+    ax.set_yticks(list(range(0, 91, 15)))
+    ax.set_ylabel(
+        "Minutes since Last Lightning Strike")
+    ax.set_xlabel(
+        "Evening of 14 September 2019 (CDT), approximate game delays shaded")
     ax.axhline(30, lw=2, color='r')
+    ax.set_ylim(bottom=-0.1)
+    ax.axvspan(
+        datetime.datetime(2019, 9, 14, 15, 29),
+        datetime.datetime(2019, 9, 14, 16, 18), color='k', alpha=0.4, zorder=2)
+    ax.axvspan(
+        datetime.datetime(2019, 9, 14, 16, 45),
+        datetime.datetime(2019, 9, 14, 18, 52), color='k', alpha=0.4, zorder=2)
     ax.grid(True)
     ax.set_title((
-        "Iowa State vs South Dakota State Football Game\n"
+        "Iowa State vs Iowa Football Game\n"
         "Time since Last Lightning Strike within 8 miles of Jack Trice Stadium\n"
         "Data courtesy of National Lightning Detection Network"))
     fig.savefig('test.png')
