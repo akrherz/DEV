6c5a6bb89b34cfd5468451f6a572148df2739a79
akrherz <akrherz@iastate.edu>
2019-10-03 13:58:47 -0500

    add simple trouble sounding exerciser

---
+++ metpy/trouble_sounding.py
@@ -0,0 +1,17 @@
+import datetime
+
+import pandas as pd
+from siphon.simplewebservice.iastate import IAStateUpperAir
+from metpy.calc import precipitable_water, surface_based_cape_cin
+from metpy.units import units
+
+res = IAStateUpperAir.request_data(datetime.datetime(2018, 4, 7, 17), 'KXMR')
+# pw = precipitable_water(res['dewpoint'].values * units.degC, res['pressure'].values * units('hPa'))
+# print(pw)
+filtered = res[pd.notnull(res['dewpoint'])]
+# pw = precipitable_water(filtered['dewpoint'].values * units.degC, filtered['pressure'].values * units('hPa'))
+# print(pw)
+(sbcape, sbcin) = surface_based_cape_cin(
+        filtered['pressure'].values * units.hPa,
+        filtered['temperature'].values * units.degC,
+        filtered['dewpoint'].values * units.degC)
