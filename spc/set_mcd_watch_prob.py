eca2461d476fbd8c911e7967eb366f7d102a0eaf
akrherz <akrherz@iastate.edu>
2019-10-21 09:54:11 -0500

    add backfill tool of MCD watch probs
    akrherz/iem#211

---
+++ spc/set_mcd_watch_prob.py
@@ -0,0 +1,35 @@
+"""Update database storage of watch_confidence."""
+import sys
+
+from pyiem.nws.products import mcd
+from pyiem.util import get_dbconn, noaaport_text
+
+
+def main(argv):
+    """Go Main Go."""
+    pgconn = get_dbconn('postgis')
+    cursor = pgconn.cursor()
+    cursor2 = pgconn.cursor()
+    cursor.execute("""
+        SELECT product_id, product, num from mcd
+        where watch_confidence is null and year = %s
+        ORDER by num ASC
+    """, (int(argv[1]), ))
+    for row in cursor:
+        try:
+            prod = mcd.parser(noaaport_text(row[1]))
+        except:
+            print("%s fail" % (row[2], ))
+            continue
+        val = prod.find_watch_probability()
+        cursor2.execute("""
+            UPDATE mcd SET watch_confidence = %s where
+            product_id = %s
+        """, (val, row[0]))
+
+    cursor2.close()
+    pgconn.commit()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
