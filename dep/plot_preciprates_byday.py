f5d29fd7223b9e3c282d78c8a02d8fa052c487f5
akrherz <akrherz@iastate.edu>
2019-11-20 09:47:15 -0600

    some dep stuff

---
+++ dep/plot_preciprates_byday.py
@@ -0,0 +1,49 @@
+"""Generate a visual showing rainfall rates."""
+import calendar
+import datetime
+
+from pyiem.dep import read_cli
+from pyiem.plot.use_agg import plt
+
+
+def main():
+    """Go Main Go."""
+    df = read_cli("/i/0/cli/091x041/091.48x041.88.cli")
+    df["jday"] = df.index.strftime("%j").astype("i")
+    gdf = df[["jday", "maxr"]].groupby("jday").max()
+
+    (fig, ax) = plt.subplots(1, 1)
+    ax.bar(
+        gdf.index,
+        gdf["maxr"],
+        width=1,
+        facecolor="tan",
+        edgecolor="tan",
+        zorder=2,
+        label="Raw Daily",
+    )
+    gdf = df[["jday", "maxr"]].groupby("jday").max().rolling(14).mean()
+    ax.plot(
+        gdf.index,
+        gdf["maxr"],
+        color="blue",
+        zorder=4,
+        label="Two Week Smoothed",
+    )
+    ax.set_xticks([1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335])
+    ax.set_xticklabels(calendar.month_abbr[1:])
+    ax.set_title(
+        (
+            "DEP 2007-2019 Maximum Precipitation Rate\n"
+            "For 41.88N 91.48W, Hourly Rate extrapolated from 2 minute value."
+        )
+    )
+    ax.legend()
+    ax.set_ylabel("Precipitation Rate [mm/hr]")
+    ax.grid(True)
+    ax.set_xlim(0, 366)
+    fig.savefig("test.png")
+
+
+if __name__ == "__main__":
+    main()
---
+++ dep/results_by_length.py
@@ -0,0 +1,48 @@
+"""Debug our flowpath length fun."""
+import sys
+
+from pyiem.util import get_dbconn
+from pyiem.dep import read_env
+import pandas as pd
+from pandas.io.sql import read_sql
+
+
+def main(argv):
+    """Go Main Go."""
+    huc12 = argv[1]
+    pgconn = get_dbconn("idep")
+    df = read_sql(
+        """
+        SELECT fpath, st_length(geom) from flowpaths where scenario = 0
+        and huc_12 = %s ORDER by st_length ASC
+    """,
+        pgconn,
+        params=(huc12,),
+        index_col="fpath",
+    )
+    df["delivery"] = 0.0
+    df["loss"] = 0.0
+    df["events"] = 0.0
+    df["precip"] = 0.0
+    sts = pd.Timestamp(year=2008, day=1, month=1)
+    ets = pd.Timestamp(year=2019, day=1, month=1)
+    for fpath, row in df.iterrows():
+        res = read_env(
+            "/i/0/env/%s/%s/%s_%s.env" % (huc12[:8], huc12[8:], huc12, fpath)
+        )
+        res["delivery"] = res["sed_del"] / row["st_length"]
+        df2 = res[(res["date"] > sts) & (res["date"] < ets)]
+        df.at[fpath, "delivery"] = df2["delivery"].sum() * 4.163
+        df.at[fpath, "loss"] = df2["av_det"].sum() * 4.163
+        df.at[fpath, "events"] = df2["delivery"].count()
+        df.at[fpath, "precip"] = df2["precip"].sum() / 25.4
+    print(df)
+    baseline = df["delivery"].mean() / 11.0
+    for length in range(0, 100, 5):
+        df2 = df[df["st_length"] > length]
+        val = df2["delivery"].mean() / 11.0
+        print("%3i %.2f %4.2f" % (length, val, val / baseline))
+
+
+if __name__ == "__main__":
+    main(sys.argv)
