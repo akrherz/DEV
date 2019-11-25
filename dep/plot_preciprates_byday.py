9e47d27747fb1c3decf370203b404f2d38526d19
akrherz <akrherz@iastate.edu>
2019-11-25 12:48:04 -0600

    sundry

--- dep/plot_preciprates_byday.py
+++ dep/plot_preciprates_byday.py
@@ -1,21 +1,22 @@
 """Generate a visual showing rainfall rates."""
 import calendar
-import datetime
 
 from pyiem.dep import read_cli
 from pyiem.plot.use_agg import plt
+from metpy.units import units
 
 
 def main():
     """Go Main Go."""
     df = read_cli("/i/0/cli/091x041/091.48x041.88.cli")
     df["jday"] = df.index.strftime("%j").astype("i")
+    df["maxr"] = (df["maxr"].values * units("mm")).to(units("inch")).m
     gdf = df[["jday", "maxr"]].groupby("jday").max()
 
     (fig, ax) = plt.subplots(1, 1)
     ax.bar(
         gdf.index,
-        gdf["maxr"],
+        gdf["maxr"].values,
         width=1,
         facecolor="tan",
         edgecolor="tan",
@@ -39,9 +40,9 @@ def main():
         )
     )
     ax.legend()
-    ax.set_ylabel("Precipitation Rate [mm/hr]")
+    ax.set_ylabel("Precipitation Rate [in/hr]")
     ax.grid(True)
-    ax.set_xlim(0, 366)
+    ax.set_xlim(60, 182)
     fig.savefig("test.png")
 
 
--- model/plot_ndfd.py
+++ model/plot_ndfd.py
@@ -1,53 +1,59 @@
-"""Plot WPC"""
-from __future__ import print_function
+"""Plot NDFD data.
+
+https://tgftp.nws.noaa.gov/SL.us008001/ST.opnl/DF.gr2/DC.ndfd/AR.conus/VP.001-003/
+"""
 import datetime
-import subprocess
 
-import numpy as np
-import pygrib
-import cartopy.io.shapereader as shpreader
-import cartopy.crs as ccrs
-from pyiem.plot.use_agg import plt
 from pyiem.plot import MapPlot, nwssnow
-
+import pygrib
 
 
 def main():
     """Go Main"""
-    grbs = pygrib.open('ds.snow.bin')
+    grbs = pygrib.open("ds.snow.bin")
     # skip 1-off first field
     total = None
     lats = lons = None
     for grb in grbs[1:]:
         if lats is None:
             lats, lons = grb.latlons()
-            total = grb['values']
+            total = grb["values"]
             continue
-        total += grb['values']
+        total += grb["values"]
+        print(grb.validDate)
     # TODO tz-hack here
-    analtime = grb.analDate - datetime.timedelta(hours=5)
+    analtime = grb.analDate - datetime.timedelta(hours=6)
 
     mp = MapPlot(
-        sector='custom', west=-100, east=-92, north=45, south=41,
-        axisbg='tan',
-        title=("NWS Forecasted Accumulated Snowfall "
-               "thru 7 PM 12 April 2019"),
-        subtitle='NDFD Forecast Issued %s' % (
-            analtime.strftime("%-I %p %-d %B %Y"), )
+        sector="custom",
+        west=-100,
+        east=-88,
+        north=46,
+        south=40,
+        axisbg="tan",
+        title=(
+            "NWS Forecasted Accumulated Snowfall "
+            "thru 12 PM 27 November 2019"
+        ),
+        subtitle="NDFD Forecast Issued %s"
+        % (analtime.strftime("%-I %p %-d %B %Y"),),
     )
     cmap = nwssnow()
-    cmap.set_bad('tan')
+    cmap.set_bad("tan")
     mp.pcolormesh(
-        lons, lats, total * 39.3701,
+        lons,
+        lats,
+        total * 39.3701,
         [0.01, 1, 2, 3, 4, 6, 8, 12, 18, 24, 30, 36],
         cmap=cmap,
-        units='inch')
+        units="inch",
+    )
 
     mp.drawcounties()
     mp.drawcities()
-    mp.postprocess(filename='test.png')
+    mp.postprocess(filename="test.png")
     mp.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
