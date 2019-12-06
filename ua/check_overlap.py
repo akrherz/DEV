d0dfd9ac1993c2d404ef191e404307660b643ea1
akrherz <akrherz@iastate.edu>
2019-12-06 08:59:00 -0600

    add RAOB diagnostic

---
+++ ua/check_overlap.py
@@ -0,0 +1,24 @@
+"""See if we need to dedup."""
+
+from pyiem.network import Table as NetworkTable
+
+
+def main():
+    """Go Main Go"""
+    nt = NetworkTable("RAOB", only_online=False)
+
+    for station in nt.sts:
+        if not station.startswith("_"):
+            continue
+        # Magic
+        stations = nt.sts[station]["name"].split("--")[1].strip().split()
+        for station2 in stations:
+            ab = nt.sts[station2]["archive_begin"]
+            ae = nt.sts[station2]["archive_end"]
+            on = nt.sts[station2]["online"]
+            print("%s %s[%s] %s -> %s" % (station, station2, on, ab, ae))
+        print()
+
+
+if __name__ == "__main__":
+    main()
