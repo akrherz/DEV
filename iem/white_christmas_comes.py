073998c8bca20e7f87d096eeb865fbe88f0b4c73
akrherz <akrherz@iastate.edu>
2019-12-17 21:35:22 -0600

    18 dec 2019 feature

--- iem/white_christmas_comes.py
+++ iem/white_christmas_comes.py
@@ -1,80 +1,77 @@
-import iemdb
-import numpy as np
-import mx.DateTime
+"""When does White Christmas Apear?"""
 import datetime
-COOP = iemdb.connect('coop', bypass=True)
 
-ccursor = COOP.cursor()
-ccursor2 = COOP.cursor()
-
-# Get white christmases
-ccursor.execute("""SELECT year from alldata_ia where sday = '1225' and
-  snowd >= 1 and station = 'IA2203'""")
-
-counts = np.zeros( (60,), 'f')
-
-for row in ccursor:
-    ccursor2.execute("""SELECT day, snowd from alldata_ia where
-    station = 'IA2203' and sday < '1225' and year = %s ORDER by day DESC 
-    LIMIT 60""", (row[0],))
-    count = -2
-    counts[-1] += 1
-    for row2 in ccursor2:
-        if row2[1] >= 1:
-            counts[count] += 1
-            count -= 1
-        else:
-            break
-    #print row[0], count
-
-counts2 = np.zeros((60,), 'f') 
-for i in range(-1,-61,-1):
-    ts = datetime.datetime(2000,12,25) + datetime.timedelta(days=i)
-    ccursor.execute("""WITH obs as (
-       SELECT year, snowd from alldata_ia where
-       station = 'IA2203' and sday = %s),
-      mins as (SELECT year, min(snowd) from alldata_ia 
-       where station = 'IA2203' and sday >= %s and sday < '1225'
-       GROUP by year),
-      combo as (SELECT m.year, m.min, o.snowd from obs o JOIN mins m
-      on (m.year = o.year) )
-
-      SELECT sum(case when snowd >= 1 then 1 else 0 end),
-      sum( case when snowd >= 1 and min >= 1 then 1 else 0 end) from combo
-     """, (ts.strftime("%m%d"), ts.strftime("%m%d")))  
-    row =  ccursor.fetchone()
-    if row[0] > 0:
-      counts2[i] = float(row[1]) / float(row[0]) * 100.
-
- 
-ets = mx.DateTime.DateTime(2000, 12, 25)
-sts = ets - mx.DateTime.RelativeDateTime(days=60)
-xticks = []
-xticklabels = []
-for i in range(2,60,3):
-    ts = sts + mx.DateTime.RelativeDateTime(days=i+1)
-    xticks.append( i )
-
-    xticklabels.append( ts.strftime("%-d\n%b"))
-
-import matplotlib.pyplot as plt
+import numpy as np
+from pyiem.util import get_dbconn
+from pyiem.plot.use_agg import plt
 
 
+def main():
+    """Go Main Go."""
+    pgconn = get_dbconn("coop")
+    ccursor = pgconn.cursor()
+    ccursor2 = pgconn.cursor()
 
-(fig, ax) = plt.subplots(1, 1)
+    # Get white christmases
+    ccursor.execute(
+        """SELECT year from alldata_ia where sday = '1225' and
+    snowd >= 1 and station = 'IA2203' and year > 1894"""
+    )
 
-ax.bar(np.arange(60)-0.3, counts / counts[-1] * 100.0, width=0.3,
-       fc='b', ec='b', label='Snowcover on 12/25 Arrival')
-ax.bar(np.arange(60), counts2, width=0.3, fc='r', ec='r',
-       label='Snowcover Survived until 12/25')
-ax.set_xticks( xticks )
-ax.set_xticklabels( xticklabels )
-ax.set_xlim(28,60)
-ax.set_ylim(0, 100)
-ax.set_yticks( [0,25,50,75,100])
-ax.grid(True)
-ax.set_ylabel("Frequency [%]")
-ax.set_title("Des Moines 1895-2013 White Christmas")
-ax.legend(loc=2)
+    counts = np.zeros((60,), "f")
 
-fig.savefig('test.png')
+    for row in ccursor:
+        ccursor2.execute(
+            """SELECT day, snowd from alldata_ia where
+        station = 'IA2203' and sday < '1225' and year = %s
+        and snowd is not null ORDER by day DESC
+        LIMIT 60""",
+            (row[0],),
+        )
+        idx = -2
+        counts[-1] += 1
+        for row2 in ccursor2:
+            if row2[1] >= 1:
+                counts[idx] += 1
+                idx -= 1
+            else:
+                break
+
+    ets = datetime.date(2000, 12, 25)
+    sts = ets - datetime.timedelta(days=60)
+    xticks = []
+    xticklabels = []
+    for i in range(2, 60, 3):
+        ts = sts + datetime.timedelta(days=i + 1)
+        xticks.append(i)
+
+        xticklabels.append(ts.strftime("%-d\n%b"))
+
+    (fig, ax) = plt.subplots(1, 1)
+
+    ax.bar(
+        np.arange(60),
+        counts / counts[-1] * 100.0,
+        width=1,
+        align="center",
+        fc="b",
+        ec="b",
+        label="Snowcover on 12/25 Arrival",
+    )
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(xticklabels)
+    ax.set_xlim(28, 60)
+    ax.set_ylim(0, 100)
+    ax.set_yticks([0, 10, 25, 50, 75, 90, 100])
+    ax.grid(True)
+    ax.set_ylabel("Frequency [%]")
+    ax.set_title(
+        "Des Moines 1895-2018 White Christmas\nDate which snow cover (1+ inch) arrived"
+    )
+    ax.legend(loc=2)
+
+    fig.savefig("191218.png")
+
+
+if __name__ == "__main__":
+    main()
