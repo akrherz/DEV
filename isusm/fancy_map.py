445e8030efdcc03ebd216cf3ddb8a2068ea4770b
akrherz <akrherz@iastate.edu>
2020-01-07 14:21:56 -0600

    add ISUSM fancy maps for pub stuff

---
+++ isusm/fancy_map.py
@@ -0,0 +1,32 @@
+"""Make something pretty."""
+
+from pyiem.plot import MapPlot
+from pyiem.network import Table as NetworkTable
+
+
+def main():
+    """Go Main Go."""
+    nt = NetworkTable("ISUSM")
+    counties = {}
+    lons = []
+    lats = []
+    vals = []
+    for sid in nt.sts:
+        counties[nt.sts[sid]["ugc_county"]] = 1
+        lons.append(nt.sts[sid]["lon"])
+        lats.append(nt.sts[sid]["lat"])
+        vals.append("x")
+
+    mp = MapPlot(
+        title="ISU Soil Moisture Station Locations + Counties Covered",
+        subtitle="As of 7 Jan 2020, %i stations in %i counties"
+        % (len(nt.sts), len(counties)),
+        nocaption=True,
+    )
+    mp.fill_ugcs(counties, bins=[0, 1, 2])
+    mp.plot_values(lons, lats, vals, textsize=18, labelbuffer=0)
+    mp.postprocess(filename="map.png")
+
+
+if __name__ == "__main__":
+    main()
