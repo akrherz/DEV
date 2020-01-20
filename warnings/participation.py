fb73fddd105f8b20f09f8ece4cd95fedb0efa7fe
akrherz <akrherz@iastate.edu>
2020-01-20 13:50:35 -0600

    update twitter plot of participation
    https://twitter.com/akrherz/status/1219346454491340805

--- warnings/participation.py
+++ warnings/participation.py
@@ -1,45 +1,60 @@
 """did the UGC have a warning on most active days."""
-import matplotlib.colors as mpcolors
-import matplotlib.cm as cm
-import numpy as np
 
+import numpy as np
 from pyiem.plot import MapPlot
 from pyiem.util import get_dbconn
-POSTGIS = get_dbconn('postgis')
-pcursor = POSTGIS.cursor()
-
-cmap = cm.get_cmap("Accent")
-cmap.set_under("#ffffff")
-cmap.set_over("black")
-
-m = MapPlot(sector='nws', axisbg='#EEEEEE',
-            title='1+ TOR warn for 100 most active TOR warn days 1986-2015',
-            subtitle=('A day is defined as 12 to 12 UTC period, did the '
-                      'county get 1+ warning during those 100 events?'),
-            cwas=True)
-
-bins = np.arange(0, 101, 10)
-bins[0] = 1
-norm = mpcolors.BoundaryNorm(bins, cmap.N)
+import matplotlib.cm as cm
 
 
-pcursor.execute("""
- WITH data as (
-  SELECT ugc, date(issue at time zone 'UTC' + '12 hours'::interval)
-  from warnings where phenomena in ('TO') and significance = 'W'
-  ),
- maxdays as (
-  SELECT date, count(*) from data GROUP by date ORDER by count DESC LIMIT 100
- ),
- events as (
- SELECT distinct ugc, d.date from data d JOIN maxdays m on (m.date = d.date)
- )
+def main():
+    """Go Main Go."""
+    pgconn = get_dbconn("postgis")
+    pcursor = pgconn.cursor()
 
- SELECT ugc, count(*) from events GROUP by ugc
-""")
-data = {}
-for row in pcursor:
-    data[row[0]] = float(row[1])
+    cmap = cm.get_cmap("Accent")
+    cmap.set_under("#ffffff")
+    cmap.set_over("black")
 
-m.fill_ugc_counties(data, bins, cmap=cmap, units='Count')
-m.postprocess(filename='test.png')
+    m = MapPlot(
+        sector="nws",
+        axisbg="#EEEEEE",
+        title="1+ Tornado Warn. for 100 most active Tornado Warn. days 1986-2019",
+        subtitle=(
+            "A day is defined as 12 to 12 UTC period, did the "
+            "county get 1+ warning during those 100 events?"
+        ),
+        cwas=True,
+    )
+
+    bins = np.arange(0, 31, 5)
+    bins[0] = 1
+
+    pcursor.execute(
+        """
+    WITH data as (
+        SELECT ugc, date(issue at time zone 'UTC' + '12 hours'::interval)
+        from warnings where phenomena in ('TO') and significance = 'W'
+        and issue < '2020-01-01'
+    ),
+    maxdays as (
+        SELECT date, count(*) from data
+        GROUP by date ORDER by count DESC LIMIT 100
+    ),
+    events as (
+        SELECT distinct ugc, d.date from data d
+        JOIN maxdays m on (m.date = d.date)
+    )
+
+    SELECT ugc, count(*) from events GROUP by ugc
+    """
+    )
+    data = {}
+    for row in pcursor:
+        data[row[0]] = float(row[1])
+
+    m.fill_ugcs(data, bins, cmap=cmap, units="Count")
+    m.postprocess(filename="test.png")
+
+
+if __name__ == "__main__":
+    main()
