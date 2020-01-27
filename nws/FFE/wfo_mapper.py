beea7f6eb4cb86de6959d6cb340534878b02251c
akrherz <akrherz@iastate.edu>
2020-01-27 13:25:09 -0600

    updated FFE counts that uses the database

--- nws/FFE/wfo_mapper.py
+++ nws/FFE/wfo_mapper.py
@@ -1,47 +1,54 @@
 """Generic plotter"""
-from __future__ import print_function
-import datetime
-from calendar import month_abbr
 
-import numpy as np
-import pandas as pd
-from pandas.io.sql import read_sql
 from pyiem.plot.use_agg import plt
 from pyiem.plot import MapPlot
-from pyiem.network import Table
 from pyiem.util import get_dbconn
+from pandas.io.sql import read_sql
 
 
 def main():
     """Go MAin"""
-    df = pd.read_csv('flood_emergencies.csv')
-    df2 = df[['source', 'eventid', 'phenomena', 'significance', 'year']
-             ].drop_duplicates()
-    gdf = df2.groupby('source').count()
-    vals = {}
-    labels = {}
-    for wfo, row in gdf.iterrows():
-        if wfo == 'TJSJ':
-            wfo = 'SJU'
-        else:
-            wfo = wfo[1:]
-        vals[wfo] = int(row['eventid'])
-        labels[wfo] = "%s" % (row['eventid'], )
+    pgconn = get_dbconn("postgis")
+    df = read_sql(
+        """
+        WITH data as (
+            select distinct wfo, extract(year from issue) as year, eventid
+            from warnings where phenomena = 'FF' and is_emergency
+            and issue < '2020-01-01'
+        )
+        SELECT wfo, count(*) from data GROUP by wfo ORDER by count DESC
+    """,
+        pgconn,
+        index_col="wfo",
+    )
+    if "JSJ" in df.index:
+        df.at["SJU", "count"] = df.at["JSJ", "count"]
 
-    bins = list(range(0, 31, 3))
-    bins[0] = 1.
-    cmap = plt.get_cmap('plasma_r')
-    cmap.set_over('black')
-    cmap.set_under('white')
-    mp = MapPlot(sector='nws', continentalcolor='white', figsize=(12., 9.),
-                 title=("2003-2018 Flash Flood Emergency Events"),
-                 subtitle=('based on unofficial IEM archives, searching '
-                           '"FFS", "FLW", "FFS".'))
-    mp.fill_cwas(vals, bins=bins, lblformat='%s', labels=labels,
-                 cmap=cmap, ilabel=True,  # clevlabels=month_abbr[1:],
-                 units='count')
-    mp.postprocess(filename='test.png')
+    bins = list(range(0, 37, 6))
+    bins[0] = 1
+    cmap = plt.get_cmap("plasma_r")
+    cmap.set_over("black")
+    cmap.set_under("white")
+    mp = MapPlot(
+        sector="nws",
+        continentalcolor="white",
+        figsize=(12.0, 9.0),
+        title=("2003-2019 Flash Flood Emergency Events"),
+        subtitle=(
+            "based on unofficial IEM archives, searching "
+            '"FFS", "FLW", "FFS".'
+        ),
+    )
+    mp.fill_cwas(
+        df["count"].to_dict(),
+        bins=bins,
+        lblformat="%.0f",
+        cmap=cmap,
+        ilabel=True,  # clevlabels=month_abbr[1:],
+        units="count",
+    )
+    mp.postprocess(filename="test.png")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
