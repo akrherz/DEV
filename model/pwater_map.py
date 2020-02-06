7724eef88d62262b981ac3a4f9df1eb5944f67c5
akrherz <akrherz@iastate.edu>
2020-02-06 08:06:55 -0600

    6 feb 2020 feature

--- model/pwater_map.py
+++ model/pwater_map.py
@@ -1,48 +1,57 @@
 """Map of precipitable water"""
-from __future__ import print_function
-import psycopg2
+
 import numpy as np
-from pandas.io.sql import read_sql
-from pyiem.datatypes import distance
 from pyiem.network import Table as NetworkTable
+from pyiem.plot.use_agg import plt
 from pyiem.plot.geoplot import MapPlot
-import matplotlib.pyplot as plt
+from pyiem.util import get_dbconn
+from metpy.units import units
+from pandas.io.sql import read_sql
 
 
 def main():
     """Go Main"""
-    nt = NetworkTable(['AWOS', 'IA_ASOS'])
-    pgconn = psycopg2.connect(database='mos', host='localhost', user='nobody',
-                              port=5555)
-    df = read_sql("""
+    nt = NetworkTable(["AWOS", "IA_ASOS"], only_online=False)
+    pgconn = get_dbconn("mos")
+    df = read_sql(
+        """
     select station, avg(pwater) from model_gridpoint where
     model = 'NAM' and extract(hour from runtime at time zone 'UTC') = 0
     and pwater > 0 and pwater < 100 and
-    extract(month from runtime) between 4 and 9 and ftime = runtime
+    extract(month from runtime) = 1 and ftime = runtime
     GROUP by station ORDER by avg
-    """, pgconn, index_col='station')
-    df['lat'] = None
-    df['lon'] = None
-    df['pwater'] = distance(df['avg'].values, 'MM').value('IN')
+    """,
+        pgconn,
+        index_col="station",
+    )
+    df["lat"] = None
+    df["lon"] = None
+    df["pwater"] = (df["avg"].values * units("mm")).to(units("inch")).m
     for station in df.index.values:
-        df.at[station, 'lat'] = nt.sts[station[1:]]['lat']
-        df.at[station, 'lon'] = nt.sts[station[1:]]['lon']
+        df.at[station, "lat"] = nt.sts[station[1:]]["lat"]
+        df.at[station, "lon"] = nt.sts[station[1:]]["lon"]
 
-    mp = MapPlot(title=('00z Analysis NAM Warm-Season Average '
-                        'Precipitable Water [in]'),
-                 subtitle=("based on grid point samples "
-                           "from 2004-2017 (April-September)"))
+    mp = MapPlot(
+        title=("00z Analysis NAM January Average " "Precipitable Water [in]"),
+        subtitle=("based on grid point samples " "from 2004-2020 for January"),
+    )
     cmap = plt.get_cmap("plasma_r")
-    cmap.set_under('white')
-    cmap.set_over('black')
-    mp.contourf(df['lon'], df['lat'], df['pwater'],
-                np.arange(0.94, 1.13, 0.03), cmap=cmap,
-                units='inch')
+    cmap.set_under("white")
+    cmap.set_over("black")
+    print(df["pwater"].describe())
+    mp.contourf(
+        df["lon"],
+        df["lat"],
+        df["pwater"],
+        np.arange(0.26, 0.37, 0.02),
+        cmap=cmap,
+        units="inch",
+    )
     mp.drawcounties()
     mp.drawcities()
-    mp.postprocess(filename='170901.png')
+    mp.postprocess(filename="200205.png")
     mp.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
