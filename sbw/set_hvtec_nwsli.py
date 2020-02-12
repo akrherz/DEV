76baff459e9f2b392a97c0ee4b5d9f36018e9a99
akrherz <akrherz@iastate.edu>
2020-02-12 14:07:19 -0600

    add backfill of HVTEC_NWSLI into sbw
    refs akrherz/iem#247

---
+++ sbw/set_hvtec_nwsli.py
@@ -0,0 +1,87 @@
+"""See akrherz/iem#247"""
+import sys
+
+from pyiem.util import get_dbconn, noaaport_text
+from pyiem.nws.products.vtec import parser
+from tqdm import tqdm
+
+
+def main(argv):
+    """Go Main Go."""
+    pgconn = get_dbconn("postgis")
+    cursor = pgconn.cursor()
+    cursor2 = pgconn.cursor()
+
+    table = "sbw_%s" % (argv[1],)
+
+    cursor.execute(
+        """
+        SELECT report, status, wfo, eventid, significance, phenomena,
+        updated, polygon_begin from """
+        + table
+        + """
+        WHERE phenomena in ('FA', 'FF', 'FL', 'HY', 'IJ') and
+        (length(hvtec_nwsli) != 5 or hvtec_nwsli is null)
+        ORDER by polygon_begin ASC
+    """
+    )
+    considered = 0
+    desc = tqdm(cursor, total=cursor.rowcount)
+    for row in desc:
+        desc.set_description(row[7].strftime("%b %d"))
+        considered += 1
+        try:
+            prod = parser(noaaport_text(row[0]))
+        except Exception as exp:
+            print(exp)
+            sys.exit()
+        found = False
+        for seg in prod.segments:
+            if not seg.vtec:
+                continue
+            v = seg.vtec[0]
+            if (
+                v.phenomena == row[5]
+                and v.significance == row[4]
+                and v.etn == row[3]
+            ):
+                cursor2.execute(
+                    """
+                    UPDATE """
+                    + table
+                    + """ SET hvtec_nwsli = %s
+                    WHERE wfo = %s and phenomena = %s and significance = %s
+                    and status = %s and eventid = %s and updated = %s
+                    and polygon_begin = %s
+                """,
+                    (
+                        seg.get_hvtec_nwsli(),
+                        row[2],
+                        row[5],
+                        row[4],
+                        row[1],
+                        row[3],
+                        row[6],
+                        row[7],
+                    ),
+                )
+                found = True
+                if cursor2.rowcount == 0:
+                    print(prod)
+                    print(row)
+                    print(cursor2.rowcount)
+                    sys.exit()
+        if considered % 1000 == 0:
+            cursor2.close()
+            pgconn.commit()
+            cursor2 = pgconn.cursor()
+        if not found:
+            print(prod)
+            sys.exit()
+    print("Processed %s rows" % (considered,))
+    cursor2.close()
+    pgconn.commit()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
