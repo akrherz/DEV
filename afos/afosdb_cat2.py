a232361edaa97aaf1309fe990eeb93bc0ebac6bc
akrherz <akrherz@iastate.edu>
2020-02-27 21:57:45 -0600

    use args

--- afos/afosdb_cat2.py
+++ afos/afosdb_cat2.py
@@ -1,28 +1,27 @@
-"""Send products from AFOS database to pyWWA."""
+"""Dump products to file."""
+import sys
 
 from pyiem.util import get_dbconn, noaaport_text
 
 
-def main():
+def main(argv):
     """Go Main Go."""
     pgconn = get_dbconn("afos")
-    cursor = pgconn.cursor("streamer")
+    cursor = pgconn.cursor()
 
     cursor.execute(
         """
         SELECT data, entered at time zone 'UTC', pil from products
-        WHERE pil in ('PFMBIS', 'PFMARX', 'PFMMPX', 'PFMDLH', 'PFMFGF')
-        and entered > '2016-01-01 00:00+00'
-    """
+        WHERE pil = %s
+        and entered >= '2020-02-26 12:00+00'
+        ORDER by entered ASC
+    """,
+        (argv[1],),
     )
-    for _i, row in enumerate(cursor):
-        with open(
-            "PFM/%s/%s_%s.txt"
-            % (row[1].year, row[2], row[1].strftime("%Y%m%d%H%M")),
-            "a",
-        ) as fh:
+    with open("%s.txt" % (argv[1],), "a") as fh:
+        for _i, row in enumerate(cursor):
             fh.write(noaaport_text(row[0]))
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
