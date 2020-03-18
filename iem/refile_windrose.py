7a9b70f2ccdb216c266f2d0d65e755fb0266c4ff
akrherz <akrherz@iastate.edu>
2020-03-18 08:56:04 -0500

    reorg windrose files to match new reality

---
+++ iem/refile_windrose.py
@@ -0,0 +1,43 @@
+"""Reorganize wind rose files."""
+import os
+import shutil
+import calendar
+
+from tqdm import tqdm
+from pyiem.util import get_dbconn
+
+OLDDIR = "/mesonet/share/windrose/climate"
+
+
+def main():
+    """ Go Main Go."""
+    pgconn = get_dbconn("mesosite")
+    cursor = pgconn.cursor()
+    cursor.execute(
+        """
+        SELECT id, network from stations where network ~* 'ASOS' or
+        network = 'AWOS' or network ~* 'DCP' ORDER by id ASC"""
+    )
+    for row in tqdm(cursor, total=cursor.rowcount):
+        testfn = "%s/yearly/%s_yearly.png" % (OLDDIR, row[0])
+        if not os.path.isfile(testfn):
+            continue
+        cachedir = "/mesonet/share/windrose/%s/%s" % (row[1], row[0])
+        if not os.path.isdir(cachedir):
+            os.makedirs(cachedir)
+        shutil.move(testfn, "%s/%s_yearly.png" % (cachedir, row[0]))
+        for i, abbr in enumerate(calendar.month_abbr[1:]):
+            abbr = abbr.lower()
+            testfn = "%s/monthly/%02i/%s_%s.png" % (
+                OLDDIR,
+                i + 1,
+                row[0],
+                abbr,
+            )
+            if not os.path.isfile(testfn):
+                continue
+            shutil.move(testfn, "%s/%s_%s.png" % (cachedir, row[0], abbr))
+
+
+if __name__ == "__main__":
+    main()
