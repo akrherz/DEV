ca7df67648f6128c03f8a6456cc42e69111fe8c1
akrherz <akrherz@iastate.edu>
2020-04-09 10:30:38 -0500

    add another one-off

---
+++ coop/map_frequency.py
@@ -0,0 +1,64 @@
+"""Map values"""
+
+from pyiem.util import get_dbconn
+from pyiem.plot import MapPlot
+from pyiem.plot.use_agg import plt
+from pyiem.network import Table as NetworkTable
+from pandas.io.sql import read_sql
+
+
+def main():
+    """Go Main!"""
+    nt = NetworkTable("IACLIMATE")
+    pgconn = get_dbconn("coop")
+
+    df = read_sql(
+        """
+    with data as (
+        select station, year, sum(precip) from alldata_ia
+        WHERE sday >= '0601' and sday < '0606' and year >= 1995
+        and year < 2010
+        GROUP by station, year
+    )
+    select station, sum(case when sum >= 0.5 then 1 else 0 end) as hits,
+    count(*) from data GROUP by station ORDER by station ASC
+    """,
+        pgconn,
+        index_col="station",
+    )
+    df["lat"] = 0.0
+    df["lon"] = 0.0
+    df["freq"] = df["hits"] / df["count"] * 100.0
+    for station, _ in df.iterrows():
+        if station in nt.sts and station != "IA0000" and station[2] != "C":
+            df.at[station, "lat"] = nt.sts[station]["lat"]
+            df.at[station, "lon"] = nt.sts[station]["lon"]
+
+    mp = MapPlot(
+        title="1-5 June :: Frequency of +0.5 inches of Precipitation",
+        subtitle=("based on 1995-2019 data"),
+        sector="state",
+        state="IA",
+        drawstates=True,
+        continentalcolor="white",
+    )
+    df2 = df[df["lat"] > 20]
+    cmap = plt.get_cmap("terrain_r")
+    mp.contourf(
+        df2["lon"].values,
+        df2["lat"].values,
+        df2["freq"].values,
+        range(0, 101, 10),
+        extend="neither",
+        units="Frequency [%]",
+        cmap=cmap,
+    )
+    mp.plot_values(
+        df2["lon"].values, df2["lat"].values, df2["freq"].values, fmt="%.0f%%"
+    )
+    mp.drawcounties()
+    mp.postprocess(filename="test.png")
+
+
+if __name__ == "__main__":
+    main()
