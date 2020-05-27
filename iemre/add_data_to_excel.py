408d12012b96c36226a5adeb3d15f130844561e1
akrherz <akrherz@iastate.edu>
2020-05-27 12:50:00 -0500

    add iemre sampled data

---
+++ iemre/add_data_to_excel.py
@@ -0,0 +1,53 @@
+"""A util script to dump IEMRE json service to csv files"""
+
+import requests
+from pyiem.util import get_dbconn
+from pandas.io.sql import read_sql
+import pandas as pd
+
+
+def workflow(gid, lat, lon):
+    """Do Some Work Please"""
+    res = []
+    for year in range(2010, 2016):
+        print("Processing GID: %s year: %s" % (gid, year))
+        uri = (
+            "http://mesonet.agron.iastate.edu/iemre/multiday/"
+            "%s-01-01/%s-12-31/%s/%s/json"
+        ) % (year, year, lat, lon)
+        req = requests.get(uri)
+        for row in req.json()["data"]:
+            res.append(row)
+    df = pd.DataFrame(res)
+    df.to_csv("%s.csv" % (gid,), index=False)
+
+
+def main():
+    """Go Main Go"""
+    df = pd.read_excel("/tmp/dates.xlsx")
+    df["high_temp_f"] = 0
+    df["low_temp_f"] = 0
+    pgconn = get_dbconn("postgis")
+    counties = read_sql(
+        "SELECT name, ST_x(centroid) as lon, st_y(centroid) as lat from "
+        "ugcs where end_ts is null and substr(ugc, 1, 3) = 'IAC' "
+        "ORDER by name ASC",
+        pgconn,
+    )
+    for idx, row in df.iterrows():
+        df2 = counties[counties["name"] == row["Site"]]
+        uri = (
+            "http://mesonet.agron.iastate.edu/iemre/daily/"
+            f"{row['Date'].strftime('%Y-%m-%d')}/"
+            f"{df2.iloc[0]['lat']}/{df2.iloc[0]['lon']}/json"
+        )
+        req = requests.get(uri)
+        data = req.json()["data"][0]
+        df.at[idx, "high_temp_f"] = data["daily_high_f"]
+        df.at[idx, "high_low_f"] = data["daily_low_f"]
+
+    df.to_csv("/tmp/dates.csv")
+
+
+if __name__ == "__main__":
+    main()
