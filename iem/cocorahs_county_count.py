8438d3b50b056b19862e6a7b0ae54cad26ecc58d
akrherz <akrherz@iastate.edu>
2020-06-24 08:23:16 -0500

    25 Jun feature

--- iem/cocorahs_county_count.py
+++ iem/cocorahs_county_count.py
@@ -1,54 +1,73 @@
-import psycopg2
+"""CoCoRaHS county count map."""
+
 from pyiem.plot import MapPlot
+from pyiem.plot.use_agg import plt
+from pyiem.util import get_dbconn
 
-iemdb = psycopg2.connect(database="iem", host="iemdb", user="nobody")
-icursor = iemdb.cursor()
 
-POSTGIS = psycopg2.connect(database="postgis", host="iemdb", user="nobody")
-pcursor = POSTGIS.cursor()
+def main():
+    """Go Main Go."""
+    iemdb = get_dbconn("iem")
+    icursor = iemdb.cursor()
 
-icursor.execute(
+    postgis = get_dbconn("postgis")
+    pcursor = postgis.cursor()
+
+    icursor.execute(
+        """
+    WITH data as (
+    SELECT distinct s.iemid
+    from summary s JOIN stations t on (t.iemid = s.iemid)
+    WHERE t.network = 'IACOCORAHS' and s.day >= '2019-06-01' and pday > 0)
+
+    SELECT ugc_county, count(*)
+    from stations t JOIN data d on (d.iemid = t.iemid)
+    GROUP by ugc_county ORDER by count DESC
     """
- WITH data as (
-   SELECT distinct s.iemid
-   from summary s JOIN stations t on (t.iemid = s.iemid)
-   WHERE t.network = 'IACOCORAHS' and s.day > '2015-09-12' and pday > 0)
+    )
 
- SELECT ugc_county, count(*) from stations t JOIN data d on (d.iemid = t.iemid)
- GROUP by ugc_county ORDER by count DESC
-"""
-)
+    data = {}
+    total = 0
+    for row in icursor:
+        data[row[0]] = row[1]
+        total += row[1]
 
-data = {}
-for row in icursor:
-    data[row[0]] = row[1]
+    # Query out centroids of counties...
+    pcursor.execute(
+        """
+        SELECT ugc, ST_x(ST_centroid(geom)) as lon,
+        ST_y(ST_centroid(geom)) as lat
+        from ugcs WHERE state = 'IA' and end_ts is null and
+        substr(ugc,3,1) = 'C'
+        """
+    )
+    clons = []
+    clats = []
+    cvals = []
+    for row in pcursor:
+        cvals.append(data.get(row[0], 0))
+        clats.append(row[2])
+        clons.append(row[1])
 
-# Query out centroids of counties...
-pcursor.execute(
-    """
-    SELECT ugc, ST_x(ST_centroid(geom)) as lon,
-    ST_y(ST_centroid(geom)) as lat
-    from ugcs WHERE state = 'IA' and end_ts is null and
-    substr(ugc,3,1) = 'C'
-    """
-)
-clons = []
-clats = []
-cvals = []
-for row in pcursor:
-    cvals.append(data.get(row[0], 0))
-    clats.append(row[2])
-    clons.append(row[1])
+    mp = MapPlot(
+        axisbg="white",
+        title=f"Iowa CoCoRaHS Observers Per County ({total} Total)",
+        subtitle=(
+            "Sites with at least one report in past year (June 2019-2020)"
+        ),
+    )
+    cmap = plt.get_cmap("YlGnBu")
+    mp.fill_ugcs(
+        data,
+        [1, 2, 3, 4, 5, 7, 10, 15, 20],
+        cmap=cmap,
+        spacing="proportional",
+        extend="max",
+    )
+    mp.plot_values(clons, clats, cvals, labelbuffer=0)
+    mp.drawcounties()
+    mp.postprocess(filename="200625.png")
 
 
-m = MapPlot(
-    axisbg="white",
-    title="Iowa CoCoRaHS Observers Per County",
-    subtitle=(
-        "Sites with at least one report in past year " "(Sep 2015-2016)"
-    ),
-)
-m.fill_ugcs(data, [1, 2, 3, 4, 5, 7, 10, 15, 20])
-m.plot_values(clons, clats, cvals, labelbuffer=0)
-m.drawcounties()
-m.postprocess(filename="test.png")
+if __name__ == "__main__":
+    main()
