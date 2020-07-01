aa2c111ff17799beda47fb5b94f15e892b517fb9
akrherz <akrherz@iastate.edu>
2020-07-01 05:26:11 -0500

    plot 2019-2020 season snowfall

--- model/plot_nohrsc.py
+++ model/plot_nohrsc.py
@@ -1,33 +1,29 @@
 """Plot WPC"""
-from __future__ import print_function
-import glob
 
-import numpy as np
 import netCDF4
-from metpy.units import units
 from pyiem.plot.use_agg import plt
 from pyiem.plot import MapPlot
-from pyiem import reference
 
 
 def main():
     """Go Main"""
-    nc = netCDF4.Dataset("/tmp/sfav2_CONUS_2018093012_to_2019021312.nc")
+    nc = netCDF4.Dataset("/tmp/sfav2_CONUS_2019093012_to_2020063012.nc")
     lats = nc.variables["lat"][:]
     lons = nc.variables["lon"][:]
     data = nc.variables["Data"][:] * 1000.0 / 25.4
     nc.close()
 
     mp = MapPlot(
-        sector="iowa",
+        sector="midwest",
         continentalcolor="tan",
         title=(
-            "National Snowfall Analysis - NOHRSC " "- Season Total Snowfall"
+            "National Snowfall Analysis - NOHRSC - "
+            "2019-2020 Season Total Snowfall"
         ),
-        subtitle="Snowfall up until 7 AM 13 Feb 2019",
+        subtitle="Snowfall up until 7 PM 30 Jun 2020",
     )
     cmap = plt.get_cmap("terrain_r")
-    levs = [0.1, 2, 5, 8, 12, 18, 24, 30, 36, 42, 48]
+    levs = [0.1, 2, 5, 8, 12, 18, 24, 30, 36, 42, 48, 60]
     mp.pcolormesh(
         lons,
         lats,
@@ -38,8 +34,8 @@ def main():
         clip_on=False,
         spacing="proportional",
     )
-    mp.drawcounties()
-    mp.drawcities()
+    # mp.drawcounties()
+    # mp.drawcities()
     mp.postprocess(filename="test.png")
 
 
