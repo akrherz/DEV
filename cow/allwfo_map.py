be157883fc55c399c4875a5692718ff2117b89ea
akrherz <akrherz@iastate.edu>
2020-07-29 13:04:56 -0500

    sync laptop content

---
+++ cow/allwfo_map.py
@@ -0,0 +1,32 @@
+"""Generate a pretty map."""
+
+from pyiem.plot.use_agg import plt
+from pyiem.plot.geoplot import MapPlot
+import numpy as np
+import pandas as pd
+
+
+def main():
+    """Go Main Go."""
+    df = pd.read_csv("allwfo.csv", index_col="wfo")
+    mp = MapPlot(
+        sector="conus",
+        title="2015-2019 June-August Severe Thunderstorm Warning POD",
+        subtitle=(
+            "based on unofficial IEM Cow stats using Local Storm Reports"
+        ),
+    )
+    cmap = plt.get_cmap("plasma")
+    mp.fill_cwas(
+        df["POD[1]"],
+        bins=np.arange(0, 1.01, 0.1),
+        cmap=cmap,
+        extend="neither",
+        ilabel=True,
+        lblformat="%.2f",
+    )
+    mp.postprocess(filename="test.png")
+
+
+if __name__ == "__main__":
+    main()
---
+++ cow/allwfo_via_api.py
@@ -0,0 +1,32 @@
+"""Generate a file of Cow Stats via API calls."""
+
+from pyiem.network import Table as NetworkTable
+from tqdm import tqdm
+import requests
+import pandas as pd
+
+
+def main():
+    """Our main method."""
+    nt = NetworkTable("WFO")
+    progress = tqdm(list(nt.sts.keys()))
+    res = []
+    for wfo in progress:
+        progress.set_description(wfo)
+        url = (
+            f"http://iem.local/api/1/cow.json?wfo={wfo}&"
+            "begints=2015-01-01T00:00Z&endts=2020-01-01T00:00Z&"
+            "phenomena=SV&lsrtype=SV&"
+        )
+        reg = requests.get(url)
+        jsobj = reg.json()
+        jsobj["stats"]["wfo"] = wfo
+        res.append(jsobj["stats"])
+
+    df = pd.DataFrame(res)
+    df = df.set_index("wfo")
+    df.to_csv("allwfo.csv")
+
+
+if __name__ == "__main__":
+    main()
