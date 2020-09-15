30de6a9b24f18b95c0ac171f2eeb56e8fcdad5ef
akrherz <akrherz@iastate.edu>
2020-09-15 13:05:07 -0500

    add util lister

---
+++ nws/list_bad_hvtec.py
@@ -0,0 +1,42 @@
+"""Go Fishing."""
+import sys
+import datetime
+
+from pyiem.util import get_dbconn
+from pyiem.nws.products.vtec import parser
+
+
+def main():
+    """Go Main Go."""
+    afos = get_dbconn("afos")
+    cursor = afos.cursor()
+    cursor.execute(
+        "SELECT data from products where entered > '2019-07-01' and "
+        "substr(pil, 1, 3) in ('FLS', 'FFS', 'FLW', 'FFW') ORDER by entered "
+        "ASC"
+    )
+    samples = {}
+    for row in cursor:
+        try:
+            prod = parser(row[0])
+        except Exception as exp:
+            print("ERROR, exp: %s" % (exp,))
+            continue
+        for seg in prod.segments:
+            for hvtec in seg.hvtec:
+                for col in ["beginTS", "crestTS", "endTS"]:
+                    val = getattr(hvtec, col, None)
+                    if val is None:
+                        continue
+                    if val.year in [2019, 2020]:
+                        continue
+                    if prod.source in samples:
+                        continue
+                    samples[prod.source] = 1
+                    print(
+                        f"{prod.get_product_id()} vtec:{seg.vtec[0]} hvtec:{hvtec}"
+                    )
+
+
+if __name__ == "__main__":
+    main()
