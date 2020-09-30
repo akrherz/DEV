aa7b3120a09f947e5c84998de2c7c71ad7df6b99
akrherz <akrherz@iastate.edu>
2020-09-29 22:23:53 -0500

    30 Sep feature

--- usda/daily_departures.py
+++ usda/daily_departures.py
@@ -1,9 +1,9 @@
 """Daily departures from NASS."""
 import calendar
 
-from pandas.io.sql import read_sql
 from pyiem.util import get_dbconn
 from pyiem.plot.use_agg import plt
+from pandas.io.sql import read_sql
 import pandas as pd
 
 
@@ -14,8 +14,8 @@ def main():
         """
         select year::int as year, unit_desc, week_ending, num_value,
         state_alpha from nass_quickstats
-        where commodity_desc = 'CORN' and statisticcat_desc = 'PROGRESS'
-        and unit_desc in ('PCT PLANTED', 'PCT EMERGED') and
+        where commodity_desc = 'SOYBEANS' and statisticcat_desc = 'PROGRESS'
+        and unit_desc in ('PCT HARVESTED') and
         state_alpha in ('IA', 'IL') and
         util_practice_desc = 'ALL UTILIZATION PRACTICES'
         and num_value is not null
@@ -27,37 +27,38 @@ def main():
     df["week_ending"] = pd.to_datetime(df["week_ending"])
     df = df.set_index("week_ending")
     (fig, ax) = plt.subplots(1, 1)
-    ls = {"PCT PLANTED": "-", "PCT EMERGED": "-."}
-    st = {"IA": "g", "IL": "r"}
-    for state in ["IA", "IL"]:
-        for unit in ["PCT PLANTED", "PCT EMERGED"]:
+    ls = {"PCT HARVESTED": "-", "PCT EMERGED": "-."}
+    st = {2012: "g", 2019: "k", 2020: "r"}
+    for state in ["IA"]:
+        for unit in ["PCT HARVESTED"]:
             df2 = df[(df["unit_desc"] == unit) & (df["state_alpha"] == state)]
             df3 = df2.resample("D").interpolate(method="linear")
             df3["doy"] = pd.to_numeric(df3.index.strftime("%j"))
             avg = (
-                df3[(df3["year"] > 2008) & (df3["year"] < 2019)]
+                df3[(df3["year"] > 2008) & (df3["year"] < 2020)]
                 .groupby("doy")
                 .mean()
             )
-            d2019 = df3[df3["year"] == 2019].set_index("doy")
-            data = d2019["num_value"] - avg["num_value"]
-            ax.plot(
-                data.index.values,
-                data.values,
-                label="%s %s" % (state, unit),
-                linestyle=ls[unit],
-                lw=2,
-                color=st[state],
-            )
-    ax.set_xticks((1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 365))
+            for year in [2012, 2019, 2020]:
+                d2020 = df3[df3["year"] == year].set_index("doy")
+                data = d2020["num_value"] - avg["num_value"]
+                ax.plot(
+                    data.index.values,
+                    data.values,
+                    label=f"{year}",
+                    linestyle=ls[unit],
+                    lw=2,
+                    color=st[year],
+                )
+    ax.set_xticks((1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335))
     ax.set_xticklabels(calendar.month_abbr[1:])
-    ax.set_xlim(90, 155)
+    ax.set_xlim(240, 330)
     ax.grid(True)
     ax.set_ylabel("Absolute Percentage Departure from 10yr Avg")
     ax.set_title(
         (
-            "USDA NASS Corn Acres Planted and Emerged\n"
-            "Departure from 2009-2018 Average"
+            "USDA NASS Iowa Soybean Acres Harvested\n"
+            "Departure from 2009-2019 Average"
         )
     )
     ax.legend(ncol=1)
