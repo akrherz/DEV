6b817ee0d812ee1a5c5603116c1f020d48afff34
akrherz <akrherz@iastate.edu>
2020-10-13 21:01:15 -0500

    oct 14 feature

--- iem/cli_timeseries.py
+++ iem/cli_timeseries.py
@@ -19,8 +19,8 @@ def main():
     pgconn = get_dbconn("iem")
     df = read_sql(
         """
-    SELECT valid, station, snow_jul1 - snow_jul1_normal as departure
-    from cli_data WHERE valid > '2018-10-01' and station in
+    SELECT valid, station, precip_jan1 - precip_jan1_normal as departure
+    from cli_data WHERE valid > '2020-01-01' and station in
     ('KDSM', 'KDBQ', 'KSUX', 'KMCW', 'KMLI', 'KOMA') ORDER by valid ASC
     """,
         pgconn,
@@ -29,22 +29,23 @@ def main():
 
     (fig, ax) = plt.subplots(1, 1)
     for station, gdf in df.groupby("station"):
+        vals = gdf["departure"].values
         ax.plot(
             gdf["valid"].values,
-            gdf["departure"].values,
-            label=STATIONS[station],
+            vals,
+            label=f'{STATIONS[station]} {vals[-1]:.2f}"',
             lw=2,
         )
-    ax.xaxis.set_major_formatter(mdates.DateFormatter("%b\n%Y"))
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%b"))
     ax.grid(True)
-    ax.legend(loc=2)
+    ax.legend(loc=3)
     ax.set_title(
         (
-            "Season to Date Snowfall Departures\n"
+            "2020 Year to Date Precipitation Departures\n"
             "Data from NWS CLI Reporting Sites"
         )
     )
-    ax.set_ylabel("Snowfall Departure [inch]")
+    ax.set_ylabel("Precipitation Departure [inch]")
     fig.savefig("test.png")
 
 
