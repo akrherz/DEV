55f9f85d614cfef60f03ab6cf0a6de88c2ac5ce5
akrherz <akrherz@iastate.edu>
2020-10-19 14:01:35 -0500

    add TGTP ingest for occasional usage

---
+++ asos/ingest_tgftp.py
@@ -0,0 +1,81 @@
+"""Process the METARs found at TGFTP."""
+from datetime import datetime
+import re
+import os
+import sys
+
+from pyiem.util import get_dbconn, logger, exponential_backoff
+from pyiem.nws.products.metarcollect import METARReport
+from psycopg2.extras import DictCursor
+import requests
+
+LOG = logger()
+URLBASE = "https://tgftp.nws.noaa.gov/data/observations/metar/cycles/"
+TIMERE = re.compile(
+    "^20[0-9][0-9]/[0-2][0-9]/[0-3][0-9] [0-2][0-9]:[0-5][0-9]$"
+)
+
+
+def build_xref():
+    """Need networks / iemids to make this work."""
+    dbconn = get_dbconn("mesosite")
+    cursor = dbconn.cursor()
+    cursor.execute(
+        "SELECT id, iemid, network, tzname from stations "
+        "where (network = 'AWOS' or network ~* 'ASOS') and tzname is not null"
+    )
+    xref = {}
+    for row in cursor:
+        xref[row[0]] = {"iemid": row[1], "network": row[2], "tzname": row[3]}
+    return xref
+
+
+def get_file(fn):
+    """Get the file from the server."""
+    req = exponential_backoff(requests.get, f"{URLBASE}{fn}", timeout=20)
+    tmpfn = f"/tmp/{fn}"
+    with open(tmpfn, "wb") as fh:
+        fh.write(req.content)
+    return tmpfn
+
+
+def main(argv):
+    """Go Main Go."""
+    fn = get_file(argv[1])
+    xref = build_xref()
+    pgconn = get_dbconn("iem")
+    valid = None
+    (failures, success) = (0, 0)
+    with open(fn) as fh:
+        for line in fh:
+            line = line.strip()
+            if line == "":
+                valid = None
+                continue
+            if TIMERE.match(line):
+                valid = datetime.strptime(line, "%Y/%m/%d %H:%M")
+                continue
+            # Must have an ob!
+            try:
+                mtr = METARReport(line, month=valid.month, year=valid.year)
+            except Exception:
+                failures += 1
+                continue
+            lookup = line[:4] if not line.startswith("K") else line[1:4]
+            if lookup not in xref:
+                LOG.info("Unknown %s", lookup)
+                continue
+            mtr.iemid = lookup
+            mtr.network = xref[lookup]["network"]
+            mtr.tzname = xref[lookup]["tzname"]
+            cursor = pgconn.cursor(cursor_factory=DictCursor)
+            mtr.to_iemaccess(cursor, force_current_log=True)
+            cursor.close()
+            pgconn.commit()
+            success += 1
+    LOG.info("Processed %s failures %s", success, failures)
+    os.unlink(fn)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
