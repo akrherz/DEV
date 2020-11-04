c724d8cdf7d042d250460ef1c20d5132cf7aad5d
akrherz <akrherz@iastate.edu>
2020-11-04 07:31:05 -0600

    4 Nov feature

--- asos/date_of_last.py
+++ asos/date_of_last.py
@@ -17,16 +17,16 @@ def main():
 
     cursor.execute(
         """WITH today as (
-        SELECT id, max_tmpf from summary_2017 s JOIN stations t
+        SELECT id, max_tmpf from summary_2020 s JOIN stations t
         ON (s.iemid = t.iemid) WHERE t.network in ('IA_ASOS', 'AWOS')
-        and day = '2017-01-18'),
+        and day = '2020-11-03'),
         agg as (
         SELECT t.id, max(day) from summary s, stations t, today t2
         WHERE s.iemid = t.iemid and t.id = t2.id and
-        t.network in ('IA_ASOS', 'AWOS') and day > '2016-12-01'
-        and day < '2017-01-18' and s.max_tmpf >= t2.max_tmpf
+        t.network in ('IA_ASOS', 'AWOS') and to_char(day, 'mmdd') >= '1103'
+        and day < '2020-11-03' and s.max_tmpf >= t2.max_tmpf
         GROUP by t.id)
-        SELECT id, max from agg ORDER by max ASC
+        SELECT id, max from agg ORDER by max DESC
         """
     )
 
@@ -34,19 +34,18 @@ def main():
     for row in cursor:
         lats.append(nt.sts[row[0]]["lat"])
         lons.append(nt.sts[row[0]]["lon"])
-        vals.append(row[1].strftime("%-m/%-d"))
+        vals.append(row[1].strftime("%-m/%-d\n%Y"))
         colors.append("r" if row[1] == datetime.date(2016, 12, 25) else "k")
 
     m = MapPlot(
         continentalcolor="white",
         title=(
-            "Iowa ASOS/AWOS Last Date of as Warm of "
-            "Daily High as 18 Jan 2017"
+            "Iowa ASOS/AWOS Last Nov 3 - Dec 31 Date as Warm as " "3 Nov 2020"
         ),
     )
     m.plot_values(lons, lats, vals, fmt="%s", color=colors, labelbuffer=5)
     m.drawcounties()
-    m.postprocess(filename="170119.png")
+    m.postprocess(filename="201104.png")
 
 
 if __name__ == "__main__":
