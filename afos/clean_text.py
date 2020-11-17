b82358de7efe83ff3ffb273f284ae2f1ab0c1662
akrherz <akrherz@iastate.edu>
2020-11-17 12:16:44 -0600

    alter procedure

--- afos/clean_text.py
+++ afos/clean_text.py
@@ -2,52 +2,47 @@
 import sys
 import re
 
-from pyiem.util import get_dbconn
+from pyiem.util import get_dbconn, noaaport_text
+import pandas as pd
 
 MYRE = re.compile("[A-Z0-9]{5,6} [A-Z]{4} [0-9]{5,6}")
 
 
-def main(argv):
-    """Go Main Go."""
-    table = argv[1]
-    pgconn = get_dbconn("afos")
+def do_date(pgconn, date):
+    """Process a given date."""
     cursor = pgconn.cursor()
     cursor2 = pgconn.cursor()
     cursor.execute(
-        """
-        SELECT data, source, wmo, pil, entered from """
-        + table
-        + """
-        ORDER by length(data) ASC
-    """
+        "SELECT data, ctid, tableoid::regclass as tablename "
+        "from products WHERE entered >= %s and "
+        "entered <= %s and substr(data, 12, 1) = ':'",
+        (
+            date.strftime("%Y-%m-%d 00:00+00"),
+            date.strftime("%Y-%m-%d 23:59+00"),
+        ),
     )
-    updated = 0
+    updates = 0
     for row in cursor:
-        # This line was found to be nearly always bad
-        testline = row[0].split("\r\r\n")[-2]
-        m = MYRE.search(testline)
-        if not m:
-            continue
-        # update time
-        newdata = "\r\r\n".join(row[0].split("\r\r\n")[:-2])
+        meat = row[0].split(":", 2)
+        if meat[1].find("\n") > -1:
+            print(meat)
+            sys.exit()
+        text = noaaport_text(meat[2])
         cursor2.execute(
-            """
-            UPDATE """
-            + table
-            + """ SET data = %s
-            WHERE source = %s and wmo = %s and pil = %s and entered = %s
-        """,
-            (newdata, row[1], row[2], row[3], row[4]),
+            f"UPDATE {row[2]} SET data = %s where ctid = %s", (text, row[1])
         )
-        updated += 1
-        if updated % 1000 == 0:
-            cursor2.close()
-            pgconn.commit()
-            cursor2 = pgconn.cursor()
-
-    print("updated %s rows" % (updated,))
+        updates += cursor2.rowcount
+    print(f"{date} {updates} updates")
     cursor2.close()
     pgconn.commit()
+
+
+def main(argv):
+    """Go Main Go."""
+    year = int(argv[1])
+    pgconn = get_dbconn("afos")
+    for date in pd.date_range(f"{year}/01/01", f"{year}/12/31"):
+        do_date(pgconn, date)
 
 
 if __name__ == "__main__":
