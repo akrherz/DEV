2c011337226df1af42ea760246b320a370ece599
akrherz <akrherz@iastate.edu>
2020-12-03 21:50:06 -0600

    tweak a stat

--- election/plotdelta.py
+++ election/plotdelta.py
@@ -3,12 +3,20 @@
 from pyiem.plot.use_agg import plt
 import pandas as pd
 
+TV = "Total Votes"
+
 
 def main():
     """Go Main Go."""
-    xl = pd.ExcelFile("/tmp/election.xlsx")
+    xl = pd.ExcelFile("election.xlsx")
     df2016 = pd.read_excel(xl, "2016", index_col="State")
     df2020 = pd.read_excel(xl, "2020", index_col="State")
+    df2016["third_percent"] = (
+        (df2016[TV] - df2016["Trump"] - df2016["Clinton"]) / df2016[TV] * 100.0
+    )
+    df2020["third_percent"] = (
+        (df2020[TV] - df2020["Trump"] - df2020["Biden"]) / df2020[TV] * 100.0
+    )
     df2016["trump_percent"] = df2016["Trump"] / df2016["Total Votes"] * 100
     df2016["clinton_percent"] = df2016["Clinton"] / df2016["Total Votes"] * 100
     df2016["trump_margin"] = (
@@ -17,13 +25,11 @@ def main():
     df2020["trump_percent"] = df2020["Trump"] / df2020["Total Votes"] * 100
     df2020["biden_percent"] = df2020["Biden"] / df2020["Total Votes"] * 100
     df2020["trump_margin"] = df2020["trump_percent"] - df2020["biden_percent"]
+    tip = (df2016["third_percent"] - df2020["third_percent"]) / 2.0
+    print(df2016["third_percent"])
+    delta = df2020["trump_percent"] - df2016["trump_percent"] - tip
 
     (fig, ax) = plt.subplots(1, 1, figsize=(12, 6.75))
-    delta = (
-        (df2020["Total Votes"] - df2016["Total Votes"])
-        / df2016["Total Votes"]
-        * 100
-    )
     delta = delta.sort_values()
     bars = ax.barh(range(len(delta.index)), delta.values)
     for i, (mybar, state, val) in enumerate(
@@ -43,10 +49,13 @@ def main():
         ax.text(x, i, state, fontsize=10, va="center", ha=ha)
     ax.set_ylim(-0.7, 49.7)
     ax.set_yticks([])
-    ax.set_xlim(0, 40.0)
-    ax.set_xlabel("Voter Turnout Percent Change (2020 - 2016)", fontsize=20)
+    ax.set_xlim(-6, 3.0)
+    ax.set_xlabel(
+        "Absolute Percentage Point Change (2020 - 2016)", fontsize=20
+    )
     ax.set_title(
-        "2020 vs 2016 Percentage Change in Total Votes Cast", fontsize=26
+        "2020 vs 2016 Trump Percentage Point Change\nAfter 3rd Party Percentage Point 2016-2020 Delta Equally Removed",
+        fontsize=16,
     )
     fig.text(0.05, 0.01, "@akrherz, 3 Dec 2020. Data: uselectionatlas.org")
     fig.savefig("test.png")
