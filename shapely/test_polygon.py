20f98e217fc3fae8a6b5a3c5abd5501ee89dd02f
akrherz <akrherz@iastate.edu>
2020-12-28 09:54:54 -0600

    add shapely test example

---
+++ shapely/test_polygon.py
@@ -0,0 +1,19 @@
+"""See what shapely says."""
+
+from shapely.geometry import LinearRing, Polygon
+
+
+def main():
+    """Go Main Go."""
+    X = [-77.66, -77.45, -77.5, -77.73, -77.81, -77.81, -77.77, -77.66]
+    Y = [35.5, 35.44, 35.39, 35.45, 35.59, 35.58, 35.61, 35.5]
+
+    lr = LinearRing(zip(X, Y))
+    assert lr.is_valid
+
+    poly = Polygon(lr)
+    assert poly.is_valid
+
+
+if __name__ == "__main__":
+    main()
