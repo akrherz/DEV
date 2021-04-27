98745c9b5a121fa5ed073fbd26992d58e1ba9ea1
akrherz <akrherz@iastate.edu>
2021-04-27 08:03:08 -0500

    iterate

---
+++ climodat/add_climatesites_from_acis.py
@@ -0,0 +1,152 @@
+"""Review what ACIS says for potential climate sites."""
+# stdlib
+from datetime import datetime, date, timedelta
+import sys
+
+# Third Party
+from pyiem.network import Table as NetworkTable
+from pyiem.util import get_dbconn, logger, convert_value
+from pyiem.reference import nwsli2state
+import requests
+
+LOG = logger()
+state2nwsli = dict([(value, key) for key, value in nwsli2state.items()])
+
+
+def make_dates(tokens):
+    """Convert to dates."""
+    if not tokens:
+        return date(2000, 1, 1), date(2000, 1, 1)
+    fmt = "%Y-%m-%d"
+    return datetime.strptime(tokens[0], fmt), datetime.strptime(tokens[1], fmt)
+
+
+def process_state(state, data):
+    """Do Some Magic."""
+    pgconn = get_dbconn("mesosite")
+    cursor = pgconn.cursor()
+    COOP_SITES = NetworkTable(f"{state}_COOP", only_online=False)
+    DCP_SITES = NetworkTable(f"{state}_DCP", only_online=False)
+    CLIMATE_SITES = NetworkTable(f"{state}CLIMATE", only_online=False)
+    added = []
+    known = 0
+    dups = 0
+    for meta in data["meta"]:
+        coops = [
+            s.split()[0]
+            for s in meta["sids"]
+            if s.find(f"{state2nwsli[state]} 7") > -1
+        ]
+        acis_sids = [s.split()[0] for s in meta["sids"] if s.endswith(" 2")]
+        if not acis_sids:
+            # LOG.info("failed to get 2-type station for %s", meta["sids"])
+            continue
+        # Check for duplicate listed stations
+        for sid in acis_sids:
+            if sid in added:
+                dups += 1
+                LOG.info("Skipping %s as redundant station id", sid)
+                continue
+        acis = acis_sids[0]
+        if len(acis_sids) > 1:
+            LOG.info("Duplicated 2-type IDs %s, %s", acis_sids, meta["name"])
+            for _acis in acis_sids:
+                lookup = state + _acis[-4:]
+                if lookup in CLIMATE_SITES.sts:
+                    acis = _acis
+        if len(acis) != 6:
+            LOG.info("ACIS id %s not len=6 %s", acis, meta["sids"])
+            continue
+        clstation = state + acis[-4:]
+        # We already know about this site
+        if clstation in CLIMATE_SITES.sts:
+            known += 1
+            continue
+        (maxt, pcpn) = [make_dates(r) for r in meta["valid_daterange"]]
+        if (maxt[1] - maxt[0]) < timedelta(days=365) and (
+            pcpn[1] - pcpn[0]
+        ) < timedelta(days=365):
+            LOG.info("Skipping %s with too little data", clstation)
+            continue
+        nwsli = None
+        if len(coops) > 1:
+            LOG.info("Duplicated coops %s %s", meta["sids"], meta["name"])
+            for _nwsli in coops:
+                if _nwsli in COOP_SITES.sts:
+                    nwsli = _nwsli
+        elif coops:
+            nwsli = coops[0]
+        # This station is online if temp or precip last date is this year
+        online = date.today().year in [maxt[1].year, pcpn[1].year]
+        tracking = None
+        if online and nwsli is not None:
+            if nwsli in COOP_SITES.sts:
+                tracking = f"{nwsli}|{state}_COOP"
+            elif nwsli in DCP_SITES.sts:
+                LOG.info("Ignoring %s as tracks %s[DCP]", clstation, nwsli)
+                continue
+            else:
+                LOG.info("--------- Add COOP? %s", nwsli)
+        if online and tracking is None:
+            LOG.info(
+                "skipping add for online %s without station: %s",
+                clstation,
+                meta,
+            )
+            continue
+        LOG.info("Add %s online: %s %s", clstation, online, tracking)
+        CLIMATE_SITES.sts[clstation] = {}
+        added.append(acis)
+        cursor.execute(
+            "INSERT into stations(id, name, network, country, state, "
+            "plot_name, online, metasite, geom, elevation) VALUES "
+            "(%s, %s, %s, %s, %s, %s, %s, 't', 'SRID=4326;POINT(%s %s)', %s) "
+            "RETURNING iemid",
+            (
+                clstation,
+                meta["name"],
+                f"{state}CLIMATE",
+                "US",
+                state,
+                meta["name"],
+                online,
+                meta["ll"][0],
+                meta["ll"][1],
+                convert_value(meta.get("elev", -999), "feet", "meter"),
+            ),
+        )
+        iemid = cursor.fetchone()[0]
+        # setup the tracking attribute
+        if tracking:
+            cursor.execute(
+                "INSERT into station_attributes(iemid, attr, value) "
+                "VALUES (%s, %s, %s)",
+                (iemid, "TRACKS_STATION", tracking),
+            )
+
+    cursor.close()
+    pgconn.commit()
+    LOG.info("Added: %s Known: %s Dups: %s", len(added), known, dups)
+
+
+def main(argv):
+    """Go Main Go."""
+    state = argv[1]
+    req = requests.post(
+        "http://data.rcc-acis.org/StnMeta",
+        json={
+            "meta": "valid_daterange,sids,name,ll,elev",
+            "elems": "maxt,pcpn",
+            "state": state,
+        },
+    )
+    data = req.json()
+    if "meta" not in data:
+        LOG.info("Got status_code %s with no meta", req.status_code)
+        LOG.info(req.content)
+        return
+    process_state(state, data)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
