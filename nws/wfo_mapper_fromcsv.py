ee6d79208289d7638abaa881be4f2694f6377379
akrherz <akrherz@iastate.edu>
2021-05-08 06:48:38 -0500

    ranks for issuance counts
    https://twitter.com/akrherz/status/1390996474272550912

--- nws/wfo_mapper_fromcsv.py
+++ nws/wfo_mapper_fromcsv.py
@@ -10,26 +10,26 @@ import pandas as pd
 def main():
     """Go Main"""
     df = pd.read_csv("wfo.csv", index_col=None)
-    df["wfo"] = df["wfo"].apply(lambda x: x[1:])
+    # df["wfo"] = df["wfo"].apply(lambda x: x[1:])
     df = df.set_index("wfo")
     print(df)
     # df["count"] = 100. - df["count"]
-    vals = df["year"].to_dict()
-    # bins = list(range(12))
-    bins = np.arange(2012, 2022, 1)
+    vals = df["rank"].to_dict()
+    bins = list(range(1, 22, 2))
+    # bins = np.arange(2012, 2022, 1)
     # bins = [0, 0.1, 0.2, 0.5, 0.75, 1, 2, 5, 10]
     # bins = [1, 5, 10, 25, 50, 75, 100, 125]
     # bins = np.arange(0, 17, 2)
     cmap = get_cmap("Greens")
-    cmap.set_over("lightyellow")
+    # cmap.set_over("lightyellow")
     mp = MapPlot(
         sector="nws",
         continentalcolor="white",
         twitter=True,
-        title=(
-            "Year of Last Hazardous Weather Outlook (HWO) by NWS Forecast Office"
+        title=("2021 Rank for Least Severe T'Storm + Tornado Warnings Issued"),
+        subtitle=(
+            "based on unofficial IEM archives over 1 Jan - 7 May, 2002-2021, rank of 1 indicates least number issued, may include ties"
         ),
-        subtitle=("based on IEM processing ending 19 April 2021"),
     )
     mp.fill_cwas(
         vals,
@@ -37,8 +37,8 @@ def main():
         lblformat="%.0f",  # , labels=labels,
         cmap=cmap,
         ilabel=True,  # clevlabels=clevlabels,
-        units="count",
-        extend="max",
+        units="rank out of 20",
+        extend="neither",
         spacing="proportional",
     )
 
