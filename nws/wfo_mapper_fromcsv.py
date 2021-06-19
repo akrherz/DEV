47c6dc66a43c6c7d8d8047efe6f316cd942a5758
akrherz <akrherz@iastate.edu>
2021-06-19 12:18:21 -0500

    plot of simultaneous warnings
    https://twitter.com/akrherz/status/1406292864926208007

--- nws/wfo_mapper_fromcsv.py
+++ nws/wfo_mapper_fromcsv.py
@@ -9,32 +9,33 @@ import pandas as pd
 
 def main():
     """Go Main"""
-    df = pd.read_csv("wfo.csv", index_col=None)
+    df = pd.read_csv("wfo.csv", index_col=None, skipinitialspace=True)
+    df["utc_valid"] = pd.to_datetime(df["utc_valid"])
     # df["wfo"] = df["wfo"].apply(lambda x: x[1:])
     df = df.set_index("wfo")
     print(df)
     # df["count"] = 100. - df["count"]
     # data = (df["count"] - df["avg"]).round(0).astype(int)
     # data = (df["rank"]).round(0).astype(int)
-    vals = ((df["count"] - df["avg"]) / df["stddev"]).to_dict()
+    vals = df["count"]
     print(vals["PSR"])
-    bins = list(range(-100, 101, 25))
+    bins = list(range(1, 31, 3))
     # bins = np.arange(2012, 2022, 1)
     # bins = [0, 0.1, 0.2, 0.5, 0.75, 1, 2, 5, 10]
     # bins = [1, 5, 10, 25, 50, 75, 100, 125]
-    bins = np.arange(-3.1, 3.1, 0.5)
-    cmap = get_cmap("RdBu")
+    # bins = np.arange(-3.1, 3.1, 0.5)
+    cmap = get_cmap("jet")
     # cmap.set_over("lightyellow")
     mp = MapPlot(
         sector="nws",
         continentalcolor="white",
         twitter=True,
         title=(
-            "2021 Standardized Departure of Total Severe T'Storm + Tornado Warnings Issued "
-            "vs Past 20 Years"
+            "2005-2021 Maximum Number of Simultaneous SVR + TOR + FFW + SMW "
+            "Warnings"
         ),
         subtitle=(
-            "based on unofficial IEM archives over 1 Jan - 2 June, 2002-2021 "
+            "based on unofficial IEM archives up till 19 June 2021"
             # f"nationwide depature {data.sum():.0f} warnings"
             # f"rank of 1 is least number"
         ),
@@ -42,12 +43,12 @@ def main():
     mp.fill_cwas(
         vals,
         bins=bins,
-        lblformat="%.1f",  # , labels=labels,
+        lblformat="%.0f",  # , labels=labels,
         labelbuffer=0,
         cmap=cmap,
         ilabel=True,  # clevlabels=clevlabels,
-        units="sigma",
-        extend="both",
+        units="count",
+        extend="neither",
         spacing="proportional",
     )
 
