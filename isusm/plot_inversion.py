e0fa89f2614745530fbdc1804ab57ecb18deb112
akrherz <akrherz@iastate.edu>
2021-07-06 11:32:18 -0500

    improve plot

--- isusm/plot_inversion.py
+++ isusm/plot_inversion.py
@@ -1,4 +1,5 @@
 """Exp plot."""
+from itertools import combinations
 
 import pandas as pd
 from pyiem.plot.use_agg import plt
@@ -6,39 +7,39 @@ from pyiem.plot.use_agg import plt
 
 def main():
     """Go Main Go."""
-    df = pd.read_csv("AEA_Inversion_MinSI.dat", skiprows=[0, 2, 3])
+    df = pd.read_csv("AEA_Inversion2_MinSI.dat", skiprows=[0, 2, 3])
     df["valid"] = pd.to_datetime(df["TIMESTAMP"])
 
     fig, axes = plt.subplots(3, 1, figsize=(10.24, 7.68))
     ax = axes[0]
     ax.set_title(
-        "Ames AEA Farm 8-14 June 2021 Inversion Station Equal Height Test"
+        "Ames AEA Farm 24 Jun -6 Jul 2021 Inversion Station Equal Height Test"
     )
     ax.plot(df["valid"], df["T5_Avg"], label="CS215 '5'")
-    ax.plot(df["valid"], df["T10_Avg"], label="T107 '10'")
-    ax.plot(df["valid"], df["T15_Avg"], label="T107 '15'")
+    ax.plot(df["valid"], df["T10_Avg"], label="CS215 '10'")
+    ax.plot(df["valid"], df["T15_Avg"], label="CS215 '15'")
     ax.grid(True)
     ax.set_ylabel("Temperature C")
     ax.legend(ncol=3)
 
     cols = ["T5_Avg", "T10_Avg", "T15_Avg"]
     df["range"] = df[cols].max(axis=1) - df[cols].min(axis=1)
-    cols = ["T10_Avg", "T15_Avg"]
-    df["range2"] = df[cols].max(axis=1) - df[cols].min(axis=1)
+    # cols = ["T10_Avg", "T15_Avg"]
+    # df["range2"] = df[cols].max(axis=1) - df[cols].min(axis=1)
     ax = axes[1]
     ax.plot(df["valid"], df["range"], label="All Three")
-    ax.plot(df["valid"], df["range2"], label="Just T107")
+    # ax.plot(df["valid"], df["range2"], label="Just T107")
     ax.set_ylabel("Range of Temperature Values C")
     ax.grid(True)
     ax.legend()
 
-    df["delta"] = df["T5_Avg"] - df[["T10_Avg", "T15_Avg"]].mean(axis=1)
     ax = axes[2]
-    ax.plot(df["valid"], df["delta"], label="All Three")
-    ax.set_ylabel("CS215 minus T107 Average C")
+    for (one, two) in combinations([5, 10, 15], 2):
+        df["delta"] = df[f"T{one}_Avg"] - df[f"T{two}_Avg"]
+        ax.plot(df["valid"], df["delta"], label=f"{one} - {two}")
+        ax.set_ylabel("CS215 Difference C")
     ax.grid(True)
     ax.legend()
-
     fig.savefig("inversion.png")
 
 
