27570b58a12ae45d86dc5a48268b20bbc78465ec
akrherz <akrherz@iastate.edu>
2021-07-07 10:17:17 -0500

    support day 2 and 3 checks

--- spc/compare_outlook.py
+++ spc/compare_outlook.py
@@ -1,20 +1,27 @@
 """Compare IEM vs SPC, to see who is better!"""
 import subprocess
+import datetime
 
 import requests
 import pandas as pd
 import geopandas as gpd
-from pyiem.util import get_dbconn, logger
+import pytz
+from pyiem.util import get_dbconn, logger, utc
 
 LOG = logger()
 BASEURL = "https://www.spc.noaa.gov/products/outlook/archive"
 CANONICAL = {
-    1: "0100",
-    6: "1200",
-    13: "1300",
-    16: "1630",
-    20: "2000",
+    # 1: "0100",
+    # 6: "1200",
+    # 7: "0700",  # 2
+    8: "0830",
+    # 13: "1300",
+    # 16: "1630",
+    # 17: "1730",  # 2
+    # 20: "2000",
 }
+DAY = 3
+PIL = "PTSDY3"
 
 
 def reprocess(product_id, valid):
@@ -36,23 +43,33 @@ def reprocess(product_id, valid):
 
 def run(valid, cycle, secondtrip):
     """Do the comparison for a given valid time."""
+    label = CANONICAL[cycle]
+    if cycle == 7:
+        lts = valid.astimezone(pytz.timezone("America/Chicago"))
+        if lts.dst():
+            label = "0600"
+    if cycle == 8:
+        lts = valid.astimezone(pytz.timezone("America/Chicago"))
+        if lts.dst():
+            label = "0730"
     uri = valid.strftime(
-        f"{BASEURL}/%Y/day1otlk_%Y%m%d_{CANONICAL[cycle]}_cat.lyr.geojson"
+        f"{BASEURL}/%Y/day{DAY}otlk_%Y%m%d_{label}_cat.lyr.geojson"
     )
     try:
         spc = gpd.read_file(uri)
     except Exception as exp:
         LOG.info("Failed fetching %s with %s", uri, exp)
         return False
+    dbvalid = valid + datetime.timedelta(days=(DAY - 1))
     iem = gpd.read_postgis(
         "SELECT threshold, category, geom, product_id from "
         "spc_outlook_geometries g JOIN "
         "spc_outlook o on (g.spc_outlook_id = o.id) WHERE outlook_type = 'C' "
-        f"and date(issue at time zone 'UTC') = '{valid:%Y-%m-%d}' and "
-        "day = 1 and cycle = %s and "
+        f"and date(issue at time zone 'UTC') = '{dbvalid:%Y-%m-%d}' and "
+        "day = %s and cycle = %s and "
         "category = 'CATEGORICAL'",
         get_dbconn("postgis"),
-        params=(cycle,),
+        params=(DAY, cycle),
         geom_col="geom",
     )
     for idx, row in spc.iterrows():
@@ -62,7 +79,7 @@ def run(valid, cycle, secondtrip):
         spc_size = row["geometry"].area
         if iemrow.empty:
             iem_size = -1
-            product_id = f"{row['ISSUE']}-KWNS-WUUS01-PTSDY1"
+            product_id = f"{row['ISSUE']}-KWNS-WUUS0{DAY}-{PIL}"
             LOG.info("setting product_id to %s", product_id)
         else:
             product_id = iemrow["product_id"].iloc[0]
@@ -82,10 +99,11 @@ def run(valid, cycle, secondtrip):
 
 def main():
     """Go Main Go."""
-    for date in pd.date_range("2020/03/08", "2021/05/17"):
+    for date in pd.date_range("2019/11/01", "2021/07/07"):
+        valid = utc(date.year, date.month, date.day)
         for cycle in CANONICAL:
-            if run(date, cycle, False):
-                run(date, cycle, True)
+            if run(valid, cycle, False):
+                run(valid, cycle, True)
 
 
 if __name__ == "__main__":
