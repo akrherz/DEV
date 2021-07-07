4ea4f19525f830fccb07f58ea0df976658befd10
akrherz <akrherz@iastate.edu>
2021-07-06 23:14:28 -0500

    make twitter animation
    https://twitter.com/akrherz/status/1412624840503267331

--- warnings/simple_county.py
+++ warnings/simple_county.py
@@ -1,24 +1,28 @@
 """Map UGC stuff."""
+import sys
+import subprocess
 
 from pandas.io.sql import read_sql
-import numpy as np
+import pytz
 from pyiem.plot import MapPlot, get_cmap
-from pyiem.util import get_dbconn
+from pyiem.util import get_dbconn, utc
 
 
-def main():
+def main(argv):
     """Go Main Go."""
+    hr = int(argv[1])
+    valid = utc(2021, 7, 7, hr).astimezone(pytz.timezone("America/Chicago"))
     pgconn = get_dbconn("postgis")
 
-    cmap = get_cmap("terrain")
-    cmap.set_over("red")
+    cmap = get_cmap("jet")
+    # cmap.set_over("purple")
 
     mp = MapPlot(
         sector="conus",
         axisbg="#EEEEEE",
         title=(
             "Percentage of All Tornado Watches for Location "
-            "Active at 3 AM CDT (8 UTC)"
+            "Active at %s CDT (%s UTC)" % (valid.strftime("%-I %p"), hr)
         ),
         subtitle=("Based on unofficial IEM archives (2005-2021)"),
         cwas=True,
@@ -34,12 +38,13 @@ def main():
     where phenomena = 'TO' and significance = 'A'), totals as (select ugc,
     count(*) from warnings where phenomena = 'TO' and significance = 'A'
     GROUP by ugc), morning as (select ugc, count(*) from events where
-    extract(hour from ts) = 8 and extract(minute from ts) = 0 GROUP by ugc)
+    extract(hour from ts) = %s and extract(minute from ts) = 0 GROUP by ugc)
     select t.ugc, t.count as events, m.count as morning
     from totals t LEFT JOIN morning m on
     (t.ugc = m.ugc)
     """,
         pgconn,
+        params=(hr,),
         index_col="ugc",
     )
     df = df.fillna(0)
@@ -47,15 +52,19 @@ def main():
     print(df["percent"].max())
     mp.fill_ugcs(
         df["percent"].to_dict(),
-        bins=[0, 0.1, 5, 10, 15, 20, 25, 30, 35, 40],
+        bins=[0, 0.1, 5, 10, 20, 30, 40, 50, 60, 70, 80],
         cmap=cmap,
         units="%",
         spacing="uniform",
         extend="max",
     )
     mp.draw_cwas()
-    mp.postprocess(filename="test.png")
+    i = hr - 12 if hr >= 12 else hr + 12
+    mp.fig.savefig(f"frame_{i:02.0f}.png")
+    subprocess.call(
+        f"convert frame_{i:02.0f}.png frame_{i:02.0f}.gif", shell=True
+    )
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
