dc0a080440598946e32896e9e3d70e64e82ef788
akrherz <akrherz@iastate.edu>
2021-08-16 07:16:02 -0500

    16 aug feature

--- isusm/plot_srad.py
+++ isusm/plot_srad.py
@@ -14,18 +14,18 @@ def main():
         SELECT valid, slrkj_tot * 1000. / 60. as wmps,
         extract(day from valid) as day,
         extract(minute from valid) + extract(hour from valid) * 60. as minute
-        from sm_minute where station = 'AEEI4'
-        and valid > '2019-06-12' ORDER by valid ASC
+        from sm_minute where station = 'NWLI4'
+        and valid > '2021-08-14' ORDER by valid ASC
     """,
         pgconn,
         index_col=None,
     )
 
     (fig, ax) = plt.subplots(1, 1)
-    df2 = df[df["day"] == 12]
-    ax.plot(df2["minute"].values, df2["wmps"].values, label="12 June 2019")
-    df2 = df[df["day"] == 13]
-    ax.plot(df2["minute"].values, df2["wmps"].values, label="13 June 2019")
+    df2 = df[df["day"] == 14]
+    ax.plot(df2["minute"].values, df2["wmps"].values, label="14 August 2021")
+    df2 = df[df["day"] == 15]
+    ax.plot(df2["minute"].values, df2["wmps"].values, label="15 August 2021")
     ax.grid(True)
     ax.set_xlim(300, 22 * 60)
     ax.set_xticks(range(360, 22 * 60 + 1, 120))
@@ -45,7 +45,7 @@ def main():
     ax.set_xlabel("Central Daylight Time")
     ax.legend()
     ax.set_ylabel("One Minute Averaged Solar Rad [W m-2]")
-    ax.set_title("ISU Soil Moisture -- Ames Horticulture Farm")
+    ax.set_title("ISU Soil Moisture -- Newell: Allee ISU Research Farm")
     fig.savefig("test.png")
 
 
