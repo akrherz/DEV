c5e52f3767c08bb88a38dbc2e3c89968bcae162f
akrherz <akrherz@iastate.edu>
2021-09-07 20:39:41 -0500

    run all the files at once

--- imerg/extract_timeseries.py
+++ imerg/extract_timeseries.py
@@ -1,5 +1,4 @@
 """Create simple timeseries files."""
-import sys
 import os
 
 from tqdm import tqdm
@@ -8,45 +7,45 @@ from pyiem.util import logger
 import osgeo.gdal as gdal
 
 LOG = logger()
+FMT = "%Y-%m-%d %H:%M"
 
 
-def main(argv):
+def main():
     """Go Main Go."""
-    lon = float(argv[1])
-    lat = float(argv[2])
-    if len(argv) > 3:
-        outfn = argv[3]
-    else:
-        outfn = "imerg_%s_%s.csv" % (lon, lat)
-    # Ankeny 41.74373,-93.5909
-    y = int(900 - (lat * 10))
-    x = int(1800 - (lon * -10))
-    LOG.info("Running for lon:%s lat:%s y:%s x:%s", lon, lat, y, x)
+    jobs = []
+    for line in open("QUEUE"):
+        lon, lat, fn = line.strip().split()
+        y = int(900 - (float(lat) * 10))
+        x = int(1800 - (float(lon) * -10))
+        jobs.append([x, y, open(fn, "w")])
+        jobs[-1][2].write("valid,accum_mm\n")
     dates = pd.date_range(
         "2007/01/01",
         "2021/01/01 07:00",
         freq="1800s",
     )
     progress = tqdm(dates)
-    with open(outfn, "w") as outfp:
-        outfp.write("valid,accum_mm\n")
-        for dt in progress:
-            progress.set_description(dt.strftime("%Y%m%d%H%M"))
-            fn = dt.strftime(
-                "/mesonet/ARCHIVE/data/%Y/%m/%d/"
-                "GIS/imerg/p30m_%Y%m%d%H%M.png"
-            )
-            if not os.path.isfile(fn):
-                precip = 0
-            else:
-                data = gdal.Open(fn).ReadAsArray()
-                imgval = data[y, x]
+    for dt in progress:
+        progress.set_description(dt.strftime("%Y%m%d%H%M"))
+        fn = dt.strftime(
+            "/mesonet/ARCHIVE/data/%Y/%m/%d/" "GIS/imerg/p30m_%Y%m%d%H%M.png"
+        )
+        if not os.path.isfile(fn):
+            for j in jobs:
+                j[2].write("%s,%.2f\n" % (dt.strftime(FMT), 0))
+        else:
+            data = gdal.Open(fn).ReadAsArray()
+            for j in jobs:
+                imgval = data[j[1], j[0]]
                 if imgval < 200:
                     precip = imgval * 0.25
                 else:
                     precip = 50.0 + (imgval - 200)
-            outfp.write("%s,%.2f\n" % (dt.strftime("%Y-%m-%d %H:%M"), precip))
+                j[2].write("%s,%.2f\n" % (dt.strftime(FMT), precip))
+
+    for j in jobs:
+        j[2].close()
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
