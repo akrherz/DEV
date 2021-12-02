06037962fe8049ec3cee526d20a532be2867ca3f
akrherz <akrherz@iastate.edu>
2021-12-02 10:34:38 -0600

    zap climodat sites that are pure estimates

---
+++ climodat/zap_estimate_only.py
@@ -0,0 +1,55 @@
+"""Remove realtime status for sites that are getting 100% estimates.
+
+We generally do not want to have climodat realtime stations that are purely
+estimated for one of the variables.  Some sites only report temp or precip
+for example.  This script will remove realtime status for such sites.
+"""
+
+from pandas.io.sql import read_sql
+from pyiem.reference import state_names
+from pyiem.util import get_dbconn
+
+
+def do(ccursor, mcursor, state):
+    """Do great things."""
+    df = read_sql(
+        "select station, "
+        "sum(case when temp_estimated then 1 else 0 end) as testimated, "
+        "sum(case when precip_estimated then 1 else 0 end) as pestimated, "
+        "count(*) as obs "
+        f"from alldata_{state} where year = 2021 and "
+        "substr(station, 3, 1) not in ('T', 'C') and "
+        "substr(station, 3, 4) != '0000' GROUP by station",
+        get_dbconn("coop"),
+        index_col="station",
+    )
+    df2 = df.query("obs == testimated or obs == pestimated")
+    for station in df2.index.values:
+        # Set online to false in mesosite
+        mcursor.execute(
+            "UPDATE stations SET online = 'f' WHERE id = %s", (station,)
+        )
+        # Truncate alldata for this station and year
+        ccursor.execute(
+            f"DELETE from alldata_{state} WHERE station = %s and year = 2021",
+            (station,),
+        )
+        print(f"Culling {ccursor.rowcount} rows for 2021 data for {station}")
+
+
+def main():
+    """Workflow."""
+    coopdb = get_dbconn("coop")
+    mesositedb = get_dbconn("mesosite")
+    for state in state_names:
+        ccursor = coopdb.cursor()
+        mcursor = mesositedb.cursor()
+        do(ccursor, mcursor, state)
+        ccursor.close()
+        mcursor.close()
+        coopdb.commit()
+        mesositedb.commit()
+
+
+if __name__ == "__main__":
+    main()
