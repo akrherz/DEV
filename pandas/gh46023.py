f4e55c013b53c817ac0e6d568fbbbac4775252d0
akrherz <akrherz@iastate.edu>
2022-02-18 10:49:33 -0600

    add pandas doc issue repo

---
+++ pandas/gh46023.py
@@ -0,0 +1,15 @@
+"""
+https://github.com/pandas-dev/pandas/issues/46023
+"""
+import subprocess
+import os
+import pandas as pd
+
+# Get process id of the current process
+pid = os.getpid()
+
+subprocess.call(f"lsof -p {pid} | grep postgres", shell=True)
+print("running read_sql")
+pd.read_sql("select 1", "postgresql://localhost/akrherz")
+
+subprocess.call(f"lsof -p {pid} | grep postgres", shell=True)
