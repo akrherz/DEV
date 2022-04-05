f05ba1211b9ad4b065f5d860b96509a15394dae8
akrherz <akrherz@iastate.edu>
2022-04-05 11:34:32 -0500

    update for current usage

--- roads/compute_archive_begin_end.py
+++ roads/compute_archive_begin_end.py
@@ -1,6 +1,6 @@
 """Figure out what our time domain is for our segments."""
 
-from pyiem.util import get_dbconn
+from pyiem.util import get_dbconn, get_sqlalchemy_conn
 import pandas as pd
 from pandas.io.sql import read_sql
 
@@ -8,25 +8,21 @@ from pandas.io.sql import read_sql
 def main():
     """Go Main Go."""
     pgconn = get_dbconn("postgis")
-    base = read_sql(
-        """
-    select segid, archive_begin, archive_end from roads_base
-    """,
-        pgconn,
-        index_col="segid",
-    )
-    for year in range(2003, 2018):
-        table = "roads_%s_%s_log" % (year, year + 1)
-        df = read_sql(
-            """
-            SELECT segid, min(valid), max(valid) from """
-            + table
-            + """
-            WHERE valid is not null GROUP by segid
-        """,
-            pgconn,
+    with get_sqlalchemy_conn("postgis") as conn:
+        base = read_sql(
+            "select segid, archive_begin, archive_end from roads_base",
+            conn,
             index_col="segid",
         )
+    for year in range(2018, 2022):
+        table = f"roads_{year}_{year + 1}_log"
+        with get_sqlalchemy_conn("postgis") as conn:
+            df = read_sql(
+                f"SELECT segid, min(valid), max(valid) from {table} "
+                "WHERE valid is not null GROUP by segid",
+                conn,
+                index_col="segid",
+            )
         for segid, row in df.iterrows():
             curmin = base.at[segid, "archive_begin"]
             curmax = base.at[segid, "archive_end"]
@@ -39,14 +35,10 @@ def main():
     for segid, row in base.iterrows():
         if pd.isnull(row["archive_begin"]) or pd.isnull(row["archive_end"]):
             continue
-        print(
-            "%s %s -> %s" % (segid, row["archive_begin"], row["archive_end"])
-        )
+        print(f"{segid} {row['archive_begin']} -> {row['archive_end']}")
         cursor.execute(
-            """
-        update roads_base SET archive_begin = %s, archive_end = %s
-        where segid = %s
-        """,
+            "update roads_base SET archive_begin = %s, archive_end = %s "
+            "where segid = %s",
             (row["archive_begin"], row["archive_end"], segid),
         )
     cursor.close()
