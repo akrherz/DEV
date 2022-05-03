748d11551256c85d42f43669edd7702f1b4761ce
akrherz <akrherz@iastate.edu>
2022-05-03 12:27:33 -0500

    update cod lapse generation

--- frames/get.py
+++ frames/get.py
@@ -6,9 +6,8 @@ import requests
 
 def main():
     """Go Main"""
-    # Generate series of images between 0z and 12z on the 3rd of August
-    now = datetime.datetime(2019, 4, 21, 0, 0)
-    ets = datetime.datetime(2019, 6, 24, 0, 0)
+    now = datetime.datetime(2022, 4, 1, 0, 0)
+    ets = datetime.datetime(2022, 5, 1, 0, 0)
     interval = datetime.timedelta(minutes=1440)
 
     stepi = 0
@@ -16,34 +15,26 @@ def main():
         uri = now.strftime(
             (
                 "http://mtarchive.geol.iastate.edu/%Y/%m/%d/cod/sat/goes16/"
-                "global/fulldiskeast/abi08/000index.txt"
+                "regional/northcentral/abi11/000index.txt"
             )
         )
         req = requests.get(uri)
         if req.status_code != 200:
             print(uri)
             continue
-        fns = [None] * 24
         for line in req.content.decode("ascii").split("\n"):
             tokens = line.split("_")
             if len(tokens) != 3:
                 continue
-            hr = int(tokens[2][8:10])
-            if fns[hr] is None:
-                fns[hr] = line
-        for fn in fns:
-            if fn is None:
-                continue
             uri = now.strftime(
                 (
                     "http://mtarchive.geol.iastate.edu/%Y/%m/%d/cod/sat/goes16/"
-                    "global/fulldiskeast/abi08/" + fn
+                    f"regional/northcentral/abi11/{line}"
                 )
             )
             req = requests.get(uri)
-            fp = open("images/%05i.jpg" % (stepi,), "wb")
-            fp.write(req.content)
-            fp.close()
+            with open(f"images/{stepi:05.0f}.jpg", "wb") as fp:
+                fp.write(req.content)
             stepi += 1
 
         now += interval
