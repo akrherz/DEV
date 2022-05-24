f2ad3f0f79e7741ce9fe5eccb6da76d4b756fc7e
akrherz <akrherz@iastate.edu>
2022-05-23 20:49:24 -0500

    bling like crazy for 24 May feature

--- model/rtma_maxmin.py
+++ model/rtma_maxmin.py
@@ -2,62 +2,111 @@
 import os
 import datetime
 
+import matplotlib.colors as mpcolors
 import numpy as np
+import pandas as pd
 import pygrib
 from pyiem.plot.use_agg import plt
 from pyiem.plot import MapPlot
+from pyiem.plot.util import sector_setter, stretch_cmap
+from pyiem.reference import Z_FILL, LATLON, EPSG
+from pyiem.util import get_sqlalchemy_conn
+from scipy import stats
 from metpy.units import units
 
 
 def plot():
     """Do plotting work"""
-    cmap = plt.get_cmap("inferno_r")
+    with get_sqlalchemy_conn("iem") as conn:
+        df = pd.read_sql(
+            "select id, st_x(geom) as lon, st_y(geom) as lat, min_tmpf from "
+            "summary_2022 s JOIN stations t on (s.iemid = t.iemid) "
+            "WHERE t.network = 'IA_ASOS' and day = '2022-05-23' and "
+            "min_tmpf > 0",
+            conn,
+            index_col="id",
+        )
+    df["rtma"] = np.nan
+    cmap = plt.get_cmap("turbo")
     # cmap.set_under('black')
     # cmap.set_over('red')
-    minval = (np.load("minval.npy") * units.degK).to(units.degF)
-    maxval = (np.load("maxval.npy") * units.degK).to(units.degF)
-    diff = maxval - minval
+    minval = (np.load("minval.npy") * units.degK).to(units.degF).m
+    # maxval = (np.load("maxval.npy") * units.degK).to(units.degF)
+    # diff = maxval - minval
     lons = np.load("lons.npy")
     lats = np.load("lats.npy")
+    for i, row in df.iterrows():
+        # find closest point in the rtma grid
+        dist = np.sqrt((lons - row["lon"]) ** 2 + (lats - row["lat"]) ** 2)
+        (xidx, yidx) = np.unravel_index(dist.argmin(), dist.shape)
+        df.loc[i, "rtma"] = minval[xidx, yidx]
     mp = MapPlot(
-        sector="conus",
-        title=(
-            r"Difference between warmest 3 Oct and coldest 4 "
-            "Oct 2m Temperature"
-        ),
+        sector="iowa",
+        title=("Iowa 23 May 2022 Morning Minimum Temperature"),
         subtitle=(
-            "based on hourly NCEP Real-Time Mesoscale Analysis "
-            "(RTMA) ending midnight CDT"
+            "analysis based on hourly NCEP Real-Time Mesoscale Analysis "
+            "(RTMA), Iowa Airport Observations Overlaid"
         ),
     )
-    mp.ax.text(
-        0.5,
-        0.97,
-        (
-            r"Pixel Difference Range: %.1f$^\circ$F to %.1f$^\circ$F, "
-            r"Domain Analysis Range: %.1f$^\circ$F to %.1f$^\circ$F"
-        )
-        % (
-            np.min(diff).magnitude,
-            np.max(diff).magnitude,
-            np.min(minval).magnitude,
-            np.max(maxval).magnitude,
-        ),
-        transform=mp.ax.transAxes,
-        fontsize=12,
-        ha="center",
-        bbox=dict(pad=0, color="white"),
-        zorder=50,
+    ax = mp.fig.add_axes([0.05, 0.35, 0.29, 0.3])
+    df["delta"] = df["rtma"] - df["min_tmpf"]
+    ax.scatter(df["min_tmpf"], df["delta"])
+    # Add a fit line
+    slope, intercept, _r_value, _p_value, _std_err = stats.linregress(
+        df["min_tmpf"], df["delta"]
     )
+    ax.plot(
+        [32, 50],
+        [slope * x + intercept for x in [32, 50]],
+        color="red",
+        linewidth=2,
+    )
+    ax.grid(True)
+    ax.set_xlabel("Observed Min Temp (F)")
+    ax.set_ylabel("RTMA Min Temp Bias (F)", labelpad=3)
+    ax.set_title("RTMA Min Temp Bias vs Obs")
+    mp.panels[0].ax.set_position([0.35, 0.5, 0.55, 0.4])
+    clevs = range(30, 49, 2)
     mp.pcolormesh(
         lons,
         lats,
-        diff,
-        range(0, 61, 5),
+        minval,
+        clevs,
         cmap=cmap,
         clip_on=False,
         units=r"$^\circ$F",
     )
+    mp.plot_values(
+        df["lon"],
+        df["lat"],
+        df["min_tmpf"].values,
+        fmt="%.0f",
+        fontsize=10,
+        color="white",
+        labelbuffer=0,
+        outlinecolor="k",
+    )
+
+    mp.fig.text(
+        0.4,
+        0.46,
+        "With Temperature Dependent Bias Correction",
+        fontsize=14,
+    )
+    sector_setter(mp, [0.35, 0.05, 0.55, 0.4])
+    cmap = stretch_cmap(cmap, clevs, extend="both")
+    norm = mpcolors.BoundaryNorm(clevs, cmap.N)
+    mp.panels[1].pcolormesh(
+        lons,
+        lats,
+        minval - (slope * minval) - intercept,
+        norm=norm,
+        cmap=cmap,
+        zorder=Z_FILL,
+        crs=LATLON,
+    )
+    print(mp.panels[0].set_extent(mp.panels[1].get_extent(), EPSG[3857]))
+    mp.drawcounties()
     mp.postprocess(filename="test.png")
 
 
