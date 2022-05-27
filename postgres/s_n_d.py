a688d3ca276c2a3bc1e939e791fd47de4a607cc8
akrherz <akrherz@iastate.edu>
2022-05-27 13:02:22 -0500

    cleanup data only *updated* before 2 days ago

--- postgres/s_n_d.py
+++ postgres/s_n_d.py
@@ -28,48 +28,36 @@ def main():
 
     # Create summary table entries for two days from now
     day2 = today + datetime.timedelta(days=2)
-    sql = """
-        INSERT into summary_%s(iemid, day) select iemid, '%s' from current
-    """ % (
-        day2.year,
-        day2.strftime("%Y-%m-%d"),
+    sql = (
+        f"INSERT into summary_{day2.year}(iemid, day) "
+        f"select iemid, '{day2:%Y-%m-%d}' from current"
     )
     atomic(pgconn, sql)
 
     # Copy any data older than 12 AM from yesterday
     yest = today - datetime.timedelta(days=1)
     fndate = today - datetime.timedelta(days=2)
-    tmpfn = "%s.db" % (fndate.strftime("%Y%m%d"),)
-    sql = """
-        COPY (select * from current_log where valid < '%s 00:00')
-        to '/tmp/%s' WITH HEADER CSV
-    """ % (
-        yest.strftime("%Y-%m-%d"),
-        tmpfn,
+    tmpfn = f"{fndate:%Y%m%d}.db"
+    sql = (
+        "COPY (select * from current_log where "
+        f"updated < '{yest:%Y-%m-%d} 00:00') to '/tmp/{tmpfn}' WITH HEADER CSV"
     )
     atomic(pgconn, sql)
 
     # Delete out any data older than that
-    sql = """
-        DELETE from current_log WHERE valid < '%s 00:00'
-    """ % (
-        yest.strftime("%Y-%m-%d"),
-    )
+    sql = f"DELETE from current_log WHERE updated < '{yest:%Y-%m-%d} 00:00'"
     atomic(pgconn, sql)
     pgconn.close()
 
-    mydir = "iemaccess/%s" % (fndate.strftime("%Y_%m"),)
-    if not os.path.isdir(mydir):
-        os.makedirs(mydir)
-    subprocess.call("gzip /tmp/%s" % (tmpfn,), shell=True)
-    shutil.move("/tmp/%s.gz" % (tmpfn,), mydir)
+    mydir = f"iemaccess/{fndate:%Y_%m}"
+    os.makedirs(mydir, exist_ok=True)
+    subprocess.call(f"gzip /tmp/{tmpfn}", shell=True)
+    shutil.move(f"/tmp/{tmpfn}.gz", mydir)
 
     # Vacuum
     subprocess.call("vacuumdb -q -z -t current iem", shell=True)
     subprocess.call("vacuumdb -q -z -t current_log iem", shell=True)
-    subprocess.call(
-        "vacuumdb -q -z -t summary_%s iem" % (today.year,), shell=True
-    )
+    subprocess.call(f"vacuumdb -q -z -t summary_{today.year} iem", shell=True)
 
 
 if __name__ == "__main__":
