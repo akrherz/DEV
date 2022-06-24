7492f2ef7e29f12b1c876566dd368dab4201ca75
akrherz <akrherz@iastate.edu>
2022-06-23 21:41:31 -0500

    24 Jun feature

--- rwis/rwis_avgt.py
+++ rwis/rwis_avgt.py
@@ -1,58 +1,57 @@
 """Ancient."""
 import calendar
 
-import numpy as np
-from pyiem.util import get_dbconn
-import matplotlib.pyplot as plt
-
-
-def movingaverage(interval, window_size):
-    """Avert."""
-    window = np.ones(int(window_size)) / float(window_size)
-    return np.convolve(interval, window, "same")
+import pandas as pd
+from pyiem.plot import figure_axes
+from pyiem.util import get_sqlalchemy_conn
 
 
 def main():
     """Go Main."""
-    RWIS = get_dbconn("rwis")
-    rcursor = RWIS.cursor()
-
-    rcursor.execute(
-        """
-    select extract(doy from valid) as d, avg(tfs1), stddev(tfs1),
-    sum(case when tfs1 < 32 then 1 else 0 end), count(*) from alldata
-    where station = 'RMTI4' and extract(hour from valid) between 12 and 16
-    and tfs1 between -30 and 160
-    GROUP by d ORDER by d ASC
-    """
+    with get_sqlalchemy_conn("rwis") as conn:
+        df = pd.read_sql(
+            """
+        select extract(doy from valid) as d, avg(tfs1), stddev(tfs1),
+        sum(case when tfs1 < 32 then 1 else 0 end) as hits, count(*)
+        from alldata
+        where station = 'RAMI4' and extract(hour from valid) = 12
+        and tfs1 between -30 and 160
+        GROUP by d ORDER by d ASC
+        """,
+            conn,
+            index_col="d",
+        )
+    df = (
+        df.assign(freq=lambda df_: df_["hits"] / df_["count"] * 100.0)
+        .rolling(14, min_periods=1, center=True)
+        .mean()
     )
-    doy = []
-    avg = []
-    stddev = []
-    freq = []
-    for row in rcursor:
-        doy.append(row[0])
-        avg.append(row[1])
-        stddev.append(row[2])
-        freq.append(row[3] / float(row[4]) * 100.0)
 
-    (fig, ax) = plt.subplots(1, 1)
+    (fig, ax) = figure_axes(
+        title="2000-2022 Ames RWIS I-35 :: Average Noon Pavement Temperature",
+        apctx={"_r": "43"},
+    )
 
-    ax.plot(doy, avg, label="Average")
-    ax2 = ax.twinx()
-    ax2.plot(doy, freq, color="r", label="Frequency")
-    ax2.set_ylabel("Frequency of Freezing Temperature [%]", color="r")
-    ax2.set_ylim(0, 100)
-    ax.set_xticks((1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 365))
+    ax.fill_between(
+        df.index,
+        df["avg"] - df["stddev"],
+        df["avg"] + df["stddev"],
+        color="tan",
+        label="+/- 1 STD",
+    )
+    ax.plot(df.index, df["avg"], label="Average")
+    # ax2 = ax.twinx()
+    # ax2.plot(df.index, df["freq"], color="r", label="Frequency")
+    # ax2.set_ylabel("Frequency of Freezing Temperature [%]", color="r")
+    # ax2.set_ylim(0, 100)
+    ax.set_xticks((1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335))
     ax.set_xticklabels(calendar.month_abbr[1:])
+    ax.set_xlabel("Noon Central Daylight/Standard, 14 Day Smooth Applied")
     ax.set_xlim(1, 366)
     ax.plot([1, 366], [32, 32], linestyle="--")
     # ax.plot(doy, stddev, label='Pavement')
     ax.grid(True)
-    ax.set_title(
-        "2000-2012 Marshalltown RWIS US-30\n"
-        "Average Noon-4 PM Pavement Temperature"
-    )
+    ax.legend()
     ax.set_ylabel(r"Average Temperature $^{\circ}\mathrm{F}$", color="b")
 
     fig.savefig("test.png")
