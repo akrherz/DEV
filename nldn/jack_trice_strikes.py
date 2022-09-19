afd1456a711c19f1ffbbff9569ec4fbef36c1778
akrherz <akrherz@iastate.edu>
2022-09-19 08:42:56 -0500

    18 Sep feature

--- nldn/jack_trice_strikes.py
+++ nldn/jack_trice_strikes.py
@@ -1,39 +1,42 @@
 """The 30 minute shot clock."""
 import datetime
 
-from pyiem.util import get_dbconn
-from pyiem.plot.use_agg import plt
+from pyiem.util import get_sqlalchemy_conn
+from pyiem.plot import figure_axes
 import pandas as pd
-from pandas.io.sql import read_sql
+
+JACK_TRICE = [-93.6379, 42.0140]
+KINNICK = [-91.55106, 41.65863]
 
 
 def get_data():
     """Fetch."""
-    pgconn = get_dbconn("nldn")
-    df = read_sql(
-        """
-        select distinct date_trunc('minute', valid) as dt from
-        nldn2019_09 where valid > '2019-09-14 15:00' and
-        valid < '2019-09-14 21:00' and st_distance(
-            ST_GeomFromEWKT('SRID=4326;POINT(-93.6379 42.0140)')::geography,
-            geom::geography) < 12874.8 ORDER by dt ASC
-    """,
-        pgconn,
-        index_col=None,
-    )
+    with get_sqlalchemy_conn("nldn") as conn:
+        df = pd.read_sql(
+            """
+            select distinct date_trunc('minute', valid) as dt from
+            nldn2022_09 where valid > '2022-09-17 12:00' and
+            valid < '2022-09-18 04:00' and st_distance(
+                ST_GeomFromEWKT('SRID=4326;POINT(%s %s)')::geography,
+                geom::geography) < 12874.8 ORDER by dt ASC
+        """,
+            conn,
+            params=KINNICK,
+            index_col=None,
+        )
     df.to_csv("data.csv")
 
 
 def main():
     """Go Main Go."""
-    get_data()
+    # get_data()
     df = pd.read_csv("data.csv")
     df["dt"] = pd.to_datetime(df["dt"]) - datetime.timedelta(hours=5)
     df["s"] = df["dt"].dt.strftime("%Y%m%d%H%M")
     x = []
     y = []
-    sts = datetime.datetime(2019, 9, 14, 15)
-    ets = datetime.datetime(2019, 9, 14, 21)
+    sts = datetime.datetime(2022, 9, 17, 17)
+    ets = datetime.datetime(2022, 9, 18, 2)
     interval = datetime.timedelta(minutes=1)
     now = sts
     current = None
@@ -52,37 +55,51 @@ def main():
         y.append(current)
         now += interval
 
-    (fig, ax) = plt.subplots(1, 1)
+    (fig, ax) = figure_axes(
+        apctx={"_r": "43"},
+    )
     ax.plot(x, y, zorder=6)
     ax.set_xticks(xticks)
     ax.set_xticklabels(xticklabels)
     ax.set_yticks(list(range(0, 91, 15)))
-    ax.set_ylabel("Minutes since Last Lightning Strike")
+    ax.set_ylabel("Minutes since Last Lightning Strike", fontsize=16)
     ax.set_xlabel(
-        "Evening of 14 September 2019 (CDT), approximate game delays shaded"
+        "Evening of 17 September 2022 (CDT), approximate game delays shaded",
+        fontsize=16,
     )
     ax.axhline(30, lw=2, color="r")
-    ax.set_ylim(bottom=-0.1)
+    ax.set_ylim(bottom=-0.1, top=91)
     ax.axvspan(
-        datetime.datetime(2019, 9, 14, 15, 29),
-        datetime.datetime(2019, 9, 14, 16, 18),
+        datetime.datetime(2022, 9, 17, 22, 7),
+        datetime.datetime(2022, 9, 17, 23, 55),  # Not likely right
         color="k",
         alpha=0.4,
         zorder=2,
     )
     ax.axvspan(
-        datetime.datetime(2019, 9, 14, 16, 45),
-        datetime.datetime(2019, 9, 14, 18, 52),
+        datetime.datetime(2022, 9, 17, 23, 57),
+        datetime.datetime(2022, 9, 18, 0, 33),  # 12 minute warmup?
         color="k",
         alpha=0.4,
         zorder=2,
+    )
+    ax.axvspan(
+        datetime.datetime(2022, 9, 17, 20, 43),
+        datetime.datetime(2022, 9, 17, 21, 44),  # 12 minute warmup
+        color="k",
+        alpha=0.4,
+        zorder=2,
+    )
+    ax.set_xlim(
+        datetime.datetime(2022, 9, 17, 18, 30),
+        datetime.datetime(2022, 9, 18, 1, 30),
     )
     ax.grid(True)
     ax.set_title(
         (
-            "Iowa State vs Iowa Football Game\n"
+            "Nevada vs Iowa Football Game\n"
             "Time since Last Lightning Strike within 8 miles "
-            "of Jack Trice Stadium\n"
+            "of Kinnick Stadium\n"
             "Data courtesy of National Lightning Detection Network"
         )
     )
