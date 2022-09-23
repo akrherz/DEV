cb836fc185eec1fed60f6dd11ecea63e0ddacb9e
akrherz <akrherz@iastate.edu>
2022-09-22 21:29:36 -0500

    23 Sept feature

--- iemre/coverage.py
+++ iemre/coverage.py
@@ -1,67 +1,97 @@
-import calendar
+"""Accumulated coverage over iemre."""
+import os
 
-import netCDF4
-import pandas as pd
 import numpy as np
 import tqdm
-import matplotlib.pyplot as plt
+import pandas as pd
+import geopandas as gpd
+from pyiem import iemre
+from pyiem.grid.zs import CachingZonalStats
+from pyiem.plot import figure_axes
+from pyiem.util import ncopen, get_sqlalchemy_conn
 
 
 def get_data():
     """Get data"""
-    nc = netCDF4.Dataset("/mesonet/data/iemre/state_weights.nc")
-    iowa = nc.variables["IA"][:]
-    # totalpts = np.sum(iowa > 0)
-    nc.close()
+    with get_sqlalchemy_conn("postgis") as conn:
+        states = gpd.read_postgis(
+            "SELECT the_geom, state_abbr from states "
+            "where state_abbr = 'IA'",
+            conn,
+            index_col="state_abbr",
+            geom_col="the_geom",
+        )
 
     rows = []
-    for year in tqdm.tqdm(range(1893, 2018)):
-        nc = netCDF4.Dataset(
-            "/mesonet/data/iemre/" + str(year) + "_mw_daily.nc"
-        )
-        high = nc.variables["low_tmpk_12z"]
-        minval = high[200]
-        for idx in range(201, 360):
-            minval = np.minimum(minval, high[idx])
-            rows.append(
-                dict(
-                    year=year,
-                    doy=idx,
-                    count=np.sum(np.logical_and(minval < 273, iowa > 0)),
+    for year in tqdm.tqdm(range(1893, 2022)):
+        with ncopen(f"/mesonet/data/iemre/{year}_iemre_daily.nc") as nc:
+            high = nc.variables["low_tmpk_12z"]
+            if year == 1893:
+                hasdata = np.zeros(
+                    (nc.dimensions["lat"].size, nc.dimensions["lon"].size)
                 )
-            )
+                czs = CachingZonalStats(iemre.AFFINE)
+                czs.gen_stats(hasdata, states["the_geom"])
+                for nav in czs.gridnav:
+                    grid = np.ones((nav.ysz, nav.xsz))
+                    grid[nav.mask] = 0.0
+                    jslice = slice(nav.y0, nav.y0 + nav.ysz)
+                    islice = slice(nav.x0, nav.x0 + nav.xsz)
+                    hasdata[jslice, islice] = np.where(
+                        grid > 0, 1, hasdata[jslice, islice]
+                    )
 
-        nc.close()
+            minval = high[200]
+            for idx in range(201, 360):
+                minval = np.minimum(minval, high[idx])
+                rows.append(
+                    dict(
+                        year=year,
+                        doy=idx,
+                        count=np.sum(
+                            np.logical_and(minval < 273, hasdata > 0)
+                        ),
+                    )
+                )
 
     df = pd.DataFrame(rows)
-    df.to_csv("tmp.csv")
+    df.to_csv("/tmp/iemre.csv")
 
 
 def main():
     """go"""
-    df = pd.read_csv("tmp.csv")
+    if not os.path.isfile("/tmp/iemre.csv"):
+        get_data()
+    df = pd.read_csv("/tmp/iemre.csv")
     iowapts = df["count"].max()
     df["areal"] = df["count"] / iowapts * 100.0
-    (fig, ax) = plt.subplots(1, 1)
+    (fig, ax) = figure_axes(
+        apctx={"_r": "43"},
+        title=r"Iowa Accumulated Areal Coverage of First Fall sub 32$^\circ$F",
+        subtitle="Based on grid analysis between 1893-2021",
+    )
     df2 = df.groupby("doy").mean()
     ax.plot(df2.index.values, df2["areal"].values, label="Average", lw=2)
     df2 = df.groupby("doy").max()
     ax.plot(df2.index.values, df2["areal"].values, label="Max", lw=2)
     df2 = df.groupby("doy").min()
     ax.plot(df2.index.values, df2["areal"].values, label="Min", lw=2)
-    df2 = df[df["year"] == 2016]
-    ax.plot(df2["doy"].values, df2["areal"].values, label="2016", lw=2)
-    df2 = df[df["year"] == 2017]
-    ax.plot(df2["doy"].values, df2["areal"].values, label="2017", lw=2)
+    df2 = df[df["year"] == 2021]
+    ax.plot(df2["doy"].values, df2["areal"].values, label="2021", lw=2)
     ax.grid(True)
     ax.legend(loc=2)
-    ax.set_xticks([1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335])
-    ax.set_xticklabels(calendar.month_abbr[1:])
+    xticks = []
+    xticklabels = []
+    for dt in pd.date_range("2000/08/25", "2000/12/05"):
+        if dt.day in [1, 10, 20]:
+            xticks.append(int(dt.strftime("%j")))
+            xticklabels.append(f"{dt:%-d %b}")
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(xticklabels)
     ax.set_xlim(235, 336)
     ax.set_yticks([0, 10, 25, 50, 75, 90, 100])
     ax.set_ylabel("Areal Coverage [%]")
-    ax.set_title(r"Iowa Areal Coverage of First Fall sub 32$^\circ$F")
-    fig.savefig("171020.png")
+    fig.savefig("220923.png")
 
 
 if __name__ == "__main__":
