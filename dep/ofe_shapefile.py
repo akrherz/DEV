30542a89ad9ce8b2d17a4778b98e732a37651e0e
akrherz <akrherz@iastate.edu>
2022-10-04 20:44:32 -0500

    fix OFE 1 dumping bug

--- dep/ofe_shapefile.py
+++ dep/ofe_shapefile.py
@@ -29,21 +29,20 @@ def main():
             index_col=None,
         )
     res = []
-    for flowpathid, df in points.groupby("flowpath"):
-        line = flowpaths.at[flowpathid, "geom"]
-        huc12 = flowpaths.at[flowpathid, "huc_12"]
-        fpath = flowpaths.at[flowpathid, "fpath"]
+    for flowpathid, row in flowpaths.iterrows():
+        line = row["geom"]
+        huc12 = row["huc_12"]
+        fpath = row["fpath"]
         ofe = 1
-        if len(df.index) > 1:
-            for _, row in df.iterrows():
-                (ofeline, line) = split(line, row["geom"]).geoms
-                res.append([huc12, fpath, ofe, ofeline])
-                ofe += 1
+        for _, row in points[points["flowpath"] == flowpathid].iterrows():
+            (ofeline, line) = split(line, row["geom"]).geoms
+            res.append([huc12, fpath, ofe, ofeline])
+            ofe += 1
         res.append([huc12, fpath, ofe, line])
     gdf = gpd.GeoDataFrame(
         res, columns=["huc12", "flowpath", "ofe", "geom"], geometry="geom"
     )
-    gdf.to_file("dep_220930_ofes")
+    gdf.to_file("dep_221004_ofes")
 
 
 if __name__ == "__main__":
