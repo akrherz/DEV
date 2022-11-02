885bde75fcd3ccd37e696926c55904871909cbf2
akrherz <akrherz@iastate.edu>
2022-11-02 09:02:16 -0500

    use simplier logic

--- dep/ofe_shapefile.py
+++ dep/ofe_shapefile.py
@@ -1,48 +1,34 @@
 """Dump our OFEs to a shapefile."""
 
+from sqlalchemy import text
 import geopandas as gpd
-from shapely.ops import split
 from pyiem.util import get_sqlalchemy_conn
 
 
 def main():
     """Go main Go."""
+    hucs = (
+        "070801020905 070801030408 070801050302 070801070304 070802011102 "
+        "070802050807 070802051502 070802090102 070802090406 071000030704 "
+        "071000040910 071000061405 071000070702 071000080502 071000080602 "
+        "071000080701 071000081505 102300010607 102300030509 102300031003 "
+        "102300031209 102300031403 102300050303 102300070305 102400010302"
+    ).split()
     with get_sqlalchemy_conn("idep") as conn:
-        flowpaths = gpd.read_postgis(
-            "SELECT fid, huc_12, fpath, geom from flowpaths "
-            "where scenario = 0",
-            conn,
-            geom_col="geom",
-            index_col="fid",
-        )
-        points = gpd.read_postgis(
+        ofe = gpd.read_postgis(
+            text(
+                """
+            SELECT o.ofe, o.geom, f.huc_12, f.fpath from flowpaths f
+            JOIN flowpath_ofes o on (f.fid = o.flowpath) WHERE
+            f.scenario = 0 and f.huc_12 in :hucs
             """
-            with data as (
-                select geom, flowpath, ofe, lag(ofe) OVER
-                (PARTITION by flowpath ORDER by length asc) from
-                flowpath_points where scenario = 0)
-            select flowpath, ofe, geom from data where ofe > lag
-            ORDER by flowpath ASC, ofe ASC
-            """,
+            ),
             conn,
+            params={"hucs": tuple(hucs)},
             geom_col="geom",
             index_col=None,
         )
-    res = []
-    for flowpathid, row in flowpaths.iterrows():
-        line = row["geom"]
-        huc12 = row["huc_12"]
-        fpath = row["fpath"]
-        ofe = 1
-        for _, row in points[points["flowpath"] == flowpathid].iterrows():
-            (ofeline, line) = split(line, row["geom"]).geoms
-            res.append([huc12, fpath, ofe, ofeline])
-            ofe += 1
-        res.append([huc12, fpath, ofe, line])
-    gdf = gpd.GeoDataFrame(
-        res, columns=["huc12", "flowpath", "ofe", "geom"], geometry="geom"
-    )
-    gdf.to_file("dep_221004_ofes")
+    ofe.to_file("dep_221101_ofes.shp")
 
 
 if __name__ == "__main__":
