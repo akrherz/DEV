db95a9ee65d17432c495784a0fb7b9da2ef56d1a
akrherz <akrherz@iastate.edu>
2022-12-25 13:58:23 -0600

    dec 2022 blizzard lapse
    https://twitter.com/akrherz/status/1607101387531329537

--- frames/ant_tracks.py
+++ frames/ant_tracks.py
@@ -1,28 +1,24 @@
 """Simple."""
-import datetime
 
 import requests
-from pyiem.util import utc
+import pandas as pd
+from tqdm import tqdm
 
 
 def main():
     """Go Main Go."""
-    sts = utc(2021, 1, 26, 16, 20)
-    ets = utc(2021, 1, 26, 20, 46)
-    interval = datetime.timedelta(minutes=5)
-    i = 436
-    now = sts
-    while now < ets:
+    series = pd.date_range("2022/12/21 09:00", "2022/12/25 13:00", freq="300S")
+    progress = tqdm(series)
+    i = 0
+    for now in progress:
+        progress.set_description(f"{now:%Y%m%d %H%M}")
         uri = now.strftime(
-            (
-                "http://mesonet.agron.iastate.edu/roads/iem.php?"
-                "trucks&nexrad&valid=%Y-%m-%d%%20%H:%M"
-            )
+            "http://mesonet.agron.iastate.edu/roads/iem.php?"
+            "trucks&nexrad&valid=%Y-%m-%d%%20%H:%M"
         )
-        req = requests.get(uri)
-        with open("images/%05i.png" % (i,), "wb") as fh:
+        req = requests.get(uri, timeout=60)
+        with open(f"images/{i:05.0f}.png", "wb") as fh:
             fh.write(req.content)
-        now += interval
         i += 1
 
 
