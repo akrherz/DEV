d5927c6409c60611aa8c56d7d42fa7ff5c962240
akrherz <akrherz@iastate.edu>
2023-06-05 12:27:29 -0500

    adjust to use scenario 2000

--- dep/strips_copy.py
+++ dep/strips_copy.py
@@ -9,12 +9,18 @@ import subprocess
 from pyiem.util import get_dbconn
 
 XREF = {
-    "ARM": ["102400030406", "5"],
-    "MCN": ["102802010203", "239"],
-    "RHO": ["070801050702", "72"],
-    "SPIRIT": ["102300030203", "199"],
-    "WHI": ["071000070104", "256"],
-    "WOR": ["070801050307", "249"],
+    "ARM_TRT": ["102400030406", "10005"],
+    "ARM_CTL": ["102400030602", "10049"],
+    "MCN_TRT": ["102802010203", "10120"],
+    "MCN_CTL": ["102802010203", "20120"],
+    "RHO_TRT": ["070801050702", "10072"],
+    "RHO_CTL": ["070801050702", "20072"],
+    "SPIRIT_TRT": ["102300030203", "10751"],
+    "SPIRIT_CTL": ["102300030203", "10324"],
+    "WHI_TRT": ["071000070104", "10249"],
+    "WHI_CTL": ["071000070104", "10123"],
+    "WOR_TRT": ["070801050307", "10667"],
+    "WOR_CTL": ["070801050307", "10073"],
 }
 
 
@@ -26,21 +32,27 @@ def main():
     for sid, (huc12, fp) in XREF.items():
         for cat in ["man", "slp", "sol", "prj"]:
             shutil.copyfile(
-                f"/i/0/{cat}/{huc12[:8]}/{huc12[8:]}/{huc12}_{fp}.{cat}",
+                f"/i/2000/{cat}/{huc12[:8]}/{huc12[8:]}/{huc12}_{fp}.{cat}",
                 f"/tmp/strips_files/{sid}.{cat}",
             )
-        for fn in glob.glob(f"/i/0/rot/{huc12[:8]}/{huc12[8:]}/{huc12}_{fp}*"):
+        for fn in glob.glob(
+            f"/i/2000/rot/{huc12[:8]}/{huc12[8:]}/{huc12}_{fp}*"
+        ):
             shutil.copyfile(fn, f"/tmp/strips_files/{sid}_{fn[-5:]}")
         cursor.execute(
             """
             SELECT climate_file from flowpaths where huc_12 = %s and
-            fpath = %s and scenario = 0
+            fpath = %s and scenario = 2000
             """,
             (huc12, fp),
         )
         clifn = cursor.fetchone()[0]
         subprocess.call(
-            ["scp", f"mesonet@iem12:{clifn}", f"/tmp/strips_files/{sid}.cli"]
+            [
+                "scp",
+                f"mesonet@iem12:{clifn.replace('2000', '0')}",
+                f"/tmp/strips_files/{sid}.cli",
+            ]
         )
 
 
