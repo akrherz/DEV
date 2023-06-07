cd0006472ff7825afa4b1c334f1cbdd68cb4fb8e
akrherz <akrherz@iastate.edu>
2023-06-06 21:50:52 -0500

    7 Jun 2023 feature

--- nexrad/stormattr_drct_hist.py
+++ nexrad/stormattr_drct_hist.py
@@ -1,58 +1,62 @@
 """Histogram"""
 
-from pandas.io.sql import read_sql
-from pyiem.plot.use_agg import plt
-from pyiem.util import get_dbconn
+import pandas as pd
+from pyiem.plot import figure
+from pyiem.util import get_sqlalchemy_conn
 
 
 def main():
     """Do great things"""
-    pgconn = get_dbconn("postgis")
-    alldf = read_sql(
-        """
-    select drct / 15 as bindrct, count(*) from nexrad_attributes_log
-    where nexrad = 'DMX' and sknt >= 5 GROUP by bindrct ORDER by bindrct
-    """,
-        pgconn,
-        index_col=None,
-    )
+    with get_sqlalchemy_conn("radar") as pgconn:
+        alldf = pd.read_sql(
+            """
+        select drct / 15 as bindrct, count(*) from nexrad_attributes_log
+        where nexrad = 'DMX' and sknt >= 5 and
+        extract(month from valid) in (5, 6) GROUP by bindrct ORDER by bindrct
+        """,
+            pgconn,
+            index_col=None,
+        )
+        yestdf = pd.read_sql(
+            """
+        select drct / 15 as bindrct, count(*) from nexrad_attributes_2023
+        where nexrad = 'DMX' and valid > '2023-06-01' and sknt >= 5
+        GROUP by bindrct ORDER by bindrct
+        """,
+            pgconn,
+            index_col=None,
+        )
     alldf["freq"] = alldf["count"] / alldf["count"].sum() * 100.0
-    yestdf = read_sql(
-        """
-    select drct / 15 as bindrct, count(*) from nexrad_attributes_2017
-    where nexrad = 'DMX' and valid > 'TODAY' and sknt >= 5
-    GROUP by bindrct ORDER by bindrct
-    """,
-        pgconn,
-        index_col=None,
-    )
     yestdf["freq"] = yestdf["count"] / yestdf["count"].sum() * 100.0
 
-    (fig, [ax1, ax2]) = plt.subplots(2, 1, sharex=True)
+    fig = figure(
+        title="Des Moines NEXRAD Storm Attribute Direction Frequency [May-June]",
+        subtitle="2000-2023 for storms traveling at least 5 knots speed",
+        figsize=(10.24, 7.68),
+    )
+    ax1 = fig.add_axes([0.1, 0.55, 0.8, 0.35])
+    ax2 = fig.add_axes([0.1, 0.1, 0.8, 0.35])
     ax1.bar(
         alldf["bindrct"] * 15.0 + 7.5,
         alldf["freq"],
         width=15,
         alpha=0.5,
         fc="r",
-        label="2000-2017",
+        label="2000-2023",
     )
     ax1.set_xticks(range(0, 361, 45))
     ax1.set_xticklabels(["N", "NE", "E", "SE", "S", "SW", "W", "NW", "N"])
     ax1.grid(True)
     ax1.legend()
     ax1.set_ylabel("Frequency [%]")
-    ax1.set_title(
-        "Des Moines NEXRAD Storm Attribute Direction Frequency"
-        "\n2000-2017 for storms traveling at least 5 knots speed"
-    )
+    ax1.set_xlim(-1, 361)
     ax2.bar(
         yestdf["bindrct"] * 15.0 + 7.5,
         yestdf["freq"],
         width=15,
         alpha=0.5,
         fc="b",
-        label="4 Jul 2017",
+        label="1-6 Jun 2023",
     )
     ax2.set_xticks(range(0, 361, 45))
     ax2.set_xticklabels(["N", "NE", "E", "SE", "S", "SW", "W", "NW", "N"])
@@ -62,8 +66,7 @@ def main():
     ax2.legend()
     ax2.set_xlim(-1, 361)
     ax2.set_xlabel("Direction Storm Traveling From")
-    fig.text(0.01, 0.01, "Plot Generated 4 Jul 2017")
-    fig.savefig("test.png")
+    fig.savefig("230607.png")
 
 
 if __name__ == "__main__":
