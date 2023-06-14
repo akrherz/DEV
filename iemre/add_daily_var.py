546d833d4785dddfb54168b96019bca3ca6662e0
akrherz <akrherz@iastate.edu>
2023-06-14 08:46:31 -0500

    added new daily var

--- iemre/add_daily_var.py
+++ iemre/add_daily_var.py
@@ -9,7 +9,7 @@ from pyiem.util import ncopen
 
 def main():
     """go Main go"""
-    for yr in range(1893, 2020):
+    for yr in range(1893, 2024):
         fn = iemre.get_daily_ncname(yr)
         if not os.path.isfile(fn):
             print("Miss %s" % (fn,))
@@ -17,15 +17,22 @@ def main():
         print(fn)
         nc = ncopen(fn, "a", timeout=300)
         v1 = nc.createVariable(
-            "power_swdn", np.float, ("time", "lat", "lon"), fill_value=1.0e20
+            "high_soil4t", np.uint16, ("time", "lat", "lon"), fill_value=65535
         )
-        v1.units = "MJ d-1"
-        v1.long_name = "All Sky Insolation Incident on a Horizontal Surface"
-        v1.standard_name = (
-            "All Sky Insolation Incident on a Horizontal Surface"
-        )
+        v1.units = "K"
+        v1.scale_factor = 0.01
+        v1.long_name = "4inch Soil Temperature Daily High"
+        v1.standard_name = "4inch Soil Temperature"
         v1.coordinates = "lon lat"
-        v1.description = "from NASA POWER"
+
+        v1 = nc.createVariable(
+            "low_soil4t", np.uint16, ("time", "lat", "lon"), fill_value=65535
+        )
+        v1.units = "K"
+        v1.scale_factor = 0.01
+        v1.long_name = "4inch Soil Temperature Daily Low"
+        v1.standard_name = "4inch Soil Temperature"
+        v1.coordinates = "lon lat"
 
         nc.sync()
         nc.close()
