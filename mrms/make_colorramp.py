f62019dcc9ec92d692b825baf7b36646511b6182
akrherz <akrherz@iastate.edu>
2023-07-10 14:23:09 -0500

    misc mrms updates

--- mrms/make_colorramp.py
+++ mrms/make_colorramp.py
@@ -1,10 +1,11 @@
 """Generate a color ramp image, please."""
+import numpy as np
 from PIL import Image, ImageDraw, ImageFont
-import numpy
+
 import pyiem.mrms as mrms
 
 font = ImageFont.truetype(
-    "/home/akrherz/projects/pyVBCam/lib/veramono.ttf", 10
+    "/home/akrherz/projects/pyVBCam/src/pyvbcam/data/veramono.ttf", 10
 )
 
 
@@ -12,7 +13,7 @@ def make_gr_colorramp():
     """
     Make me a crude color ramp
     """
-    c = numpy.zeros((256, 3), numpy.int)
+    c = np.zeros((256, 3), int)
 
     # Gray for missing
     c[0, :] = [144, 144, 144]
@@ -28,7 +29,7 @@ def make_gr_colorramp():
 
 def main():
     """Go Main Go."""
-    data = numpy.zeros((30, 256), numpy.uint8)
+    data = np.zeros((30, 256), np.uint8)
 
     for i in range(255):
         data[0:15, i : i + 1] = i
@@ -44,17 +45,24 @@ def main():
     #         0-1   -> 100 - 0.01 res ||  0 - 25   -> 100 - 0.25 mm  0
     #         1-5   -> 80 - 0.05 res  ||  25 - 125 ->  80 - 1.25 mm  100
     #         5-20  -> 75 - 0.20 res  || 125 - 500  ->  75 - 5 mm    180
-    xs = [25, 50, 75, 100, 120, 140, 160, 180, 190, 205, 230]
-    ys = [0.25, 0.5, 0.75, 1, 2, 3, 4, 5, 7, 10, 15]
+    # xs = [25, 50, 75, 100, 120, 140, 160, 180, 190, 205, 230]
+    # ys = [0.25, 0.5, 0.75, 1, 2, 3, 4, 5, 7, 10, 15]
+    # a2m 0.02 mm per index
+    xs = []
+    ys = [0.01, 0.05, 0.1, 0.15]
+    for y in ys:
+        xs.append(int((y * 25.4) / 0.02))
+    print(xs)
+
     # 1hr precip
     # 1 mm resolution
     # xs = numpy.arange(25,255, 25)
     # ys = numpy.arange(1,10)
 
     for x, y in zip(xs, ys):
-        (w, h) = font.getsize(` y `)
+        w = font.getlength(str(y))
         draw.line([x, 17, x, 10], fill=255)
-        draw.text((x - (w / 2), 18), ` y `, fill=255, font=font)
+        draw.text((x - (w / 2), 18), str(y), fill=255, font=font)
     draw.text((235, 18), "in", fill=255, font=font)
 
     """ 
