052dcc433e6b65217504cb066c5d88e906fecaa7
akrherz <akrherz@iastate.edu>
2023-07-24 15:23:53 -0500

    tweak

--- dep/plot_huc12_pasture.py
+++ dep/plot_huc12_pasture.py
@@ -14,7 +14,8 @@ SQL = """
 WITH ofecounts as (
     select huc_12, count(*) as ofe_count,
     sum(case when genlu = 2 then 1 else 0 end) as pasture_count from
-    flowpath_ofes o JOIN flowpaths f on (o.flowpath = f.fid) WHERE
+    flowpath_ofes o, flowpaths f, fields d WHERE o.flowpath = f.fid
+    and o.fbndid = d.fbndid and f.huc_12 = d.huc12 and
     f.scenario = 0 GROUP by huc_12
 ), flds as (
     select huc12,
@@ -43,6 +44,7 @@ def main():
     df["ratio"] = (df["pasture_count"] / df["ofe_count"]) / (
         df["pasture_acres"] / df["ag_acres"]
     )
+    df["ratio"] = (df["pasture_acres"] / df["ag_acres"]) * 100.0
     print(df.sort_values("ratio", ascending=False).head(5))
     minx, miny, maxx, maxy = df.to_crs(4326)["simple_geom"].total_bounds
     mp = MapPlot(
@@ -52,7 +54,7 @@ def main():
         north=maxy,
         west=minx,
         east=maxx,
-        title=("Ratio of HUC12 Pasture OFE Ratio to Pasture Acres Ratio"),
+        title=("Percentage of HUC12 ISAG Acres with Pasture Landuse"),
         logo="dep",
         nocaption=True,
         continentalcolor="white",
@@ -61,7 +63,8 @@ def main():
     cmap = get_cmap("RdBu")
     cmap.set_bad("#000000")
     cmap.set_over("#ffff00")
-    bins = [0, 0.1, 0.2, 0.5, 0.7, 1, 1.2, 1.5, 2, 3, 5]
+    # bins = [0, 0.1, 0.2, 0.5, 0.7, 1, 1.2, 1.5, 2, 3, 5]
+    bins = [1, 5, 10, 25, 50, 75, 90, 95, 99]
     norm = mpcolors.BoundaryNorm(bins, cmap.N)
 
     df.to_crs(mp.panels[0].crs).plot(
@@ -70,7 +73,7 @@ def main():
         color=cmap(norm(df["ratio"])),
         zorder=Z_POLITICAL,
     )
-    mp.draw_colorbar(bins, cmap, norm, title="Ratio", extend="max")
+    mp.draw_colorbar(bins, cmap, norm, title="Percentage", extend="max")
 
     mp.fig.savefig("test.png")
 
