ad30ae426efc36ea38e046ce2c4b10b3da3c7cb4
akrherz <akrherz@iastate.edu>
2023-09-06 05:51:45 -0500

    6 Sep feature

--- model/rtma_hours.py
+++ model/rtma_hours.py
@@ -5,7 +5,7 @@ import os
 import numpy as np
 import pygrib
 
-from matplotlib.colors import ListedColormap
+# from matplotlib.colors import ListedColormap
 from metpy.units import units
 from pyiem.plot import MapPlot
 from pyiem.plot.use_agg import plt
@@ -13,50 +13,60 @@ from pyiem.plot.use_agg import plt
 
 def plot():
     """Do plotting work"""
-    cmap1 = plt.get_cmap("rainbow")
-    colors = list(cmap1(np.arange(1, 7) / 7.0))
+    cmap = plt.get_cmap("RdBu_r")
+    # colors = list(cmap1(np.arange(1, 7) / 7.0))
     # cmap2 = plt.get_cmap("Pastel1")
     # colors.extend(list(cmap2(np.arange(3) / 3.0)))
-    cmap = ListedColormap(colors)
+    # cmap = ListedColormap(colors)
+
+    total = None
+    for year in range(2018, 2023):
+        val = np.load(f"hours{year}.npy")
+        if total is None:
+            total = val
+        else:
+            total += val
 
     cmap.set_over("yellow")
-    cmap.set_under("white")
-    prev = np.load("maxval.npy")
-    today = np.load("maxval2.npy")
+    cmap.set_under("black")
+    hours = np.load("hours.npy") - (total / 5.0)
+    hours = np.where(hours == 0, np.nan, hours)
     lons = np.load("lons.npy")
     lats = np.load("lats.npy")
     mp = MapPlot(
-        sector="custom",
-        south=35,
-        west=-105,
-        north=49,
-        east=-82,
-        continentalcolor="tan",
+        sector="southernplains",
+        # south=24,
+        # west=-120,
+        # north=49,
+        # east=-62,
+        continentalcolor="white",
         # statebordercolor="white",
-        title=(
-            "Max Air Temperature April 12 vs January 1 through April 11 2023"
+        title=(r"2023 Hours AOA 95$^\circ$F Departure from 2018-2022 Average"),
+        subtitle=(
+            "1 Jan - 5 Sept, based on hourly NCEP Real-Time Mesoscale Analysis"
         ),
-        subtitle="based on hourly NCEP Real-Time Mesoscale Analysis",
     )
 
-    # levels = [1, 4, 8, 12]
+    levels = [-350, -200, -100, -50, 0, 50, 100, 200, 350]
     # levels2 = list(range(24, 14 * 24 + 1, 48))
     # levels.extend(levels2)
     # levels = range(12, 145, 12)
-    levels = range(1, 14, 2)
+    # levels = list(range(0, 14 * 24, 24))
+    # levels[0] = 1
     mp.pcolormesh(
         lons,
         lats,
-        today - prev,
+        hours,
         levels,
         cmap=cmap,
         clip_on=False,
-        units=r"$^\circ$F",
-        spacing="proportional",
+        units="hours",
+        # spacing="proportional",
         extend="both",
     )
-    mp.drawcounties()
-    mp.postprocess(filename="230413.png")
+    # mp.drawcounties()
+    # mp.draw_usdm(filled=False, hatched=True)
+    mp.postprocess(filename="230906.png")
 
 
 def process():
