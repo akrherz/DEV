8b633f67c86f9126b0e481db1c2fa0da61da8cbb
akrherz <akrherz@iastate.edu>
2023-09-14 10:07:20 -0500

    modernize

--- afos/afos2single.py
+++ afos/afos2single.py
@@ -1,29 +1,26 @@
 """Dump products to file."""
-import os
 
-from pyiem.util import get_dbconn, noaaport_text
+from pyiem.util import get_dbconnc, noaaport_text
 
 
 def main():
     """Go Main Go."""
-    pgconn = get_dbconn("afos")
-    cursor = pgconn.cursor("streamer")
+    pgconn, cursor = get_dbconnc("afos")
 
     cursor.execute(
         """
-        SELECT data, entered at time zone 'UTC', pil, source from products
-        WHERE substr(pil, 1, 3) in ('TOR', 'SVR', 'SVS') and
-        entered >= '2007-01-01 00:00+00'
+        select entered at time zone 'UTC' as ts, data, pil from products
+        where substr(pil, 1, 3) = 'SPS' and
+        source in ('KDMX', 'KDVN', 'KARX', 'KFSD', 'KOAX') and
+        data ~* 'landspout' and data ~* 'IA' ORDER by entered asc
     """
     )
     for row in cursor:
-        pil = row[2].strip()
-        mydir = f"{row[3]}/{pil}"
-        if not os.path.isdir(mydir):
-            os.makedirs(mydir)
-        fn = row[1].strftime(f"{mydir}/{pil}_%Y%m%d%H%M.txt")
+        pil = row["pil"].strip()
+        fn = row["ts"].strftime(f"{pil}_%Y%m%d%H%M.txt")
         with open(fn, "a", encoding="utf8") as fh:
-            fh.write(noaaport_text(row[0]))
+            fh.write(noaaport_text(row["data"]))
+    pgconn.close()
 
 
 if __name__ == "__main__":
