f5b804be1ca07076d8478524ae0326a1cef30869
akrherz <akrherz@iastate.edu>
2023-10-19 08:55:39 -0500

    add review of website_telemetry table

---
+++ iem/review_weberrors.py
@@ -0,0 +1,37 @@
+"""
+Review website_telemetry for errors.
+"""
+
+import requests
+
+import pandas as pd
+from pyiem.util import get_sqlalchemy_conn
+
+
+def main():
+    """Go Main Go."""
+    with get_sqlalchemy_conn("mesosite") as conn:
+        df = pd.read_sql(
+            """
+            select distinct request_uri from website_telemetry
+            where status_code = 500
+            and valid > now() - '1 day'::interval
+            """,
+            conn,
+        )
+    for uri in df["request_uri"]:
+        print("-------------------------------------------------")
+        print(uri)
+        waiting = True
+        while waiting:
+            req = requests.get("http://iem.local" + uri, timeout=30)
+            if req.status_code == 200:
+                waiting = False
+                continue
+            res = input(f"Got {req.status_code} {req.text} Try again?([y]/n) ")
+            if res == "n":
+                waiting = False
+
+
+if __name__ == "__main__":
+    main()
