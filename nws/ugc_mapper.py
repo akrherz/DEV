ab95225fe90d4425cbb264a69679285c6622993b
akrherz <akrherz@iastate.edu>
2023-10-29 11:23:35 -0500

    chore: bump ruff

--- nws/ugc_mapper.py
+++ nws/ugc_mapper.py
@@ -1,49 +1,61 @@
 """simple map with counties filled"""
 
-import cartopy.feature as cfeature
-from pandas.io.sql import read_sql
-from pyiem.plot import Z_POLITICAL, MapPlot, maue
-from pyiem.util import get_dbconn
+import geopandas as gpd
+from matplotlib import colors as mpcolors
+from pyiem.plot import MapPlot, get_cmap
+from pyiem.reference import Z_FILL
+from pyiem.util import get_sqlalchemy_conn
 
 
 def main():
     """Do Something"""
-    pgconn = get_dbconn("postgis")
-    df = read_sql(
-        """
-        SELECT ugc, count(*) from warnings where phenomena = 'BZ'
-        and significance = 'W' and issue < '2020-07-01'
-        GROUP by ugc ORDER by count DESC
-    """,
-        pgconn,
-        index_col="ugc",
-    )
+    with get_sqlalchemy_conn("postgis") as pgconn:
+        df = gpd.read_postgis(
+            """
+            with data as (
+                SELECT ugc, count(*) from warnings where phenomena = 'BZ'
+                and significance = 'W' GROUP by ugc ORDER by count DESC
+            ), agg as (
+                select d.ugc, simple_geom, count,
+                 rank() OVER (PARTITION by d.ugc ORDER by u.end_ts DESC)
+                 from ugcs u JOIN data d on
+                (u.ugc = d.ugc) ORDER by count ASC
+            )
+            select * from agg where rank = 1
+        """,
+            pgconn,
+            index_col="ugc",
+            geom_col="simple_geom",
+        )
     mp = MapPlot(
         sector="nws",
         title=(
-            "12 Nov 2005 - 1 Jul 2020 Number of NWS Issued Blizzard Warnings"
+            "12 Nov 2005 - 25 Oct 2023 Number of NWS Issued Blizzard Warnings"
         ),
         subtitle=(
-            "count by county, "
+            "count by county/zone, "
             "based on unofficial archives maintained by the "
             "IEM"
         ),
     )
-    mp.ax.add_feature(cfeature.STATES, lw=1.0, zorder=Z_POLITICAL)
 
     bins = [1, 5, 10, 15, 20, 25, 30, 40, 50, 75, 100]
     print(df["count"].max())
-    cmap = maue()
+    cmap = get_cmap("jet")
     cmap.set_under("white")
     cmap.set_over("black")
-    mp.fill_ugcs(
-        df["count"].to_dict(),
-        bins,
-        cmap=cmap,
-        spacing="proportional",
-        plotmissing=False,
-    )
-    mp.fig.savefig("test.pdf")
+    norm = mpcolors.BoundaryNorm(bins, cmap.N)
+    for panel in mp.panels:
+        df.to_crs(panel.crs).plot(
+            ax=panel.ax,
+            aspect=None,
+            fc=cmap(norm(df["count"].values)),
+            ec="k",
+            lw=0.1,
+            zorder=Z_FILL,
+        )
+    mp.draw_colorbar(bins, cmap, norm, extend="max", spacing="proportional")
+    mp.fig.savefig("test.png")
 
 
 if __name__ == "__main__":
