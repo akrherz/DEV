a09af7d601495cb031751a874d6803221961f31d
akrherz <akrherz@iastate.edu>
2024-01-23 09:40:07 -0600

    add pandas 2.2 leak example

---
+++ pandas/leak.py
@@ -0,0 +1,14 @@
+"""example leak.
+
+https://github.com/pandas-dev/pandas/issues/57039
+"""
+
+import numpy as np
+
+import pandas as pd
+
+pcp = np.ones((2700, 6100), dtype=np.float64)
+pcp[pcp < 0] = np.nan
+
+for _ in range(1000):
+    pd.read_csv("example.txt", engine="c")
