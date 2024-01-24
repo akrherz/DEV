c1c615ef9178de5573da47665031c4edff51a873
akrherz <akrherz@iastate.edu>
2024-01-24 09:47:38 -0600

    add metpy error example

---
+++ metpy/error.py
@@ -0,0 +1,28 @@
+"""
+https://github.com/Unidata/MetPy/discussions/3375
+"""
+import numpy as np
+
+import pandas as pd
+from metpy.calc import wind_components
+from metpy.units import units
+
+pd.set_option("future.no_silent_downcasting", True)
+df = (
+    pd.DataFrame(
+        {
+            "sknt": pd.Series([1, 2, 3, None], dtype=object),
+            "drct": pd.Series([4, 6, 7, None], dtype=object),
+        }
+    )
+    .fillna(np.nan)
+    .infer_objects()
+)
+
+
+print(
+    wind_components(
+        units("knot") * df["sknt"].values,
+        units("degree") * df["drct"].values,
+    )
+)
