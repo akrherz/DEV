608c3a220eea7ebf96ec317ee424772743e43c9c
akrherz <akrherz@iastate.edu>
2024-03-29 14:24:30 -0500

    feat: save response

--- iem/xcheck_service.py
+++ iem/xcheck_service.py
@@ -28,16 +28,22 @@ def main(filename, size):
     lines = random.sample(lines, min(len(lines), size))
     for line in tqdm(lines):
         uri = line.split()[6]
-        req = httpx.get(f"https://mesonet.agron.iastate.edu{uri}", timeout=300)
-        ans_status = req.status_code
-        ans_lines = len(req.text.split("\n"))
+        if uri.endswith("fmt=html"):
+            continue
+        mreq = httpx.get(
+            f"https://mesonet.agron.iastate.edu{uri}", timeout=300
+        )
+        ans_status = mreq.status_code
+        ans_lines = len(mreq.text.split("\n"))
         req = httpx.get(f"http://iem.local{uri}", timeout=300)
         dev_status = req.status_code
-        if dev_status == 422:
-            print(req.text)
         dev_lines = len(req.text.split("\n"))
         if ans_status != dev_status or ans_lines != dev_lines:
-            print(f"{uri} {ans_status} {ans_lines} {dev_status} {dev_lines}")
+            print(f"{uri} {ans_status}->{dev_status} {ans_lines}->{dev_lines}")
+            with open("local", "wb") as fh:
+                fh.write(req.content)
+            with open("mesonet", "wb") as fh:
+                fh.write(mreq.content)
             return
 
 
