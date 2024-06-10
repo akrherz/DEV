1ecee216849401e9d1d8e3635eefb64dd6e84b39
akrherz <akrherz@iastate.edu>
2024-06-10 09:27:03 -0500

    sync laptop dev

---
+++ asos/manual_feel_qc.py
@@ -0,0 +1,110 @@
+"""
+Look for bad feels like temperature and do some culling.
+"""
+
+from zoneinfo import ZoneInfo
+
+import click
+from sqlalchemy import text
+
+import pandas as pd
+from pyiem.database import get_dbconnc, get_sqlalchemy_conn
+from pyiem.network import Table as NetworkTable
+from pyiem.util import logger
+
+LOG = logger()
+
+
+def process(conn, row, station, nt):
+    """Do what we need to do here."""
+    delta = pd.Timedelta(hours=3)
+    obs = pd.read_sql(
+        "SELECT valid, tmpf, dwpf, relh, feel from alldata where "
+        "station = %s and valid >= %s and valid <= %s and "
+        "report_type in (3, 4) ORDER by valid ASC",
+        conn,
+        params=(
+            station,
+            row["valid"] - delta,
+            row["valid"] + delta,
+        ),
+    )
+    print(row["valid"])
+    print(obs.head(100))
+    res = input("Null space sep list (#t #d #r #f): ")
+    if res == "":
+        return
+    tzinfo = ZoneInfo(nt.sts[station]["tzname"])
+    iempgconn, iemcursor = get_dbconnc("iem")
+    mapper = {
+        "t": "tmpf",
+        "d": "dwpf",
+        "r": "relh",
+        "f": "feel",
+    }
+    for entry in res.split():
+        idx = int(entry[:1])
+        cullrow = obs.loc[idx]
+        colval = entry[-1]
+        col = mapper[colval]
+        conn.execute(
+            text(
+                f"UPDATE t{cullrow['valid'].year} SET {col} = null, "
+                "editable = 'f' "
+                "WHERE station = :station and valid = :dt"
+            ),
+            {
+                "station": station,
+                "dt": cullrow["valid"],
+            },
+        )
+        print(f"Setting {cullrow['valid']} {col}->null")
+
+        iemcursor.execute(
+            f"update summary_{cullrow['valid'].year} set max_feel = null, "
+            "max_tmpf = null, min_tmpf = null, min_feel = null, "
+            "avg_feel = null, max_rh = null, avg_rh = null, min_rh = null "
+            "where iemid = %s and day = %s",
+            (
+                nt.sts[station]["iemid"],
+                cullrow["valid"].astimezone(tzinfo).date(),
+            ),
+        )
+    iemcursor.close()
+    iempgconn.commit()
+    conn.commit()
+
+
+@click.command()
+@click.option("--station", required=True)
+@click.option("--network", required=True)
+@click.option("--above", type=int, default=None)
+@click.option("--below", type=int, default=None)
+@click.option("--year", type=int, default=None)
+def main(station, network, above, below, year):
+    """Go Main Go."""
+    nt = NetworkTable(network, only_online=False)
+    # Look for obs that are maybe bad
+    op = ">" if above is not None else "<"
+    tbl = f"t{year}" if year is not None else "alldata"
+    with get_sqlalchemy_conn("asos") as conn:
+        obs = pd.read_sql(
+            text(f"""
+            select valid, tmpf, dwpf, relh, feel from {tbl}
+            where station = :station and feel {op} :t
+            ORDER by feel desc
+                """),
+            conn,
+            params={
+                "station": station,
+                "t": above if above is not None else below,
+            },
+        )
+        LOG.info("Found %s rows", len(obs.index))
+
+        for _, row in obs.iterrows():
+            process(conn, row, station, nt)
+
+
+if __name__ == "__main__":
+    main()
---
+++ mrms/map_preciprate.py
@@ -0,0 +1,86 @@
+"""Plot MRMS PrecipRate."""
+
+import datetime
+import gzip
+import os
+import tempfile
+from zoneinfo import ZoneInfo
+
+import click
+import numpy as np
+import pygrib
+
+from pyiem import mrms
+from pyiem.plot import MapPlot
+from pyiem.plot.colormaps import maue
+from pyiem.util import logger
+
+LOG = logger()
+
+
+def workflow(valid, framei):
+    """Generate for this timestep!"""
+    # minutes = 2 if valid.year > 2011 else 5
+    gribfn = mrms.fetch("PrecipRate", valid)
+    # http://www.nssl.noaa.gov/projects/mrms/operational/tables.php
+    # Says units are mm/hr
+    fp = gzip.GzipFile(gribfn, "rb")
+    (_, tmpfn) = tempfile.mkstemp()
+    with open(tmpfn, "wb") as fh:
+        try:
+            fh.write(fp.read())
+        except EOFError:
+            LOG.info("caught EOFError on %s, likely corrupt, deleting", gribfn)
+            os.unlink(gribfn)
+            return
+    grbs = pygrib.open(tmpfn)
+    grb = grbs[1]
+    lats, lons = grb.latlons()
+    grbs.close()
+    os.unlink(tmpfn)
+    os.unlink(gribfn)
+
+    val = grb["values"]
+    # If has a mask, convert those to -3
+    if hasattr(val, "mask"):
+        val = np.where(val.mask, -3, val)
+    val = val
+
+    lts = valid.astimezone(ZoneInfo("America/Chicago"))
+    mp = MapPlot(
+        logo="dep",
+        caption="Daily Erosion Project",
+        title="MRMS 2 Minute Precipitation Rate mm/hr",
+        subtitle=f"{lts:%d %b %Y %I:%M %p %Z}",
+    )
+    clevels = np.arange(0, 100.1, 5)
+    clevels[0] = 0.01
+    yslice = slice(1100, 2000)
+    xslice = slice(3000, 4000)
+    cmap = maue()
+    cmap.set_under("white")
+    mp.pcolormesh(
+        lons[yslice, xslice],
+        lats[yslice, xslice],
+        val[yslice, xslice],
+        clevels,
+        cmap=cmap,
+        units="mm/hr",
+    )
+    mp.drawcounties()
+    mp.fig.savefig(f"frames/{framei:05.0f}.png")
+    mp.close()
+
+
+@click.command()
+@click.option("--valid", type=click.DateTime(), help="UTC Timestamp")
+@click.option("--frames", type=int, default=0, help="Frames")
+def main(valid, frames):
+    """Go Main Go"""
+    valid = valid.replace(tzinfo=datetime.timezone.utc)
+    for i in range(0, frames + 1):
+        workflow(valid + datetime.timedelta(minutes=i * 2), i)
+
+
+if __name__ == "__main__":
+    main()
