e9eec5a9109c44b32eab5caa9aab6615dfd141a2
akrherz <akrherz@iastate.edu>
2024-08-22 16:18:12 -0500

    feat: crosscheck iem summary

--- asos/xcheck_maxmin.py
+++ asos/xcheck_maxmin.py
@@ -1,30 +1,73 @@
-"""Some AWOSes are reporting 6 hour max/min, lets check that out!"""
+"""Review IEM processing of 6 hourly max/min temperatures."""
 
-import datetime
-import sys
+import click
+from sqlalchemy import text
 
 import pandas as pd
 from pyiem.database import get_sqlalchemy_conn
+from pyiem.network import Table as NetworkTable
 
 
-def main(argv):
+@click.command()
+@click.option("--station", type=str, required=True, help="ASOS Station ID")
+@click.option("--network", type=str, required=True, help="ASOS Network ID")
+def main(station, network):
     """xref."""
+    nt = NetworkTable(network)
     with get_sqlalchemy_conn("asos") as conn:
-        df = pd.read_sql(
-            """
-            select valid at time zone 'UTC' as utc_valid, tmpf, max_tmpf_6hr,
-            min_tmpf_6hr from t2023 where station = %s and
-            valid > '2023-10-01' ORDER by valid ASC
-            """,
+        # backup the timestamp to make top of the hour obs work more cleanly
+        # and account for standard time
+        obsdf = pd.read_sql(
+            text("""
+            select (valid - '61 minutes'::interval) at time zone 'UTC'
+            as utc_valid,
+            (valid - '61 minutes'::interval) at time zone :tzname
+            as localvalid,
+            max_tmpf_6hr,
+            min_tmpf_6hr from alldata where station = :station and
+            max_tmpf_6hr is not null and min_tmpf_6hr is not null
+            ORDER by valid ASC
+            """),
             conn,
-            params=(argv[1],),
+            params={"station": station, "tzname": nt.sts[station]["tzname"]},
             index_col="utc_valid",
-            parse_dates="utc_valid",
+            parse_dates=["utc_valid", "localvalid"],
         )
-    for valid, row in df[df["max_tmpf_6hr"].notna()].iterrows():
-        df2 = df.loc[valid - datetime.timedelta(hours=6) : valid]
-        print(row["max_tmpf_6hr"], df2["tmpf"].max())
+    # OK, we should always be able to summarize this by the localvalid.date()
+    minmaxdf = obsdf.groupby(obsdf["localvalid"].dt.date).agg(
+        {"max_tmpf_6hr": "max", "min_tmpf_6hr": "min"}
+    )
+    minmaxdf["count"] = obsdf.groupby(obsdf["localvalid"].dt.date).size()
+    with get_sqlalchemy_conn("iem") as conn:
+        summarydf = pd.read_sql(
+            text(
+                """
+            select day,
+            max_tmpf, min_tmpf from summary where iemid = :iemid
+            and max_tmpf is not null and min_tmpf is not null
+            ORDER by day ASC
+        """
+            ),
+            conn,
+            params={"iemid": nt.sts[station]["iemid"]},
+            index_col="day",
+            parse_dates="day",
+        )
+    summarydf["obs_max"] = minmaxdf["max_tmpf_6hr"]
+    summarydf["obs_min"] = minmaxdf["min_tmpf_6hr"]
+    summarydf["count"] = minmaxdf["count"]
+    # ensure we have valid obs_max and obs_min values
+    summarydf = summarydf[summarydf["obs_max"].notna()]
+    summarydf = summarydf[summarydf["obs_min"].notna()]
+    summarydf["diff_max"] = summarydf["max_tmpf"] - summarydf["obs_max"]
+    summarydf["diff_min"] = summarydf["min_tmpf"] - summarydf["obs_min"]
+    print(summarydf.sort_values("diff_max", ascending=False))
+    print(
+        summarydf[
+            (summarydf["count"] == 4) & (summarydf["diff_max"].abs() > 1)
+        ].tail(100)
+    )
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
