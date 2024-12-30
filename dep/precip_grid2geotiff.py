eff1bf90fd3515eeda667b5c5aa0baecfa3185c1
akrherz <akrherz@iastate.edu>
2024-12-30 10:39:00 -0600

    ✨ Util to convert legacy precip grid

---
+++ dep/precip_grid2geotiff.py
@@ -0,0 +1,41 @@
+"""We converted from numpy grid to GeoTIFF."""
+
+import os
+
+import numpy as np
+import pandas as pd
+import rasterio
+from affine import Affine
+from tqdm import tqdm
+
+
+def main():
+    """Go."""
+    progress = tqdm(pd.date_range("2007/01/01", "2024/12/29"))
+    for dt in progress:
+        progress.set_description(dt.strftime("%Y-%m-%d"))
+        fn = dt.strftime("/mnt/idep2/data/dailyprecip/%Y/%Y%m%d.npy")
+        if not os.path.isfile(fn):
+            continue
+        values = np.flipud(np.load(fn))
+        values = (values * 100.0).astype(np.uint16)
+        with rasterio.open(
+            fn.replace(".npy", ".geotiff"),
+            "w",
+            driver="GTiff",
+            compress="lzw",
+            height=values.shape[0],
+            width=values.shape[1],
+            count=1,
+            dtype=values.dtype,
+            crs="EPSG:4326",
+            # This is not exactly right, but close enough until reprocessing
+            transform=Affine(0.01, 0.0, -126.005, 0.0, -0.01, 49.995),
+        ) as dst:
+            dst._set_all_scales([0.01])
+            dst._set_all_offsets([0.0])
+            dst.write(values, 1)
+
+
+if __name__ == "__main__":
+    main()
