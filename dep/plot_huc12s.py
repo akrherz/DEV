f13677ae92cac3bbba4c2544c0ba326fb4029333
akrherz <akrherz@iastate.edu>
2025-01-24 13:34:30 -0600

    📝 Sundry

--- dep/plot_huc12s.py
+++ dep/plot_huc12s.py
@@ -2,6 +2,7 @@
 
 import geopandas as gpd
 import numpy as np
+import pandas as pd
 from matplotlib import colors as mpcolors
 from pyiem.database import get_sqlalchemy_conn
 from pyiem.plot import MapPlot, get_cmap
@@ -27,18 +28,27 @@ with data as (
 def main():
     """Go Main Go."""
     with get_sqlalchemy_conn("idep") as conn:
-        df = gpd.read_postgis(
-            text(SQL),
+        huc12df = gpd.read_postgis(
+            text("select simple_geom, huc_12 from huc12 where scenario = 0"),
             conn,
             geom_col="simple_geom",
             index_col="huc_12",
         )
+    datadf = pd.read_csv(
+        "/tmp/huc12_stddev_rfactor.csv",
+        index_col="huc_12",
+        dtype={"huc_12": str},
+    )
+    avgdf = pd.read_csv(
+        "/tmp/huc12_rfactor.csv", index_col="huc_12", dtype={"huc_12": str}
+    )
+    huc12df["data"] = datadf["stddev"] / avgdf["avg"]
     # print(df)
     # df["percent"] = df["no_till_acres"] / df["total"] * 100.0
     # df = df.fillna(0)
     # df["ratio"] = df["runoff"] / df["precip"] * 100.0
     # df2 = df[df["precip"] >= 5]
-    minx, miny, maxx, maxy = df.to_crs(4326)["simple_geom"].total_bounds
+    minx, miny, maxx, maxy = huc12df.to_crs(4326)["simple_geom"].total_bounds
     mp = MapPlot(
         apctx={"_r": "43"},
         sector="custom",
@@ -46,26 +56,29 @@ def main():
         north=maxy,
         west=minx,
         east=maxx,
-        title=("2008 Precipitation minus 2012 Precipitation [inch] by HUC12"),
+        title=(
+            "2007-2024 Standard Deviation of Yearly R-Factor / "
+            "Average by HUC12"
+        ),
         logo="dep",
         nocaption=True,
         continentalcolor="white",
         stateborderwidth=3,
     )
-    cmap = get_cmap("RdBu")
+    cmap = get_cmap("Reds")
     # cmap.set_bad("#000000")
     # cmap.set_over("#ffff00")
-    bins = np.arange(-20, 20.1, 4.0)
+    bins = np.arange(0, 1.1, 0.2)
     norm = mpcolors.BoundaryNorm(bins, cmap.N)
 
-    df.to_crs(mp.panels[0].crs).plot(
+    huc12df.to_crs(mp.panels[0].crs).plot(
         aspect=None,
         ax=mp.panels[0].ax,
-        color=cmap(norm(df["loss"])),
+        color=cmap(norm(huc12df["data"])),
         zorder=Z_POLITICAL,
     )
-    mp.drawcounties()
-    mp.draw_colorbar(bins, cmap, norm, title="inch", extend="both")
+    # mp.drawcounties()
+    mp.draw_colorbar(bins, cmap, norm, title="Ratio", extend="max")
 
     mp.fig.savefig("test.png")
 
