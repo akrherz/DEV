76c2d5dd2100559d2649367c79610be68e344d7b
akrherz <akrherz@iastate.edu>
2025-01-31 08:26:37 -0600

    📝 31 Jan 2025 feature

--- model/plot_nohrsc.py
+++ model/plot_nohrsc.py
@@ -1,34 +1,42 @@
-"""Plot WPC"""
+"""Plot NOHRSC snowfall data."""
 
-import netCDF4
-from pyiem.plot import MapPlot
-from pyiem.plot.use_agg import plt
+import numpy as np
+from affine import Affine
+from pyiem.plot import MapPlot, nwssnow
+from pyiem.util import ncopen
 
 
 def main():
     """Go Main"""
-    nc = netCDF4.Dataset("/tmp/sfav2_CONUS_2019093012_to_2020063012.nc")
-    lats = nc.variables["lat"][:]
-    lons = nc.variables["lon"][:]
-    data = nc.variables["Data"][:] * 1000.0 / 25.4
-    nc.close()
+    with ncopen("/tmp/sfav2_CONUS_2024093012_to_2025013012.nc") as nc:
+        lats = nc.variables["lat"][:]
+        lons = nc.variables["lon"][:]
+        data = np.flipud(nc.variables["Data"][:] * 1000.0 / 25.4)
+    data = np.ma.masked_where(data < 0.01, data)
 
     mp = MapPlot(
-        sector="midwest",
+        sector="spherical_mercator",
+        west=-100,
+        south=29,
+        east=-80,
+        north=47,
         continentalcolor="tan",
         title=(
             "National Snowfall Analysis - NOHRSC - "
-            "2019-2020 Season Total Snowfall"
+            "2024-2025 Season Total Snowfall"
         ),
-        subtitle="Snowfall up until 7 PM 30 Jun 2020",
+        subtitle="Snowfall up until 6 AM 30 Jan 2025",
     )
-    cmap = plt.get_cmap("terrain_r")
-    levs = [0.1, 2, 5, 8, 12, 18, 24, 30, 36, 42, 48, 60]
-    mp.pcolormesh(
-        lons,
-        lats,
+    cmap = nwssnow()
+    cmap.set_bad("None")
+    levs = [0.1, 2, 5, 8, 12, 18, 24, 30, 36, 48, 60]
+    mp.imshow(
         data,
-        levs,
+        Affine(
+            lons[1] - lons[0], 0.0, lons[0], 0.0, lats[0] - lats[1], lats[-1]
+        ),
+        "EPSG:4326",
+        clevs=levs,
         cmap=cmap,
         units="inch",
         clip_on=False,
@@ -36,7 +44,7 @@ def main():
     )
     # mp.drawcounties()
     # mp.drawcities()
-    mp.postprocess(filename="test.png")
+    mp.postprocess(filename="250131.png")
 
 
 if __name__ == "__main__":
