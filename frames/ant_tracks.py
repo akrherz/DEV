76ad4d29d3f829a57e2bdcd8a4968df5f7c843bc
akrherz <akrherz@iastate.edu>
2025-02-07 11:00:11 -0600

    📝 7 Feb feature

--- frames/ant_tracks.py
+++ frames/ant_tracks.py
@@ -1,24 +1,24 @@
 """Simple."""
 
+import httpx
 import pandas as pd
-import requests
 from tqdm import tqdm
 
 
 def main():
     """Go Main Go."""
-    series = pd.date_range("2022/12/21 09:00", "2022/12/25 13:00", freq="300S")
+    series = pd.date_range("2025/02/06 13:00", "2025/02/06 18:00", freq="300S")
     progress = tqdm(series)
-    i = 0
+    i = 313
     for now in progress:
         progress.set_description(f"{now:%Y%m%d %H%M}")
         uri = now.strftime(
             "http://mesonet.agron.iastate.edu/roads/iem.php?"
             "trucks&nexrad&valid=%Y-%m-%d%%20%H:%M"
         )
-        req = requests.get(uri, timeout=60)
+        resp = httpx.get(uri, timeout=60)
         with open(f"images/{i:05.0f}.png", "wb") as fh:
-            fh.write(req.content)
+            fh.write(resp.content)
         i += 1
 
 
