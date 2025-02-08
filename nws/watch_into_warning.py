8e62c8a53c6d7cc8aa8c03007cd9f659065ffc23
akrherz <akrherz@iastate.edu>
2025-02-07 22:46:54 -0600

    ✨ Create updated plot
    https://bsky.app/profile/akrherz.bsky.social/post/3lhnc4iwjs22l

--- nws/watch_into_warning.py
+++ nws/watch_into_warning.py
@@ -1,30 +1,28 @@
 """Some stats based on distance from issuance time to issue."""
 
-import sys
+import os
 
 import matplotlib.pyplot as plt
 import pandas as pd
-from pandas.io.sql import read_sql
+from pyiem.database import get_sqlalchemy_conn, sql_helper
 from pyiem.network import Table as NetworkTable
 from pyiem.plot import MapPlot
-from pyiem.util import get_dbconn
 from tqdm import tqdm
 
 
-def workflow(wfo, phenomena):
+def workflow(pgconn, wfo: str, phenomena: str):
     """do work"""
-    pgconn = get_dbconn("postgis")
-    df = read_sql(
-        """
+    df = pd.read_sql(
+        sql_helper("""
     WITH mywatches as (
         SELECT product_issue, eventid, wfo, ugc, issue,
         greatest(expire, init_expire) as expire
         from warnings w
-        WHERE phenomena = %s and significance = 'A' and wfo = %s
+        WHERE phenomena = :phenomena and significance = 'A' and wfo = :wfo
         and issue > '2006-01-01'),
     mywarnings as (
         SELECT wfo, ugc, issue, expire from warnings w
-        WHERE phenomena = %s and significance = 'W' and wfo = %s
+        WHERE phenomena = :phenomena and significance = 'W' and wfo = :wfo
         and issue > '2006-01-01'),
     agg1 as (
         SELECT a.wfo, a.eventid, a.ugc as watch_ugc, w.ugc as warn_ugc,
@@ -49,54 +47,67 @@ def workflow(wfo, phenomena):
     count(*) as watch_zones,
     sum(case when warning_issue is not null then 1 else 0 end) as warn_counties
     from agg2 GROUP by wfo, yr, eventid ORDER by yr ASC, eventid ASC
-    """,
+    """),
         pgconn,
-        params=(phenomena, wfo, phenomena, wfo),
+        params={"wfo": wfo, "phenomena": phenomena},
         index_col=None,
     )
-    if len(df.index) == 0:
-        return None
+    if df.empty:
+        return None, None, None
     eff = len(df[df["warn_counties"] > 0].index) / float(len(df.index)) * 100.0
     overall = (
         df["warn_counties"].sum() / float(df["watch_zones"].sum()) * 100.0
     )
     print("WFO: %s Efficiency: %.1f  Overall: %.1f" % (wfo, eff, overall))
-    return eff
+    return df["warn_counties"].sum(), df["watch_zones"].sum(), overall
 
 
 def main():
     """Go Main"""
     nt = NetworkTable("WFO")
     rows = []
-    for wfo in tqdm(nt.sts.keys()):
-        rows.append(dict(wfo=wfo, freq=workflow(wfo)))
+    with get_sqlalchemy_conn("postgis") as pgconn:
+        for wfo in tqdm(nt.sts.keys()):
+            warn, watch, overall = workflow(pgconn, wfo, "TO")
+            rows.append(
+                {"wfo": wfo, "warn": warn, "watch": watch, "overall": overall}
+            )
     df = pd.DataFrame(rows)
     df.to_csv("wfo.csv")
 
 
 def plot():
     """Make a pretty plot"""
+    if not os.path.isfile("wfo.csv"):
+        main()
     df = pd.read_csv("wfo.csv")
     df = df.set_index("wfo")
-    m = MapPlot(
+    overall = df["warn"].sum() / df["watch"].sum() * 100.0
+    mp = MapPlot(
         sector="conus",
-        title="Percentage of Flash Flood Watches receiving 1+ FFW",
-        subtitle="PRELIMINARY PLOT! Please do not share :)",
+        title=(
+            "Percentage of Counties/Parishes in a Tornado Watch "
+            "receiving 1+ Tornado Warning"
+        ),
+        subtitle=(
+            f"2006-2024, Overall: {df['warn'].sum():,.0f} / "
+            f"{df['watch'].sum():,.0f} = {overall:.1f}%"
+        ),
     )
-    cmap = plt.get_cmap("jet")
-    df2 = df[df["freq"].notnull()]
-    m.fill_cwas(
-        df2["freq"].to_dict(),
+    cmap = plt.get_cmap("plasma")
+    df2 = df[df["overall"].notnull()]
+    mp.fill_cwas(
+        df2["overall"].to_dict(),
         cmap=cmap,
+        bins=range(0, 40, 5),
+        extend="max",
         units="%",
         lblformat="%.0f",
         ilabel=True,
     )
-    m.postprocess(filename="test.png")
-    m.close()
+    mp.postprocess(filename="test.png")
+    mp.close()
 
 
 if __name__ == "__main__":
-    # main()
-    # plot()
-    workflow(sys.argv[1], sys.argv[2])
+    plot()
