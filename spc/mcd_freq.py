dee87172dafbad850aaf3ca9c7bcdad31c8f03fc
akrherz <akrherz@iastate.edu>
2025-04-16 17:53:40 -0500

    📝 Update plot
    https://bsky.app/profile/akrherz.bsky.social/post/3lmxonoca522k

--- spc/mcd_freq.py
+++ spc/mcd_freq.py
@@ -2,53 +2,54 @@
 Note the mcd table now explicitly stores this.
 """
 
-import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from pyiem.database import get_sqlalchemy_conn, sql_helper
+from pyiem.plot import figure_axes
 
 
 def main():
     """Go Main Go."""
-    data = [860, 1902, 1450, 717, 656, 313, 4551]
-    cats = ["5%", "20%", "40%", "60%", "80%", "95%", "No\nMention"]
-    total = float(sum(data))
-    (fig, ax) = plt.subplots(1, 1)
+    with get_sqlalchemy_conn("postgis") as conn:
+        data = pd.read_sql(
+            sql_helper("""
+    select watch_confidence, count(*) from mcd
+    where watch_confidence is not null GROUP by watch_confidence
+    order by watch_confidence
+                       """),
+            conn,
+        )
+    data["freq"] = data["count"] / data["count"].sum() * 100.0
+    (fig, ax) = figure_axes(
+        title="Storm Prediction Center :: Mesoscale Discussion (MCD)",
+        subtitle=(
+            f"MCD's with Watch Confidence ({data['count'].sum():.0f}, "
+            "1 May 2012 - 16 Apr 2025)"
+        ),
+        figsize=(8, 6),
+    )
 
-    ax.set_ylim(0, max(data) * 1.2)
-    ax.bar(np.arange(7), data, align="center")
-    for i in range(7):
-        val = "\n%.1f%%" % (data[i] / float(sum(data[:-1])) * 100.0,)
+    ax.set_ylim(0, data["freq"].max() * 1.2)
+    ax.bar(np.arange(6), data["freq"].to_numpy(), align="center")
+    for i, row in data.iterrows():
         ax.text(
             i,
-            data[i] + 250,
-            "%s\n%.1f%%%s"
-            % (data[i], data[i] / total * 100.0, val if i < 6 else ""),
+            row["freq"] + 1,
+            f"{row['freq']:.1f}%",
             va="bottom",
             ha="center",
             bbox=dict(color="#EEEEEE"),
         )
-    ax.set_xticks(range(7))
-    ax.text(
-        0.05,
-        0.95,
-        "Count\n% of Total\n% of non-No Mention Total",
-        ha="left",
-        va="top",
-        bbox=dict(color="#EEEEEE"),
-        transform=ax.transAxes,
-    )
-    ax.set_xticklabels(cats)
+    ax.set_xticks(range(6))
+    ax.set_xticklabels([f"{v}%" for v in data["watch_confidence"].to_list()])
     ax.grid(True)
-    ax.set_title(
-        "Storm Prediction Center :: Mesoscale Discussion\n"
-        f"Watch Confidence ({total:.0f} total, 1 May 2012 - 27 Mar 2017)"
-    )
-    ax.set_ylabel("Frequency (Product Count)")
-    # ax.set_xlabel("Value mentioned in text")
+    ax.set_ylabel("Frequency (%)")
+    ax.set_xlabel("Watch Confidence (%)")
 
     fig.text(
         0.01,
         0.01,
-        "@akrherz, based on unofficial IEM archives, generated 27 March 2017",
+        "@akrherz, based on unofficial IEM archives, generated 16 Apr 2025",
     )
     fig.savefig("test.png")
 
