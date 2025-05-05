dfcbd754bc8fdaf9471bbbeeacddb1141f5f12d5
akrherz <akrherz@iastate.edu>
2025-05-05 13:46:59 -0500

    ✨ Don't load ugc_provider

--- afos/reprocess_cf6.py
+++ afos/reprocess_cf6.py
@@ -28,7 +28,9 @@ def main():
         acursor = adbconn.cursor()
         for row in cursor:
             try:
-                prod = parser(row["data"], utcnow=row["entered"])
+                prod = parser(
+                    row["data"], utcnow=row["entered"], ugc_provider={}
+                )
                 progress.set_description(prod.get_product_id())
             except Exception as exp:
                 LOG.info("failed to parse %s: %s", row["data"], exp)
