3c9538c048aa58fce7498e73da0994f7a5221de1
akrherz <akrherz@iastate.edu>
2025-05-05 09:34:55 -0500

    📝 Sync laptop work

---
+++ prism/fill_800m_grid.py
@@ -0,0 +1,46 @@
+"""Utility script to take the 4km grid and 5x it to fill the 800m grid."""
+
+import shutil
+import subprocess
+
+import click
+import numpy as np
+from pyiem.util import logger, ncopen
+from tqdm import tqdm
+
+LOG = logger()
+
+
+@click.command()
+@click.option("--year", type=int, required=True, help="Year to process")
+def main(year: int):
+    """Go Main Go."""
+    ncfn = f"/mesonet/data/prism/{year}_daily.nc"
+    LOG.info("Moving %s to orig", ncfn)
+    shutil.move(ncfn, f"{ncfn}.orig")
+
+    LOG.info("Creating new file")
+    subprocess.call(
+        [
+            "python",
+            "init_daily.py",
+            "--year",
+            str(year),
+        ]
+    )
+
+    # Fill out the grid
+    with ncopen(f"{ncfn}.orig") as ncin, ncopen(ncfn, "a") as ncout:
+        for ncvar in ["ppt", "tmax", "tmin"]:
+            LOG.info("Filling out %s", ncvar)
+            progress = tqdm(range(ncout.dimensions["time"].size))
+            progress.set_description(f"Processing {ncvar}")
+            for tidx in progress:
+                # Get the 4km grid
+                data = ncin.variables[ncvar][tidx]
+                # Fill out the 800m grid
+                ncout.variables[ncvar][tidx] = np.kron(data, np.ones((5, 5)))
+
+
+if __name__ == "__main__":
+    main()
--- ua/xcheck_igra2_stations.py
+++ ua/xcheck_igra2_stations.py
@@ -7,11 +7,16 @@ from pyiem.database import get_sqlalchemy_conn, sql_helper
 
 def main():
     """GO Main."""
+    fn = "/home/akrherz/projects/pyIEM/src/pyiem/data/reference/igra2icao.txt"
+    pyiem_xref = pd.read_csv(fn, sep=" ", names=["igra2", "icao"])
+
     with get_sqlalchemy_conn("mesosite") as conn:
-        xref = pd.read_sql(
+        icao_xref = pd.read_sql(
             sql_helper(
-                "select id, synop from stations where network = 'RAOB' "
-                "and synop is not null"
+                """
+    select id, synop, country from stations where network = 'RAOB'
+    and synop is not null
+    """
             ),
             conn,
         )
@@ -21,14 +26,37 @@ def main():
     )
     resp = httpx.get(url)
     for line in resp.text.split("\n"):
-        if not line.startswith("USM"):
+        if line.strip() == "":
+            continue
+        igra2 = line[:11]
+        igra2_state = line[38:40]
+        igra2_country = igra2[:2]
+        last_year = int(line[77:81])
+        if not all(line[x].isdigit() for x in range(6, 11)):
+            # print(f"Skipping: {line.strip()}")
             continue
         synop = int(line[6:11])
-        eyear = int(line[77:81])
-        if eyear < 2020:
-            continue
-        if synop not in xref["synop"].values:
-            print(line)
+        if synop in icao_xref["synop"].values:
+            iem_country = icao_xref.loc[
+                icao_xref["synop"] == synop, "country"
+            ].values[0]
+            if igra2_country != iem_country:
+                print(
+                    f"Mismatch: {synop} {iem_country} {igra2_country} "
+                    f"{line.strip()}"
+                )
+        if igra2_country == "US" and igra2_state in ["AK", "HI"]:
+            pyiem_icao = pyiem_xref.loc[pyiem_xref["igra2"] == igra2]["icao"]
+            if pyiem_icao.empty:
+                if last_year > 2020:
+                    print(f"Missing ICAO: {igra2} {line.strip()}")
+            else:
+                pyiem_icao = pyiem_icao.values[0]
+                if pyiem_icao.startswith("K"):
+                    print(
+                        f"ID starts with K: {pyiem_icao} "
+                        f"{igra2} {line.strip()}"
+                    )
 
 
 if __name__ == "__main__":
