0b90f445e206aafb7e03d9f8b06d234ce91af476
akrherz <akrherz@iastate.edu>
2025-08-02 06:49:07 -0500

    🎨 Generalize

--- iem/merge_excel_stations.py
+++ iem/merge_excel_stations.py
@@ -1,51 +1,36 @@
 """we get an excel file with station locations, so we merge"""
 
+import click
 import pandas as pd
-from metpy.units import units as mpunits
 from pyiem.database import get_dbconn
-from pyiem.network import Table as NetworkTable
 
 
-def main():
+@click.command()
+@click.option("--filename", type=str, required=True)
+def main(filename: str):
     """Go!"""
     pgconn = get_dbconn("mesosite")
     cursor = pgconn.cursor()
-    ntcoop = NetworkTable("AZ_COOP")
-    ntdcp = NetworkTable("AZ_DCP")
-    df = pd.read_excel("/tmp/Station Contact Metadata.xlsx")
-    # df2 = df[pd.notnull(df['NWS HB5 Name'])]
-    # print(df2.columns)
+    df = pd.read_csv(filename)
     for _, row in df.iterrows():
-        nwsli = row["SID"]
-        if nwsli in ntdcp.sts:
-            if nwsli in ntcoop.sts:
-                print("%s is dual listed?" % (nwsli,))
-            else:
-                print("%s is DCP, move to COOP" % (nwsli,))
-                cursor.execute(
-                    (
-                        "UPDATE stations SET network = 'AZ_COOP' WHERE "
-                        "id = %s and network = 'AZ_DCP' "
-                    ),
-                    (nwsli,),
-                )
-        elev = (row["Elevation ft"] * mpunits("feet")).to(mpunits("meters"))
+        nwsli = row["nwsli"]
         cursor.execute(
             (
-                "UPDATE stations SET geom='SRID=4326;POINT(%s %s)'"
-                " , county = null, elevation = %s, ugc_zone = null, "
+                "UPDATE stations SET geom=ST_Point(%s, %s, 4326), "
+                "county = null, elevation = null, ugc_zone = null, "
                 "name = %s, plot_name = %s "
-                " WHERE network in ('AZ_DCP', 'AZ_COOP') and id = %s"
+                " WHERE network in ('NE_COOP', 'KS_COOP') and id = %s"
             ),
             (
-                row["Longitude"],
-                row["Latitude"],
-                elev.m,
-                row["Site Name"],
-                row["Site Name"],
+                row["lon"],
+                row["lat"],
+                row["name"],
+                row["name"],
                 nwsli,
             ),
         )
+        if cursor.rowcount == 0:
+            print(f"Failed to update {nwsli} {row['name']}")
 
     cursor.close()
     pgconn.commit()
