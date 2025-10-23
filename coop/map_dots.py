47b0339ea507a2af204d0d7f6d46af970594ea2d
akrherz <akrherz@iastate.edu>
2025-10-22 22:46:37 -0500

    📝 20 Oct feature

--- coop/map_dots.py
+++ coop/map_dots.py
@@ -2,103 +2,81 @@
 
 import os
 
+import numpy as np
 import pandas as pd
-from pyiem.database import get_dbconn, get_sqlalchemy_conn
-from pyiem.network import Table as NetworkTable
-from pyiem.plot import MapPlot
+from pyiem.database import get_sqlalchemy_conn, sql_helper
+from pyiem.plot import MapPlot, get_cmap
 from pyiem.reference import Z_OVERLAY, state_names
 from tqdm import tqdm
 
 
 def get_data():
     """Get data."""
-    conn = get_dbconn("coop")
-    cursor = conn.cursor()
-    data = []
+    dfs = []
     progress = tqdm(state_names)
     for state in progress:
         progress.set_description(state)
-        nt = NetworkTable(f"{state}CLIMATE")
-        for sid in nt.sts:
-            if nt.sts[sid]["archive_begin"] is None:
-                continue
-            if (
-                nt.sts[sid]["archive_begin"].year > 1970
-                or nt.sts[sid]["archive_end"] is not None
-            ):
-                continue
-            cursor.execute(
-                "SELECT sum(precip) from ncei_climate91 where station = %s",
-                (nt.sts[sid]["ncei91"],),
+        with get_sqlalchemy_conn("coop") as dbconn:
+            df = pd.read_sql(
+                sql_helper(
+                    """
+    with data as (
+      select station, count(*), sum(case when high = 67 then 1 else 0 end)
+      as hits
+      from {table} where high is not null group by station)
+    select station, st_x(geom) as lon, st_y(geom) as lat,
+    hits / count::float * 100 as freq
+    from stations t JOIN data d on (t.id = d.station) WHERE
+    t.network ~* 'CLIMATE' and count > 50 * 365
+                """,
+                    table=f"alldata_{state.lower()}",
+                ),
+                dbconn,
+                index_col="station",
             )
-            climo = cursor.fetchone()[0]
-            with get_sqlalchemy_conn("coop") as dbconn:
-                df = pd.read_sql(
-                    "SELECT day, precip from alldata WHERE station = %s "
-                    "ORDER by day asc",
-                    dbconn,
-                    params=(sid,),
-                    index_col="day",
-                )
-            mindays = 1
-            days = 100
-            while (days - mindays) > 1:
-                res = df["precip"].rolling(days).sum() > climo
-                if res.sum() > 0:  # hit
-                    days -= max([int((days - mindays) / 2), 1])
-                    continue
-                mindays = days
-                days += 15
-
-            data.append(
-                {
-                    "sid": sid,
-                    "days": days,
-                    "lat": nt.sts[sid]["lat"],
-                    "lon": nt.sts[sid]["lon"],
-                }
-            )
-    pd.DataFrame(data).to_csv("/tmp/data.csv", index=False)
+            dfs.append(df)
+    pd.concat(dfs).to_csv("/tmp/data.csv", index=True)
 
 
 def main():
     """Go Main Go."""
     if not os.path.isfile("/tmp/data.csv"):
         get_data()
-    df = pd.read_csv("/tmp/data.csv").sort_values("days", ascending=False)
+    df = pd.read_csv("/tmp/data.csv").sort_values("freq", ascending=False)
+    df["days"] = df["freq"] / 100.0 * 365.25
+    df["state"] = df["station"].str.slice(0, 2)
+    print(df[df["state"] == "HI"].head(20))
+    print(df["days"].describe())
     # bins = [1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366]
-    bins = [1, 7, 21, 45, 90, 120, 180, 210, 240, 270, 300]
-    # cmap = get_cmap("jet")
+    # bins = [1, 7, 21, 45, 90, 120, 180, 210, 240, 270, 300]
+    cmap = get_cmap("managua")
     # norm = mpcolors.Normalize(1, 366)
     # colors = cmap(norm(df['doy'].values))
 
     mp = MapPlot(
-        title=(
-            "Min Consec Day Period Whereby Site Accumulated Yearly Climatology"
-        ),
+        title=("Days per Year with Daily High Temperature of 67°F"),
         axisbg="white",
-        subtitle=(
-            "Based on NWS COOP Sites with 50+ years data, "
-            "NCEI 1991-2020 Climatology"
-        ),
-        sector="conus",
+        subtitle=("Based on NWS COOP Sites with 50+ years data, CA"),
+        sector="state",
+        state="HI",
     )
     # clevlabels = calendar.month_abbr[1:] + [""]
-    mp.scatter(
+    mp.contourf(
         df["lon"].values,
         df["lat"].values,
         df["days"].values,
-        bins,
-        # cmap=cmap,
-        marker="o",
-        s=50,
+        np.arange(0, 10, 1),
+        cmap=cmap,
+        # marker="o",
+        # s=50,
         zorder=Z_OVERLAY,
         extend="max",
         spacing="proportional",
+        units="days / year",
         # clevlabels=clevlabels,
     )
 
-    mp.postprocess(filename="230113.png")
+    mp.postprocess(filename="251021.png")
 
 
 if __name__ == "__main__":
