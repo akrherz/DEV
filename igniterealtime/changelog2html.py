3021fa4131ba564d4478ac8d8ee84ae74253cd5f
akrherz <akrherz@iastate.edu>
2025-12-12 14:30:43 -0600

    🎨 Remove backslashes

--- igniterealtime/changelog2html.py
+++ igniterealtime/changelog2html.py
@@ -10,11 +10,14 @@ def main():
     """Go Main."""
     with open("changelog", encoding="utf-8") as fh:
         for line in fh:
+            if line.strip() == "":
+                continue
+            # The line is basically markdown, which ends up escaping mundane
+            # things ok in HTML, like `_`.  So we remove any backslashes
+            line = line.replace("\\", "")
             if line.startswith("### "):
                 print(f"</ul>\n\n<h2>{line[3:].strip()}</h2>")
                 print("<ul>")
-                continue
-            if line.strip() == "":
                 continue
             issue, rest = line.split(maxsplit=1)
             print(
