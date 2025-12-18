ab7db23b4549e2c6c4f63d0f23cec9920b0c6ee7
akrherz <akrherz@iastate.edu>
2025-12-18 08:29:23 -0600

    📝 Feature 18 Dec 2025

--- iemre/asos_v_iemre.py
+++ iemre/asos_v_iemre.py
@@ -1,9 +1,9 @@
 """Scatter plot between IEMRE and ASOS."""
 
+import httpx
 import pandas as pd
-import requests
-from pyiem.plot.use_agg import plt
-from pyiem.util import get_sqlalchemy_conn
+from pyiem.database import get_sqlalchemy_conn
+from pyiem.plot import figure_axes
 from tqdm import tqdm
 
 
@@ -12,39 +12,42 @@ def main():
     with get_sqlalchemy_conn("iem") as pgconn:
         df = pd.read_sql(
             "SELECT id, max_tmpf, st_x(geom) as lon, st_y(geom) as lat from "
-            "summary_2020 s JOIN stations t on (s.iemid = t.iemid) WHERE "
-            "s.day = '2020-05-20' and t.network in ('IA_ASOS', 'AWOS') "
+            "summary_2025 s JOIN stations t on (s.iemid = t.iemid) WHERE "
+            "s.day = '2025-12-17' and t.network = 'IA_ASOS' "
             "ORDER by id asc",
             pgconn,
             index_col="id",
         )
-    df["srad"] = 0
+    df["snowd"] = 0.0
     progress = tqdm(df.iterrows(), total=len(df.index))
     for sid, row in progress:
         progress.set_description(sid)
         uri = (
-            "https://mesonet.agron.iastate.edu/iemre/daily/2020-05-20/"
+            "https://mesonet.agron.iastate.edu/iemre/daily/2025-12-17/"
             f"{row['lat']}/{row['lon']}/json"
         )
-        data = requests.get(uri, timeout=30).json()
-        df.at[sid, "srad"] = data["data"][0]["srad_mj"]
+        data = httpx.get(uri, timeout=30).json()
+        value = data["data"][0]["snowd_12z_in"]
+        if value < 0.2:
+            value = 0.0
+        df.at[sid, "snowd"] = value
 
-    (fig, ax) = plt.subplots(1, 1)
-    ax.scatter(df["srad"], df["max_tmpf"])
+    (fig, ax) = figure_axes(
+        title="17 December 2025 ASOS High Temperature vs Morning Snow Depth",
+        subtitle="ASOS sites labelled by station identifier",
+        figsize=(8, 6),
+    )
+    ax.scatter(df["snowd"], df["max_tmpf"])
     for sid, row in df[
         (df["max_tmpf"] >= 68) | (df["max_tmpf"] < 62)
     ].iterrows():
         ax.text(
-            row["srad"], row["max_tmpf"] + 0.2, sid, ha="center", rotation=45
+            row["snowd"], row["max_tmpf"] + 0.2, sid, ha="center", rotation=45
         )
-    ax.set_title(
-        "20 May 2020 High Temperature vs Solar Radiation\n"
-        "Selected ASOS/AWOS sites labeled by site id"
-    )
-    ax.set_xlabel("IEMRE Estimated Solar Radiation [MJ]")
-    ax.set_ylabel(r"Observed Iowa ASOS/AWOS High [$^\circ$F]")
+    ax.set_xlabel("IEMRE Estimated Morning Snow Depth [inches]")
+    ax.set_ylabel("Observed Iowa ASOS High [°F]")
     ax.grid(True)
-    fig.savefig("test.png")
+    fig.savefig("251218.png")
 
 
 if __name__ == "__main__":
