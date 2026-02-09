8ae69c8f56b841d7116a17faf520562173fa788d
akrherz <akrherz@iastate.edu>
2026-02-09 14:51:34 -0600

    🎨 Modernize

--- asos/manual_xhour_qc.py
+++ asos/manual_xhour_qc.py
@@ -3,41 +3,49 @@ IEM Autoplot 169 encovers lots of data QC issues, so we have some quasi
 automation here to help with manual dumping.
 """
 
+from datetime import timezone
+
 import click
 import pandas as pd
-import pytz
-from pyiem.database import get_dbconn, get_sqlalchemy_conn
+from pyiem.database import sql_helper, with_sqlalchemy_conn
+from sqlalchemy.engine import Connection
 
 
-def process(engine, conn, row, station):
+@with_sqlalchemy_conn("asos")
+def process(row, station, varname: str, conn: Connection | None = None):
     """Do what we need to do here."""
     delta = pd.Timedelta(hours=3)
     obs = pd.read_sql(
-        "SELECT valid, tmpf, dwpf, feel, relh from alldata where "
-        "station = %s and valid >= %s and valid <= %s and tmpf is not null "
-        "ORDER by valid ASC",
-        engine,
-        params=(
-            station,
-            row["start_valid_utc"].replace(tzinfo=pytz.UTC) - delta,
-            row["end_valid_utc"].replace(tzinfo=pytz.UTC) + delta,
+        sql_helper(
+            """
+    SELECT valid, tmpf, dwpf, feel, relh from alldata where
+    station = :station and valid >= :sts and valid <= :ets
+    and {varname} is not null ORDER by valid ASC""",
+            varname=varname,
         ),
+        conn,
+        params={
+            "station": station,
+            "sts": row["start_valid_utc"] - delta,
+            "ets": row["end_valid_utc"] + delta,
+        },
         index_col=None,
     )
     print(obs.head(100))
     res = input("List (space sep) dumped obs or enter for noop: ")
     if res == "":
         return
-    cursor = conn.cursor()
     for _, cullrow in obs.loc[[int(i) for i in res.split()]].iterrows():
-        cursor.execute(
-            f"UPDATE t{cullrow['valid'].year} SET tmpf = null, dwpf = null, "
-            "feel = null, relh = null, editable = 'f' "
-            "WHERE station = %s and valid = %s",
-            (station, cullrow["valid"]),
+        conn.execute(
+            sql_helper(
+                """
+    UPDATE {table} SET tmpf = null, dwpf = null, feel = null, relh = null,
+    editable = 'f' WHERE station = :station and valid = :valid""",
+                table=f"t{cullrow['valid'].year}",
+            ),
+            {"station": station, "valid": cullrow["valid"]},
         )
-        print(f"Nulled out {cullrow['valid']} count: {cursor.rowcount}")
-    cursor.close()
+        print(f"Nulled out {cullrow['valid']} count: {conn.rowcount}")
     conn.commit()
 
 
@@ -56,11 +64,10 @@ def main(network, station, hours, mydir, how, varname):
         f"dir:{mydir}::how:{how}::v:{varname}::_cb:1.csv"
     )
     df = pd.read_csv(url, parse_dates=["start_valid_utc", "end_valid_utc"])
-
-    conn = get_dbconn("asos")
-    with get_sqlalchemy_conn("asos") as engine:
-        for _, row in df.iterrows():
-            process(engine, conn, row, station)
+    df["start_valid_utc"] = df["start_valid_utc"].dt.tz_localize(timezone.utc)
+    df["end_valid_utc"] = df["end_valid_utc"].dt.tz_localize(timezone.utc)
+    for _, row in df.iterrows():
+        process(row, station, varname)
 
 
 if __name__ == "__main__":
