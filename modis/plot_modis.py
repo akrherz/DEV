7d8fe227e6345bb124949b2de218ca2a8b7b31d0
akrherz <akrherz@iastate.edu>
2026-02-25 07:58:36 -0600

    📝 25 Feb 2026 Feature

--- modis/plot_modis.py
+++ modis/plot_modis.py
@@ -4,19 +4,20 @@ https://wvs.earthdata.nasa.gov/
 """
 
 import matplotlib.colors as mpcolors
-from pandas import read_sql
+import pandas as pd
+from matplotlib.image import imread
+from pyiem.database import get_sqlalchemy_conn, sql_helper
 from pyiem.plot import MapPlot, get_cmap
-from pyiem.plot.use_agg import plt
 from pyiem.reference import Z_CLIP
-from pyiem.util import get_dbconnstr
 
 
 def main():
     """Go Main Go."""
     mp = MapPlot(
         title=(
-            "4 September 2024 :: Terra MODIS Corrected Reflectance True Color"
+            "22 February 2026 :: Terra MODIS Corrected Reflectance True Color"
         ),
+        subtitle="24 February 2026 ASOS Max Temperature (°F)",
         sector="custom",
         apctx={"_r": "96"},
         west=-97.6,
@@ -26,60 +27,56 @@ def main():
         stateborderwidth=2,
     )
 
-    img = plt.imread("/tmp/snapshot-2024-09-04.jpg")
-    with open("/tmp/snapshot-2024-09-04.jgw", encoding="ascii") as fh:
+    img = imread("/tmp/snapshot-2026-02-22.jpg")
+    with open("/tmp/snapshot-2026-02-22.jgw", encoding="ascii") as fh:
         res = [float(x) for x in fh.readlines()]
-    ulx = res[4]
-    lrx = ulx + res[0] * img.shape[1]
-    uly = res[5]
-    lry = res[5] + res[3] * img.shape[0]
+    a, _d, _b, e, c, f = res
+    nrows, ncols = img.shape[:2]
+
+    left = c - a / 2.0
+    right = left + a * ncols
+    top = f - e / 2.0
+    bottom = top + e * nrows
+
+    xmin, xmax = sorted((left, right))
+    ymin, ymax = sorted((bottom, top))
     mp.panels[0].ax.imshow(
         img,
-        extent=(ulx, lrx, lry, uly),
-        origin="upper",
+        extent=(xmin, xmax, ymin, ymax),
+        origin="upper" if e < 0 else "lower",
         zorder=Z_CLIP,
     )
     # mp.drawcounties("tan")
-    df = read_sql(
-        """
-        SELECT st_x(geom) as lon, st_y(geom) as lat, max_tmpf, snowd,
-        network from summary_2023 s JOIN stations t on (s.iemid = t.iemid)
-        where day = '2023-11-27' and (network ~* 'ASOS' or network ~* 'COOP')
-        and id != 'VER'
-        """,
-        get_dbconnstr("iem"),
-        index_col=None,
-    )
-    df2 = df[df["snowd"] > 0.01]
-    mp.plot_values(
-        df2["lon"].values,
-        df2["lat"].values,
-        ["."] * len(df2.index),
-        labeltextsize=6,
-        color="blue",
-        fmt="%s",
-        labelbuffer=0,
-        outlinecolor="blue",
-    )
-    df2 = df[df["max_tmpf"] > 0]
+    with get_sqlalchemy_conn("iem") as conn:
+        df = pd.read_sql(
+            sql_helper("""
+            SELECT st_x(geom) as lon, st_y(geom) as lat, max_tmpf, snowd,
+            network from summary_2026 s JOIN stations t on (s.iemid = t.iemid)
+            where day = '2026-02-24' and (network ~* 'ASOS')
+            and max_tmpf > 0
+            """),
+            conn,
+            index_col=None,
+        )
     # create a simple color ramp for the temperatures
-    cmap = get_cmap("RdYlBu_r")
+    cmap = get_cmap("RdBu_r")
     cmap.set_under("white")
     cmap.set_over("black")
-    norm = mpcolors.BoundaryNorm([25, 30, 32, 35, 40, 50, 60], cmap.N)
+    norm = mpcolors.BoundaryNorm([25, 32, 40, 50, 60], cmap.N)
     mp.plot_values(
-        df2["lon"].values,
-        df2["lat"].values,
-        df2["max_tmpf"].values,
-        labeltextsize=8,
+        df["lon"].to_numpy(),
+        df["lat"].to_numpy(),
+        df["max_tmpf"].to_numpy(),
+        labeltextsize=12,
         fmt="%.0f",
         labelbuffer=1,
-        color=[x for x in cmap(norm(df2["max_tmpf"].values))],
+        color="white",
         outlinecolor="black",
+        backgroundcolor=[x for x in cmap(norm(df["max_tmpf"].to_numpy()))],
         isolated=True,
     )
 
-    mp.fig.savefig("240905.png")
+    mp.fig.savefig("260225.png")
 
 
 if __name__ == "__main__":
