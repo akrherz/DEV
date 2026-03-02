e66ee87e8e362fb4f8fee9f0c3590d00264d1f1d
akrherz <akrherz@iastate.edu>
2026-03-02 13:37:51 -0600

    🎨 Update for KS specific dump

--- dep/dump_hillslopes.py
+++ dep/dump_hillslopes.py
@@ -1,20 +1,27 @@
 """Shrug, just do as I am told."""
 
-from geopandas import read_postgis
-from pyiem.util import get_sqlalchemy_conn
+import geopandas as gpd
+from pyiem.database import get_sqlalchemy_conn, sql_helper
+from pyiem.util import logger
+
+LOG = logger()
 
 
 def main():
     """Go Main Go."""
-    with get_sqlalchemy_conn("idep") as pgconn:
-        df = read_postgis(
-            """
-            SELECT st_transform(geom, 4326) as geom, huc_12, fpath
-            from flowpaths WHERE scenario = 0
-        """,
-            pgconn,
-        )
-    df.to_file("flowpaths.shp")
+    with get_sqlalchemy_conn("idep") as conn:
+        flowpaths = gpd.read_postgis(
+            sql_helper("""
+            SELECT st_transform(f.geom, 4326) as geom, f.huc_12, fpath
+            from flowpaths f, huc12 h WHERE f.scenario = 0 and h.scenario = 0
+            and h.states ~* 'KS' and f.huc_12 = h.huc_12
+        """),
+            conn,
+            geom_col="geom",
+            crs=4326,
+        )  # type: ignore
+    LOG.info("Found %s flowpaths", len(flowpaths.index))
+    flowpaths.to_file("ks_flowpaths.shp")
 
 
 if __name__ == "__main__":
