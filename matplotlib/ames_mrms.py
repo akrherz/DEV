c990584633c92260bbd7ac3af78ce14f206d1279
akrherz <akrherz@iastate.edu>
2026-07-03 22:15:33 -0500

    📝 4 Jul 2026 feature

--- matplotlib/ames_mrms.py
+++ matplotlib/ames_mrms.py
@@ -1,5 +1,6 @@
 """MRMS Plotting util for zoomed in areas"""
 
+import pandas as pd
 import pygrib
 from pyiem.database import get_sqlalchemy_conn
 from pyiem.plot import MapPlot, nwsprecip
@@ -8,20 +9,20 @@ from pyiem.util import mm2inch
 from sqlalchemy import text
 
 
-def get_data():
+def get_data() -> pd.DataFrame:
     """Get data"""
     lons = []
     lats = []
     vals = []
     labels = []
 
-    networks = ["IACOCORAHS"]
+    networks = ["IA_COOP"]
     with get_sqlalchemy_conn("iem") as conn:
         res = conn.execute(
             text("""
         SELECT id, st_x(geom), st_y(geom), sum(pday)
-        from summary_2024 s JOIN stations t
-        on (s.iemid = t.iemid) WHERE s.day = '2024-07-23'
+        from summary_2026 s JOIN stations t
+        on (s.iemid = t.iemid) WHERE s.day = '2026-07-03'
         and t.network = ANY(:networks)
         and pday > 0 GROUP by id, st_x, st_y
         ORDER by sum DESC
@@ -33,7 +34,48 @@ def get_data():
             lats.append(row[2])
             vals.append("%.2f" % (row[3],))
             labels.append(row[0])
-    return lons, lats, vals, labels
+    networks = ["IA_COCORAHS"]
+    with get_sqlalchemy_conn("coop") as conn:
+        res = conn.execute(
+            text("""
+        SELECT id, st_x(geom), st_y(geom), sum(precip)
+        from cocorahs_2026 s JOIN stations t
+        on (s.iemid = t.iemid) WHERE s.day = '2026-07-03'
+        and t.network = ANY(:networks)
+        and precip > 0 GROUP by id, st_x, st_y
+        ORDER by sum DESC
+        """),
+            {"networks": networks},
+        )
+        for row in res:
+            lons.append(row[1])
+            lats.append(row[2])
+            vals.append("%.2f" % (row[3],))
+            labels.append(row[0])
+
+    with get_sqlalchemy_conn("postgis") as conn:
+        res = conn.execute(
+            text("""
+        SELECT ctid, st_x(geom), st_y(geom), magnitude
+        from lsrs_2026 s WHERE valid > '2026-07-03 06:00'
+        and valid < '2026-07-03 20:00' and type = 'R'
+        and magnitude > 0 ORDER by magnitude desc
+        """),
+            {"networks": networks},
+        )
+        for row in res:
+            lons.append(row[1])
+            lats.append(row[2])
+            vals.append("%.2f" % (row[3],))
+            labels.append(row[0])
+    return pd.DataFrame(
+        {
+            "lon": lons,
+            "lat": lats,
+            "val": vals,
+            "label": labels,
+        }
+    ).sort_values("val", ascending=False)
 
 
 def main():
@@ -41,23 +83,24 @@ def main():
     title = "NOAA MRMS: RADAR + Guage Corrected Rainfall Estimates"
     mp = MapPlot(
         sector="spherical_mercator",
-        north=42.1,
-        east=-93.55,
-        south=41.95,
+        north=41.9,
+        east=-93.35,
+        south=41.55,
         west=-93.7,
         titlefontsize=14,
         title=title,
         subtitle=(
-            "MRMS 24h Ending: 7 AM 23 July 2024, Morning CoCoRaHS Reports"
+            "MRMS 24h Ending: 10 AM 3 July 2026, "
+            "Morning CoCoRaHS/COOP Reports, NWS Local Storm Reports"
         ),
     )
 
-    grbs = pygrib.open("MultiSensor_QPE_24H_Pass2_00.00_20240723-120000.grib2")
+    grbs = pygrib.open("MultiSensor_QPE_24H_Pass2_00.00_20260703-150000.grib2")
     grb = grbs.message(1)
     pcpn = mm2inch(grb["values"])
     lats, lons = grb.latlons()
     lons -= 360.0
-    clevs = [0.01, 0.05, 0.2, 0.5, 1, 1.5]
+    clevs = [0.01, 0.25, 0.5, 1, 2, 3, 5, 7, 10]
     cmap = nwsprecip()
     cmap.set_over("k")
 
@@ -73,18 +116,18 @@ def main():
         alpha=0.1,
     )
     mp.drawcounties()
-    lons, lats, vals, _labels = get_data()
+    obsdf = get_data()
     mp.plot_values(
-        lons,
-        lats,
-        vals,
+        obsdf["lon"].to_numpy(),
+        obsdf["lat"].to_numpy(),
+        obsdf["val"].to_numpy(),
         "%s",
         # labels=labels,
         labelbuffer=1,
         zorder=Z_OVERLAY2,
         labelcolor="white",
     )
-    mp.postprocess(filename="test.png")
+    mp.postprocess(filename="260704.png")
 
 
 if __name__ == "__main__":
